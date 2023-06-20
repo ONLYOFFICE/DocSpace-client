fbc7739c0786f012a28a88459bcf0e006b2e3f53
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2023-06-20 18:51:53 +0500

    Web:Dashboard Fixed style

--- packages/client/src/pages/Home/Dashboard/Dashboard.styled.ts
+++ packages/client/src/pages/Home/Dashboard/Dashboard.styled.ts
@@ -3,9 +3,12 @@ import styled from "styled-components";
 export const DashboardContainer = styled.section`
   display: grid;
 
-  grid-template-columns: repeat(auto-fill, 296px);
+  grid-template-columns: repeat(auto-fit, 264px);
   grid-auto-flow: column;
+  gap: 30px;
 
-  width: 100%;
+  width: min-content;
   height: 100%;
+
+  padding-right: 20px;
 `;

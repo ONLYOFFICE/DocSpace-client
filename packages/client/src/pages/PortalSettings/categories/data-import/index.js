0b561e90fba27e53cace3ae018d067c44b3eae7b
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-07-17 15:58:37 +0500

    data migration page was created

--- packages/client/src/pages/PortalSettings/categories/data-import/index.js
+++ packages/client/src/pages/PortalSettings/categories/data-import/index.js
@@ -1,4 +1,46 @@
+import React from "react";
+import Text from "@docspace/components/text";
+
+import styled from "styled-components";
+
+import { IntegrationButton } from "./components/integration-button";
+
+import GoogleWorkspaceSvg from "PUBLIC_DIR/images/googleworkspace.react.svg?url";
+import NextcloudSvg from "PUBLIC_DIR/images/nextcloud.logo.react.svg?url";
+import OnlyOfficeWorkspaceSvg from "PUBLIC_DIR/images/logo/workspace.svg?url";
+
+const DescriptionWrapper = styled.div`
+  max-width: 675px;
+  margin-bottom: 21px;
+  margin-top: 3px;
+`;
+const WorkspacesWrapper = styled.div`
+  max-width: 700px;
+  margin-top: 21px;
+  gap: 20px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const DataImport = () => {
-  return <div>Data Import Page</div>;
+  return (
+    <div>
+      <DescriptionWrapper>
+        <Text color="#657077" lineHeight="20px">
+          Import data from a third party service to ONLYOFFICE DocSpace. Data import allows
+          transferring data such as all users, their personal and shared documents.
+        </Text>
+      </DescriptionWrapper>
+      <Text fontWeight={600}>
+        Upload a backup copy from a desired service below to start migration.
+      </Text>
+
+      <WorkspacesWrapper>
+        <IntegrationButton icon={GoogleWorkspaceSvg} />
+        <IntegrationButton icon={NextcloudSvg} />
+        <IntegrationButton icon={OnlyOfficeWorkspaceSvg} />
+      </WorkspacesWrapper>
+    </div>
+  );
 };
 export default DataImport;

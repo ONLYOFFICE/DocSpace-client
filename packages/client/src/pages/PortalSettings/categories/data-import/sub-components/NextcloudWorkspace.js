9411969debc0dc4278a7013f142ad92a2f1535bb
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-07-18 15:08:56 +0500

    base page was added

--- packages/client/src/pages/PortalSettings/categories/data-import/sub-components/NextcloudWorkspace.js
+++ packages/client/src/pages/PortalSettings/categories/data-import/sub-components/NextcloudWorkspace.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { withTranslation } from "react-i18next";
+import { inject, observer } from "mobx-react";
 
-const NextcloudWorkspace = () => {
-  return <div>Nextcloud workspace</div>;
+import ImportSection from "./ImportSection";
+import Text from "@docspace/components/text";
+
+import PeopleIcon from "PUBLIC_DIR/images/catalog.accounts.react.svg";
+
+const NextcloudWorkspace = (props) => {
+  const { t } = props;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = () => {
+    setIsChecked((prevIsChecked) => !prevIsChecked);
+  };
+  return (
+    <div>
+      <Text className="data-import-description">{t("Settings:AboutDataImport")}</Text>
+      <ImportSection
+        isChecked={isChecked}
+        onChange={onChange}
+        sectionName="Users"
+        description="Section “Users” includes the users you selected in the previous step. By default, it is always enabled and can’t be unselected. "
+        exportSection={{ sectionName: "users", workspace: "Google Workspace" }}
+        importSection={{ sectionName: "Accounts", workspace: "DocSpace", SectionIcon: PeopleIcon }}
+      />
+    </div>
+  );
 };
 
-export default NextcloudWorkspace;
+export default inject(({ setup }) => {
+  const { initSettings } = setup;
+  return {
+    initSettings,
+  };
+})(withTranslation(["Common, Settings"])(observer(NextcloudWorkspace)));

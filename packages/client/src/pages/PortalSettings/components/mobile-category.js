f1a9179446fe0717d4853856bc1174c890669953
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2023-08-09 12:58:24 +0300

    Web: PortalSettings: Moving the styles for the mobile category to a separate folder.

--- packages/client/src/pages/PortalSettings/components/mobile-category.js
+++ packages/client/src/pages/PortalSettings/components/mobile-category.js
@@ -1,12 +1,52 @@
 import React from "react";
+import styled from "styled-components";
+
 import Text from "@docspace/components/text";
 import Link from "@docspace/components/link";
 import { Base } from "@docspace/components/themes";
-import {
-  StyledMobileCategoryWrapper,
-  StyledArrowRightIcon,
-} from "../categories/security/StyledSecurity";
 import { combineUrl } from "@docspace/common/utils";
+import commonIconsStyles from "@docspace/components/utils/common-icons-style";
+
+import ArrowRightIcon from "PUBLIC_DIR/images/arrow.right.react.svg";
+
+const StyledArrowRightIcon = styled(ArrowRightIcon)`
+  ${commonIconsStyles}
+  path {
+    fill: ${(props) => props.theme.client.settings.security.arrowFill};
+  }
+`;
+
+const StyledMobileCategoryWrapper = styled.div`
+  margin-bottom: 20px;
+
+  .category-item-heading {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+  }
+
+  .category-item-subheader {
+    font-size: 13px;
+    font-weight: 600;
+    margin-bottom: 5px;
+  }
+
+  .category-item-description {
+    color: ${(props) => props.theme.client.settings.security.descriptionColor};
+    font-size: 12px;
+    max-width: 1024px;
+  }
+
+  .inherit-title-link {
+    margin-right: 7px;
+    font-size: 16px;
+    font-weight: 600;
+  }
+
+  .link-text {
+    margin: 0;
+  }
+`;
 
 const MobileCategoryWrapper = (props) => {
   const { title, url, subtitle, onClickLink } = props;

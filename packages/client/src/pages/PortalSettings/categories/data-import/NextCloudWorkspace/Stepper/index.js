2a08d0aad2742a064ab2472574255ca6056d5ed4
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-08-28 22:00:14 +0500

    translations were added

--- packages/client/src/pages/PortalSettings/categories/data-import/NextCloudWorkspace/Stepper/index.js
+++ packages/client/src/pages/PortalSettings/categories/data-import/NextCloudWorkspace/Stepper/index.js
@@ -25,29 +25,24 @@ export const getStepsData = (t, currentStep, setCurrentStep) => {
   return [
     {
       title: t("Common:SelectFile"),
-      description:
-        "Select the Nextcloud backup file to start the data import. Only one file can be selected. Once the data upload and analysis are complete, the next step will be initiated automatically.",
+      description: t("Settings:SelectFileDescriptionNextcloud"),
       component: <FirstStep t={t} incrementStep={incrementStep} decrementStep={decrementStep} />,
     },
     {
-      title: "Select users with e-mail",
-      description:
-        "Check users from the list to import into ONLYOFFICE DocSpace. Only users with emails can be selected. Users already existing in ONLYOFFICE DocSpace are highlighted with a green color and are not checked by default.",
+      title: t("Settings:SelectUsers"),
+      description: t("Settings:SelectUsersDescriptionNextcloud"),
       component: <SecondStep t={t} incrementStep={incrementStep} decrementStep={decrementStep} />,
     },
     {
-      title: "Add emails to incomplete accounts",
-      description:
-        "Check users from the list to import into ONLYOFFICE DocSpace. Only users with emails can be selected. Users already existing in ONLYOFFICE DocSpace are highlighted with a green color and are not checked by default.",
+      title: t("Settings:AddEmails"),
+      description: t("Settings:SelectUsersDescriptionNextcloud"),
       component: <ThirdStep t={t} incrementStep={incrementStep} decrementStep={decrementStep} />,
     },
     {
-      title: "Select user types",
+      title: t("Settings:SelectUserTypes"),
       description: (
         <>
-          Select DocSpace roles for the imported users: DocSpace admin, Room admin or Power user. By
-          default, Power user role is selected for each user. You can manage the roles after the
-          import.
+          {t("Settings:SelectUserTypesDescription")}
           <HelpButton
             place="right"
             offsetRight={0}
@@ -64,19 +59,18 @@ export const getStepsData = (t, currentStep, setCurrentStep) => {
       component: <FourthStep t={t} incrementStep={incrementStep} decrementStep={decrementStep} />,
     },
     {
-      title: "Data import",
-      description:
-        "Select sections for import. They will appear in the corresponding sections of DocSpace.",
+      title: t("Settings:DataImport"),
+      description: t("Settings:ImportSectionDescription"),
       component: <FifthStep t={t} incrementStep={incrementStep} decrementStep={decrementStep} />,
     },
     {
-      title: "Processing of data import",
-      description: "Data migration in progress. Please wait.",
+      title: t("Settings:DataImportProcessing"),
+      description: t("Settings:ImportProcessingDescription"),
       component: <SixthStep t={t} incrementStep={incrementStep} decrementStep={decrementStep} />,
     },
     {
-      title: "Data import complete",
-      description: "Data import from NextCloud to ONLYOFFICE DocSpace is complete!",
+      title: t("Settings:DataImportComplete"),
+      description: t("Settings:ImportCompleteDescriptionNextcloud"),
       component: <SeventhStep t={t} incrementStep={incrementStep} decrementStep={decrementStep} />,
     },
   ];

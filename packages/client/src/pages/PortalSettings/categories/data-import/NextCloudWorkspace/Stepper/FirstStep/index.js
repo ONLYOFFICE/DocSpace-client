cea89890ca7008cb6d6c94f339ee1486334adbf7
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-09-08 15:17:25 +0500

    upload logic was added

--- packages/client/src/pages/PortalSettings/categories/data-import/NextCloudWorkspace/Stepper/FirstStep/index.js
+++ packages/client/src/pages/PortalSettings/categories/data-import/NextCloudWorkspace/Stepper/FirstStep/index.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+import { inject, observer } from "mobx-react";
+
 import Text from "@docspace/components/text";
 import FileInput from "@docspace/components/file-input";
 import SaveCancelButtons from "@docspace/components/save-cancel-buttons";
-import styled from "styled-components";
+import ProgressBar from "@docspace/components/progress-bar";
+import Button from "@docspace/components/button";
+import { CancelUploadDialog } from "SRC_DIR/components/dialogs";
+
+import { useSearchParams } from "react-router-dom";
 
 const Wrapper = styled.div`
   max-width: 350px;
@@ -21,37 +29,127 @@ const Wrapper = styled.div`
   .upload-back-buttons {
     margin-top: 16px;
   }
+
+  .select-file-progress-text {
+    margin: 12px 0;
+  }
+
+  .select-file-progress-bar {
+    margin-bottom: 16px;
+  }
 `;
 
-const FirstStep = ({ t, incrementStep, decrementStep }) => {
-  const [showReminder, setShowReminder] = useState(false);
+const FirstStep = ({
+  t,
+  incrementStep,
+  decrementStep,
+  cancelUploadDialogVisible,
+  setCancelUploadDialogVisible,
+  initMigrationName,
+  localFileUploading,
+  getMigrationStatus,
+  setUsers,
+  isFileLoading,
+  setIsFileLoading,
+}) => {
+  const [isSaveDisabled, setIsSaveDisabled] = useState(false);
+  const [searchParams] = useSearchParams();
 
-  const onClickInput = (file) => {
-    let data = new FormData();
-    data.append("file", file);
-    setShowReminder(true);
+  const [progress, setProgress] = useState(0);
+
+  const onUploadFile = async (file) => {
+    await localFileUploading(file, setProgress);
+    await initMigrationName(searchParams.get("service"));
+    const interval = setInterval(async () => {
+      const res = await getMigrationStatus();
+
+      if (!res || res.parseResult.failedArchives.length > 0) {
+        console.error("something went wrong");
+        setIsFileLoading(false);
+        clearInterval(interval);
+      } else if (res.isCompleted) {
+        setIsFileLoading(false);
+        clearInterval(interval);
+        setUsers(res);
+        setIsSaveDisabled(true);
+      }
+    }, 1000);
+  };
+
+  const onSelectFile = (file) => {
+    setIsFileLoading(true);
+    try {
+      onUploadFile(file);
+    } catch (error) {
+      console.log(error);
+      setIsFileLoading(false);
+    }
+  };
+
+  const onCancel = () => {
+    setCancelUploadDialogVisible(true);
+    setProgress(0);
+    setIsFileLoading(false);
   };
 
   return (
     <Wrapper>
       <Text className="choose-backup-file">{t("Settings:ChooseBackupFile")}</Text>
       <FileInput
-        onInput={onClickInput}
+        onInput={onSelectFile}
         className="upload-backup-input"
         placeholder={t("Settings:BackupFile")}
         scale
       />
-      <SaveCancelButtons
-        className="upload-back-buttons"
-        onSaveClick={incrementStep}
-        onCancelClick={decrementStep}
-        showReminder={showReminder}
-        saveButtonLabel={t("Settings:UploadToServer")}
-        cancelButtonLabel={t("Common:Back")}
-        displaySettings={true}
-      />
+      {isFileLoading ? (
+        <>
+          <Text className="select-file-progress-text">{t("Settings:BackupFileUploading")}</Text>
+          <ProgressBar percent={progress} className="select-file-progress-bar" />
+          <Button size="small" label={t("Common:CancelButton")} onClick={onCancel} />
+        </>
+      ) : (
+        <SaveCancelButtons
+          className="upload-back-buttons"
+          onSaveClick={incrementStep}
+          onCancelClick={decrementStep}
+          saveButtonLabel={t("Settings:UploadToServer")}
+          cancelButtonLabel={t("Common:Back")}
+          displaySettings
+          saveButtonDisabled={!isSaveDisabled}
+          isSaveButtonDisabled
+        />
+      )}
+
+      {cancelUploadDialogVisible && (
+        <CancelUploadDialog
+          visible={cancelUploadDialogVisible}
+          loading={isFileLoading}
+          onClose={() => setCancelUploadDialogVisible(false)}
+        />
+      )}
     </Wrapper>
   );
 };
 
-export default FirstStep;
+export default inject(({ dialogsStore, importAccountsStore }) => {
+  const {
+    initMigrationName,
+    localFileUploading,
+    getMigrationStatus,
+    setUsers,
+    isFileLoading,
+    setIsFileLoading,
+  } = importAccountsStore;
+  const { cancelUploadDialogVisible, setCancelUploadDialogVisible } = dialogsStore;
+
+  return {
+    setUsers,
+    localFileUploading,
+    getMigrationStatus,
+    initMigrationName,
+    cancelUploadDialogVisible,
+    setCancelUploadDialogVisible,
+    isFileLoading,
+    setIsFileLoading,
+  };
+})(observer(FirstStep));

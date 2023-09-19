b8ca2852e290bc57c9f8cda016654c102f03c4ff
mushka-n <nikitamushka@mail.ru>
2023-09-19 12:51:36 +0300

    deleted unused logs

--- packages/common/api/oformClient.js
+++ packages/common/api/oformClient.js
@@ -2,14 +2,6 @@ import AxiosOformClient from "../utils/axiosOformClient";
 
 const oformClient = new AxiosOformClient();
 
-// export const initSSR = (headers) => {
-//     oformClient.initSSR(headers);
-// };
-
 export const request = (options) => {
   return oformClient.request(options);
 };
-
-// export const setWithCredentialsStatus = (state) => {
-//   return oformClient.setWithCredentialsStatus(state);
-// };

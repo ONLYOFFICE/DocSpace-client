b868e18a0b637ae5addbc24e618c5a497e674737
mushka-n <nikitamushka@mail.ru>
2023-09-23 00:41:32 +0300

    updated gallery tile component

--- packages/client/src/pages/FormGallery/TilesView/sub-components/Tile.js
+++ packages/client/src/pages/FormGallery/TilesView/sub-components/Tile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useRef } from "react";
 import { inject, observer } from "mobx-react";
 import ContextMenuButton from "@docspace/components/context-menu-button";
 import PropTypes from "prop-types";
@@ -20,132 +20,61 @@ import {
 } from "../StyledTileView";
 import { getCategoryUrl } from "SRC_DIR/helpers/utils";
 
-const Tile = (props) => {
-  const [errorLoadSrc, setErrorLoadSrc] = React.useState(false);
+const Tile = ({
+  t,
+  thumbnailClick,
+  item,
+  getFormContextOptions,
 
-  const cm = React.useRef();
-  const tile = React.useRef();
-
-  const {
-    t,
-    thumbnailClick,
-    item,
-
-    setIsInfoPanelVisible,
-    categoryType,
-    isInfoPanelVisible,
-    setGallerySelected,
-    children,
-    contextButtonSpacerWidth,
-    tileContextClick,
-    isActive,
-    isSelected,
-    title,
-    showHotkeyBorder,
-    getIcon,
-  } = props;
+  setIsInfoPanelVisible,
+  categoryType,
+  isInfoPanelVisible,
+  setGallerySelected,
+  children,
+  contextButtonSpacerWidth,
+  tileContextClick,
+  isActive,
+  isSelected,
+  title,
+  showHotkeyBorder,
+  getIcon,
+}) => {
+  const cm = useRef();
+  const tile = useRef();
 
   const params = useParams();
   const navigate = useNavigate();
 
-  const onError = () => {
-    setErrorLoadSrc(true);
-  };
+  const previewSrc = item?.attributes.card_prewiew.data?.attributes.url;
+  const previewLoader = () => <div style={{ width: "96px" }} />;
 
-  const getIconFile = () => {
-    // const src =
-    //   item.attributes.template_image.data.attributes.formats.small.url;
-    const src = item?.attributes.card_prewiew.data?.attributes.url;
-    const svgLoader = () => <div style={{ width: "96px" }} />;
+  const formContextOptions = getFormContextOptions(
+    t,
+    item,
+    categoryType,
+    params,
+    navigate
+  );
 
-    return src ? (
-      <Link
-        className="thumbnail-image-link"
-        type="page"
-        onClick={thumbnailClick}
-      >
-        <img
-          src={src}
-          className="thumbnail-image"
-          alt="Thumbnail-img"
-          onError={onError}
-        />
-      </Link>
-    ) : (
-      <ReactSVG className="temporary-icon" src={src} loading={svgLoader} />
-    );
-  };
+  const onSelectForm = () => setGallerySelected(item);
 
-  const getContextModel = () => {
-    return [
-      {
-        key: "create",
-        label: t("Common:Create"),
-        onClick: onCreateForm,
-      },
-      {
-        key: "preview",
-        label: t("Common:Preview"),
-        onClick: () => {},
-      },
-      {
-        key: "template-info",
-        label: t("TemplateInfo"),
-        onClick: onShowTemplateInfo,
-      },
-      {
-        key: "separator",
-        isSeparator: true,
-      },
-      {
-        key: "suggest-changes",
-        label: t("FormGallery:SuggestChanges"),
-        onClick: () => {},
-      },
-    ];
-  };
+  const getContextModel = () => formContextOptions;
 
   const onCreateForm = () => {
-    const filter = FilesFilter.getDefault();
-
-    filter.folder = params.folderId;
-
-    const filterParamsStr = filter.toUrlParams();
-
-    const url = getCategoryUrl(categoryType, filter.folder);
-
-    const pathname = `${url}?${filterParamsStr}`;
-
-    setIsInfoPanelVisible(false);
-
-    navigate(
-      combineUrl(window.DocSpaceConfig?.proxy?.url, config.homepage, pathname)
+    const [createFormOption] = formContextOptions.filter(
+      (contextOption) => contextOption.key === "create"
     );
+    createFormOption?.onClick && createFormOption.onClick();
   };
 
-  const onShowTemplateInfo = () => {
-    onSelectForm();
-    if (!isInfoPanelVisible) setIsInfoPanelVisible(true);
-  };
-
-  const getOptions = () => ["create", "template-info"];
-
-  const onSelectForm = () => {
-    setGallerySelected(item);
-  };
-
-  const src = getIcon(32, ".docxf");
-  const element = <img className="react-svg-icon" src={src} />;
+  const getOptions = () =>
+    formContextOptions.map((contextOption) => contextOption.key);
 
   const onContextMenu = (e) => {
     tileContextClick && tileContextClick();
-    if (!cm.current.menuRef.current) {
-      tile.current.click(e); //TODO: need fix context menu to global
-    }
+    if (!cm.current.menuRef.current) tile.current.click(e); //TODO: need fix context menu to global
     cm.current.show(e);
   };
-
-  const icon = getIconFile();
 
   //TODO: OFORM isActive
 
@@ -160,11 +89,33 @@ const Tile = (props) => {
       onClick={onSelectForm}
       className="files-item"
     >
-      <StyledFileTileTop isActive={isActive}>{icon}</StyledFileTileTop>
+      <StyledFileTileTop isActive={isActive}>
+        {previewSrc ? (
+          <Link
+            className="thumbnail-image-link"
+            type="page"
+            onClick={thumbnailClick}
+          >
+            <img
+              src={previewSrc}
+              className="thumbnail-image"
+              alt="Thumbnail-img"
+            />
+          </Link>
+        ) : (
+          <ReactSVG
+            className="temporary-icon"
+            src={previewSrc}
+            loading={previewLoader}
+          />
+        )}
+      </StyledFileTileTop>
 
       <StyledFileTileBottom isSelected={isSelected} isActive={isActive}>
         <div className="file-icon_container">
-          <div className="file-icon">{element}</div>
+          <div className="file-icon">
+            <img className="react-svg-icon" src={getIcon(32, ".docxf")} />
+          </div>
         </div>
 
         <StyledContent>{children}</StyledContent>
@@ -177,7 +128,6 @@ const Tile = (props) => {
             onClick={onContextMenu}
             title={title}
           />
-
           <ContextMenu
             getContextModel={getContextModel}
             ref={cm}
@@ -210,7 +160,8 @@ Tile.defaultProps = {
 export default inject(
   ({ filesStore, settingsStore, auth, oformsStore }, { item }) => {
     const { categoryType } = filesStore;
-    const { gallerySelected, setGallerySelected } = oformsStore;
+    const { gallerySelected, setGallerySelected, getFormContextOptions } =
+      oformsStore;
     const { getIcon } = settingsStore;
     const { isVisible, setIsVisible } = auth.infoPanelStore;
 
@@ -219,6 +170,7 @@ export default inject(
     return {
       isSelected,
       setGallerySelected,
+      getFormContextOptions,
       getIcon,
       setIsInfoPanelVisible: setIsVisible,
       isInfoPanelVisible: isVisible,

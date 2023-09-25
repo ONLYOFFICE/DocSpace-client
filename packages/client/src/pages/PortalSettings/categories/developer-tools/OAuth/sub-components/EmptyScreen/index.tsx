dc1274fb88992ac3a8884818586e368bc25c2aaa
Timofey Boyko <timofey.boyko@onlyoffice.com>
2023-09-25 17:03:25 +0300

    Web:Client:PortalSettings: add OAuth empty page

---
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/OAuth/sub-components/EmptyScreen/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// @ts-ignore
+import EmptyScreenContainer from "@docspace/components/empty-screen-container";
+
+import EmptyScreenOauthSvgUrl from "PUBLIC_DIR/images/empty_screen_oauth.svg?url";
+
+import RegisterNewButton from "../RegisterNewButton";
+
+import { EmptyScreenProps } from "./EmptyScreen.types";
+const OAuthEmptyScreen = ({ t }: EmptyScreenProps) => {
+  return (
+    <EmptyScreenContainer
+      imageSrc={EmptyScreenOauthSvgUrl}
+      imageAlt={"Empty oauth list"}
+      headerText={t("NoOAuthAppHeader")}
+      subheadingText={t("NoOAuthAppDescription")}
+      buttons={<RegisterNewButton t={t} />}
+    />
+  );
+};
+
+export default OAuthEmptyScreen;

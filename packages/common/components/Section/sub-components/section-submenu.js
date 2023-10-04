3cb1f7d163228172d9028f72fc57cc50cb3be0a7
Viktor Fomin <enotcode@gmail.com>
2023-10-04 18:27:22 +0300

    Common: Section: Submenu: fix style

--- packages/common/components/Section/sub-components/section-submenu.js
+++ packages/common/components/Section/sub-components/section-submenu.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+import { tablet, hugeMobile } from "@docspace/components/utils/device";
+
+const Wrapper = styled.div`
+  width: calc(100% - 20px);
+
+  @media ${tablet} {
+    width: calc(100% - 16px);
+  }
+
+  @media ${hugeMobile} {
+    width: 100%;
+  }
+`;
 
 const SectionSubmenu = ({ children }) => {
-  return <div>{children}</div>;
+  return <Wrapper>{children}</Wrapper>;
 };
 
 SectionSubmenu.displayName = "SectionSubmenu";

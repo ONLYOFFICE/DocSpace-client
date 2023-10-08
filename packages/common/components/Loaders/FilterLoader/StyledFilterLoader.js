faa092f386d168c3b7fe53241b091160ee1e78f1
Timofey Boyko <timofey.boyko@onlyoffice.com>
2023-10-08 07:48:55 +0300

    Web:Common: apply new breakpoints

--- packages/common/components/Loaders/FilterLoader/StyledFilterLoader.js
+++ packages/common/components/Loaders/FilterLoader/StyledFilterLoader.js
@@ -1,6 +1,6 @@
 import styled, { css } from "styled-components";
-import { isMobile } from "react-device-detect";
-import { mobile, tablet } from "@docspace/components/utils/device";
+
+import { mobile } from "@docspace/components/utils/device";
 
 const StyledFilter = styled.div`
   width: 100%;
@@ -8,11 +8,6 @@ const StyledFilter = styled.div`
   grid-template-columns: 1fr 95px;
   grid-template-rows: 1fr;
   grid-column-gap: 8px;
-
-  /* ${isMobile &&
-  css`
-    margin-top: -22px;
-  `} */
 
   @media ${mobile} {
     grid-template-columns: 1fr 50px;

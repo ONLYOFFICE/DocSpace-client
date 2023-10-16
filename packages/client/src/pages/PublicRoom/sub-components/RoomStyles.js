3cc5aa46d83be1242b4b59cec74bf0a8c8a7d2ad
gopienkonikita <nikita.gopienko@onlyoffice.com>
2023-10-16 12:48:37 +0300

    Web: Files: PublicRoom: fixed crash

--- packages/client/src/pages/PublicRoom/sub-components/RoomStyles.js
+++ packages/client/src/pages/PublicRoom/sub-components/RoomStyles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { mobile, tablet } from "@docspace/components/utils/device";
 
 export const StyledPage = styled.div`

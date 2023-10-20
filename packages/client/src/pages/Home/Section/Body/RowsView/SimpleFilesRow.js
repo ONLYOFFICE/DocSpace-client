151345f222cd89670c201db6212465e02aa9e393
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2023-10-20 13:14:02 +0500

    Web:Badges Fixed margin

--- packages/client/src/pages/Home/Section/Body/RowsView/SimpleFilesRow.js
+++ packages/client/src/pages/Home/Section/Body/RowsView/SimpleFilesRow.js
@@ -259,10 +259,19 @@ const StyledSimpleFilesRow = styled(Row)`
             `}
     }
 
-    .file__badges {
+    .file__badges,
+    .room__badges,
+    .folder__badges {
       > div {
         margin-left: 0;
         margin-right: 0;
+      }
+    }
+
+    .room__badges,
+    .folder__badges {
+      > div {
+        margin-top: 3px;
       }
     }
 
@@ -303,6 +312,13 @@ const StyledSimpleFilesRow = styled(Row)`
       margin-top: 0px;
       align-items: center;
       height: 100%;
+    }
+
+    .room__badges,
+    .folder__badges {
+      > div {
+        margin-top: 0px;
+      }
     }
   }
 `;

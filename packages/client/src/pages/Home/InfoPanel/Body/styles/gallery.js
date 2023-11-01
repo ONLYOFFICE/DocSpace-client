2b143fadae89bc450d6d1a0aa95c35d55da90ea1
mushka-n <nikitamushka@mail.ru>
2023-11-01 16:18:22 +0300

    added margin for StyledGalleryNoThumbnail

--- packages/client/src/pages/Home/InfoPanel/Body/styles/gallery.js
+++ packages/client/src/pages/Home/InfoPanel/Body/styles/gallery.js
@@ -23,6 +23,7 @@ const StyledGalleryNoThumbnail = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
+  margin-bottom: 24px;
   .no-thumbnail-img {
     height: 96px;
     width: 96px;

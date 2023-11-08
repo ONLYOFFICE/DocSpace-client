5a2f226322e491c306c98e3ff74bab4a6d9b0f99
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2023-11-08 18:28:54 +0500

    Management: added functionality to the delete dialog

--- packages/management/src/categories/spaces/sub-components/dialogs/DeletePortalDialog/index.tsx
+++ packages/management/src/categories/spaces/sub-components/dialogs/DeletePortalDialog/index.tsx
@@ -6,53 +6,54 @@ import Button from "@docspace/components/button";
 import { useTranslation, Trans } from "react-i18next";
 import { observer } from "mobx-react";
 import ModalDialog from "@docspace/components/modal-dialog";
+import tryRedirectTo from "@docspace/common/utils/tryRedirectTo";
 import { useStore } from "SRC_DIR/store";
 import Link from "@docspace/components/link";
 
 
 const DeletePortalDialog = () => {
-    const { spacesStore } = useStore();
+    const { spacesStore, authStore } = useStore();
+    const { currentColorScheme } =
+    authStore.settingsStore;
+    const {logout} = authStore;
+
+
+    const { user } = authStore.userStore
 
     const {
+        currentPortal,
         deletePortalDialogVisible: visible,
         setDeletePortalDialogVisible,
       } = spacesStore;
 
       const { t } = useTranslation(["Management", "Common"]);  
 
-    const onClose = () => setDeletePortalDialogVisible(false);
+      const {owner, domain} = currentPortal;
+      const {displayName, email, id} = owner;  
 
 
-    const address= "portal.docspace.site";
-    const ownerName= "Dmitry Sychugov";
-    const email = "Daimends1@onlyoffice.com";
-    
+      const onClose = () => setDeletePortalDialogVisible(false);
+
+
+     
+      const onDelete = () => {
+        const protocol = window?.location?.protocol;
+        if (id === user.id) {
+          return window.open(`${protocol}//${domain}/portal-settings/delete-data/deletion`, "_self");
+        }
+       logout().then(() => window.open(`${protocol}//${domain}`, "_self"));
+    }
 
     return (
         <ModalDialogContainer 
-            isLarge
             visible={visible}
             onClose={onClose}
             displayType="modal"
         >
             <ModalDialog.Header>{t("Common:Warning")}</ModalDialog.Header>
             <ModalDialog.Body className="">
-            <Trans i18nKey="DeletePortalText" t={t} address="portal.docspace.site" ownerName="Dmitry Sychugov" email="Daimends1@onlyoffice.com">
-                
-                Please note: only the owner is able to delete the selected DocSpace. The owner of <strong>{{address}}</strong> is <strong>{{ownerName}}</strong>
-                
-                <Link
-                    className="email-link"
-                    type="page"
-                    href={`mailto:${email}`}
-                    noHover
-              //  color={currentColorScheme.main.accent}
-                    title={email}
-              >
-                {{ email }}
-              </Link>.
-              If you are not the owner, you will not be able to access the DocSpace deletion settings by clicking the DELETE button and will be redirected to the Rooms section.
-                
+            <Trans i18nKey="DeletePortalText" t={t} domain={domain} displayName={displayName} email={email}>  
+                Please note: only the owner is able to delete the selected DocSpace. The owner of <strong>{{domain}}</strong> is <strong>{{displayName}}</strong> <Link className="email-link" type="page" href={`mailto:${email}`} noHover color={currentColorScheme?.main?.accent} title={email}>{{ email }} </Link>. If you are not the owner, you will not be able to access the DocSpace deletion settings by clicking the DELETE button and will be redirected to the Rooms section.   
             </Trans>
             </ModalDialog.Body>
             <ModalDialog.Footer>
@@ -62,7 +63,7 @@ const DeletePortalDialog = () => {
           size="normal"
           scale
           primary
-       //   onClick={onHandleClick}
+          onClick={onDelete}
         />
         <Button
           key="CancelButton"

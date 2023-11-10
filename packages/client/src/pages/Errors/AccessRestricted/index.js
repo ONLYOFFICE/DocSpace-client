14ba266d5f6d1ec38878720ddd5b260b788ada4d
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2023-11-10 13:25:02 +0300

    Web: Deleted useless code.

--- packages/client/src/pages/Errors/AccessRestricted/index.js
+++ packages/client/src/pages/Errors/AccessRestricted/index.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import ErrorContainer from "@docspace/common/components/ErrorContainer";
 import { I18nextProvider, useTranslation } from "react-i18next";
 import i18n from "./i18n";
-import DocspaceLogo from "../../../DocspaceLogo";
 
 const StyledWrapper = styled.div`
   width: 100%;
@@ -19,7 +18,6 @@ const AccessRestricted = () => {
 
   return ready ? (
     <StyledWrapper>
-      <DocspaceLogo />
       <ErrorContainer
         headerText={t("AccessDenied")}
         bodyText={t("PortalRestriction")}

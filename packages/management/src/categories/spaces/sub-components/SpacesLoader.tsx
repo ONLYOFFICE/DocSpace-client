d54958e2d294a766fff6eea648f74cc49ac5fdac
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2023-11-21 14:30:18 +0400

    Fix loader

--- packages/management/src/categories/spaces/sub-components/SpacesLoader.tsx
+++ packages/management/src/categories/spaces/sub-components/SpacesLoader.tsx
@@ -1,32 +1,34 @@
 import React from "react";
-import Loaders from "@docspace/common/components/Loaders";
-import {StyledLoader} from "../StyledSpaces";
+import RectangleSkeleton from "@docspace/components/skeletons/rectangle";
+import { StyledLoader } from "../StyledSpaces";
 
 type TSpacesLoader = {
   isConfigurationSection: boolean;
-}
+};
 
-export const SpacesLoader = ({isConfigurationSection}: TSpacesLoader): JSX.Element => {
+export const SpacesLoader = ({
+  isConfigurationSection,
+}: TSpacesLoader): JSX.Element => {
   return (
     <StyledLoader>
-      <Loaders.Rectangle height="22px" className="subheader" />
+      <RectangleSkeleton height="22px" className="subheader" />
       {isConfigurationSection ? (
         <>
-          <Loaders.Rectangle height="22px" className="configuration-header" />
-          <Loaders.Rectangle height="48px" className="section-title" />
-          <Loaders.Rectangle height="52px" className="configuration-input" />
-          <Loaders.Rectangle height="52px" className="configuration-input" />
+          <RectangleSkeleton height="22px" className="configuration-header" />
+          <RectangleSkeleton height="48px" className="section-title" />
+          <RectangleSkeleton height="52px" className="configuration-input" />
+          <RectangleSkeleton height="52px" className="configuration-input" />
         </>
       ) : (
         <>
-          <Loaders.Rectangle height="23px" className="button" />
-          <Loaders.Rectangle height="160px" className="portals" />
-          <Loaders.Rectangle height="22px" className="domain-header" />
-          <Loaders.Rectangle height="52px" className="input" />
+          <RectangleSkeleton height="23px" className="button" />
+          <RectangleSkeleton height="160px" className="portals" />
+          <RectangleSkeleton height="22px" className="domain-header" />
+          <RectangleSkeleton height="52px" className="input" />
         </>
       )}
 
-      <Loaders.Rectangle height="22px" className="button" />
+      <RectangleSkeleton height="22px" className="button" />
     </StyledLoader>
   );
 };

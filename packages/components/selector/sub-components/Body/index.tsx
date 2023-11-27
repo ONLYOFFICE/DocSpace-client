8b7443204aede5792708251510425f05353b953e
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2023-11-27 13:23:06 +0500

    Web:Components:Selector Removed logger

--- packages/components/selector/sub-components/Body/index.tsx
+++ packages/components/selector/sub-components/Body/index.tsx
@@ -74,8 +74,6 @@ const Body = ({
 
   const itemsCount = hasNextPage ? items.length + 1 : items.length;
 
-  console.log({ itemsCount, isLoading });
-
   const resetCache = React.useCallback(() => {
     if (listOptionsRef && listOptionsRef.current) {
       listOptionsRef.current.resetloadMoreItemsCache(true);

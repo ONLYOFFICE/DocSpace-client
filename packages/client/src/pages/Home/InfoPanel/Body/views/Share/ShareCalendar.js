276eb1b26aa86d02a27837c11ccd1a2e107a1542
Viktor Fomin <viktor.fomin@onlyoffice.com>
2023-11-29 03:58:14 +0300

    Client: InfoPanel: Share: add calendar

---
+++ packages/client/src/pages/Home/InfoPanel/Body/views/Share/ShareCalendar.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import moment from "moment";
+
+import Calendar from "@docspace/components/calendar";
+import { isMobile } from "@docspace/components/utils/device";
+
+const StyledCalendar = styled(Calendar)`
+  position: absolute;
+  ${(props) =>
+    props.isMobile &&
+    css`
+      position: fixed;
+      bottom: 0;
+      inset-inline-start: 0;
+    `}
+`;
+
+const ShareCalendar = ({ onDateSet, closeCalendar, calendarRef, locale }) => {
+  const selectedDate = moment();
+
+  return (
+    <StyledCalendar
+      selectedDate={selectedDate}
+      setSelectedDate={onDateSet}
+      onChange={closeCalendar}
+      isMobile={isMobile()}
+      forwardedRef={calendarRef}
+      locale={locale}
+      minDate={selectedDate}
+    />
+  );
+};
+
+export default ShareCalendar;

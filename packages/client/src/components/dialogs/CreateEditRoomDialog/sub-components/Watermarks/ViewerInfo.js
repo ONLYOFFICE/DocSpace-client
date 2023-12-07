8cb87da1d5795f4e58f90692b9c9f90292c9eb42
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2023-12-07 17:05:18 +0300

    PortalSettings: Watermark: Added viewer info component.

---
+++ packages/client/src/components/dialogs/CreateEditRoomDialog/sub-components/Watermarks/ViewerInfo.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import TabContainer from "@docspace/components/tabs-container";
+import TextInput from "@docspace/components/text-input";
+import Text from "@docspace/components/text";
+import Checkbox from "@docspace/components/checkbox";
+
+import { StyledWatermark } from "./StyledComponent";
+
+const options = (t) => [
+  {
+    key: "userName",
+    title: t("UserName"),
+  },
+  {
+    key: "userEmail",
+    title: t("UserEmail"),
+  },
+  {
+    key: "userIPAddress",
+    title: t("UserIPAddress"),
+  },
+  {
+    key: "currentDate",
+    title: t("Common:CurrentDate"),
+  },
+  {
+    key: "RoomName",
+    title: t("Common:RoomName"),
+  },
+];
+const ViewerInfoWatermark = () => {
+  const { t } = useTranslation(["CreateEditRoomDialog", "Common"]);
+
+  const [isChecked, setIsChecked] = useState(true);
+  const [elements, setElements] = useState({
+    userName: false,
+    userEmail: false,
+    userIP: false,
+    currentDate: false,
+    roomName: false,
+  });
+
+  const dataTabs = options(t);
+
+  const onSelect = (item) => {
+    const updatedElem = elements[item.key];
+    const key = item.key;
+    setElements({ ...elements, [key]: !updatedElem });
+  };
+  const onCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <StyledWatermark>
+      <div>
+        <Text className="watermark-title" fontWeight={600} lineHeight="20px">
+          {t("WatermarkElements")}
+        </Text>
+        <TabContainer
+          elements={dataTabs}
+          onSelect={onSelect}
+          multiple
+          withBodyScroll={false}
+        />
+        <Text className="watermark-title" fontWeight={600} lineHeight="20px">
+          {t("Position")}
+        </Text>
+        <TextInput scale value={t("Center")} isReadOnly />
+        <Checkbox
+          className="watermark-checkbox"
+          label={t("Semitransparent")}
+          onChange={onCheckboxChange}
+          isChecked={isChecked}
+        />
+      </div>
+      <div style={{ width: "100px", height: "140px" }}></div>
+    </StyledWatermark>
+  );
+};
+export default ViewerInfoWatermark;

fdb088bd4d29871abd783692b8c33919a9ab264d
Timofey Boyko <timofey.boyko@onlyoffice.com>
2023-12-20 13:18:39 +0300

    Delete useless components

--- packages/components/email-chips/svg/index.ts
+++
@@ -1,4 +0,0 @@
-// @ts-expect-error TS(2307): Cannot find module 'PUBLIC_DIR/images/Warning.svg?... Remove this comment to see the full error message
-export { default as WarningIconSvgUrl } from "PUBLIC_DIR/images/Warning.svg?url";
-// @ts-expect-error TS(2307): Cannot find module 'PUBLIC_DIR/images/Delete.svg?u... Remove this comment to see the full error message
-export { default as DeleteIconSvgUrl } from "PUBLIC_DIR/images/Delete.svg?url";

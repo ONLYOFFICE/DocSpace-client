f850d4d065b33af1c27ad229c7cfe5759732ab0a
Timofey Boyko <timofey.boyko@onlyoffice.com>
2023-12-21 16:17:42 +0300

    Shared:Components:TopLoadingIndicator: rewrite to typescript

--- packages/shared/components/top-loading-indicator/index.ts
+++ packages/shared/components/top-loading-indicator/index.ts
@@ -1,15 +1,28 @@
 const intervalTimeout = 10;
 
 const MAX = 100;
-let timerId: any;
+let timerId: ReturnType<typeof setTimeout> | null;
 let width = 0;
 let percentage = 0;
-let increasePercentage = 0.75;
-let moreIncreasePercentage = 3;
+const increasePercentage = 0.75;
+const moreIncreasePercentage = 3;
 
 let elem =
   typeof document !== "undefined" &&
   document.getElementById("ipl-progress-indicator");
+
+const animatingWidth = () => {
+  if (width >= MAX) {
+    if (timerId) clearTimeout(timerId);
+    timerId = null;
+    if (elem) elem.style.width = "0px";
+    width = 0;
+    return;
+  }
+
+  width += percentage !== MAX ? increasePercentage : moreIncreasePercentage;
+  if (elem) elem.style.width = `${width}%`;
+};
 
 const startInterval = () => {
   if (timerId) return;
@@ -17,21 +30,6 @@ const startInterval = () => {
   if (!elem) elem = document.getElementById("ipl-progress-indicator");
 
   timerId = setInterval(animatingWidth, intervalTimeout);
-};
-
-const animatingWidth = () => {
-  if (width >= MAX) {
-    clearTimeout(timerId);
-    timerId = null;
-    // @ts-expect-error TS(2531): Object is possibly 'null'.
-    elem.style.width = 0;
-    width = 0;
-    return;
-  }
-
-  width += percentage !== MAX ? increasePercentage : moreIncreasePercentage;
-  // @ts-expect-error TS(2531): Object is possibly 'null'.
-  elem.style.width = width + "%";
 };
 
 export default class TopLoaderService {

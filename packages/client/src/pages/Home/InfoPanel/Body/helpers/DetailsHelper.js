b0a8ab7b29ac605522079109419c99e1137d1827
Alexey Safronov <alexey.safronov@onlyoffice.com>
2023-12-25 16:22:07 +0400

    Refactoring: merged several methods to getAuthorDecoration

--- packages/client/src/pages/Home/InfoPanel/Body/helpers/DetailsHelper.js
+++ packages/client/src/pages/Home/InfoPanel/Body/helpers/DetailsHelper.js
@@ -195,7 +195,7 @@ class DetailsHelper {
   getPropertyContent = (propertyId) => {
     switch (propertyId) {
       case "Owner":
-        return this.getItemOwner();
+        return this.getAuthorDecoration("createdBy");
       case "Location":
         return this.getItemLocation();
 
@@ -217,7 +217,7 @@ class DetailsHelper {
       case "Date modified":
         return this.getItemDateModified();
       case "Last modified by":
-        return this.getItemLastModifiedBy();
+        return this.getAuthorDecoration("updatedBy");
       case "Creation date":
         return this.getItemCreationDate();
 
@@ -232,21 +232,17 @@ class DetailsHelper {
 
   /// Property  //
 
-  getItemOwner = () => {
-    const onOpenUser = () => this.openUser(this.item.createdBy, this.navigate);
+  getAuthorDecoration = (byField = "createdBy") => {
+    const onClick = () => this.openUser(this.item[byField], this.navigate);
+
+    const displayName = this.item[byField]?.displayName;
+    const name = displayName ? decode(displayName) : "";
+
+    //console.log("getAuthorDecoration", { name, displayName });
 
     return this.personal || this.isVisitor || this.isCollaborator
-      ? text(
-          this.item.createdBy?.displayName
-            ? decode(this.item.createdBy?.displayName)
-            : ""
-        )
-      : link(
-          this.item.createdBy?.displayName
-            ? decode(this.item.createdBy?.displayName)
-            : "",
-          onOpenUser
-        );
+      ? text(name)
+      : link(name, onClick);
   };
 
   getItemLocation = () => {
@@ -291,23 +287,6 @@ class DetailsHelper {
     return text(
       parseAndFormatDate(this.item.updated, this.personal, this.culture)
     );
-  };
-
-  getItemLastModifiedBy = () => {
-    const onOpenUser = () => this.openUser(this.item.updatedBy, this.navigate);
-
-    return this.personal || this.isVisitor || this.isCollaborator
-      ? text(
-          this.item.updatedBy?.displayName
-            ? decode(this.item.updatedBy?.displayName)
-            : ""
-        )
-      : link(
-          this.item.updatedBy?.displayName
-            ? decode(this.item.updatedBy?.displayName)
-            : "",
-          onOpenUser
-        );
   };
 
   getItemCreationDate = () => {

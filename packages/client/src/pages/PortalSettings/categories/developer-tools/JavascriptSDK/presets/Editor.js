2f7b05d4eac82087a74d42b9bacffed99ad4ae75
Vladimir Khvan <vladimir.hvan@onlyoffice.com>
2023-12-26 13:42:44 +0500

    Web: Client: JavascriptSDK: editor was complemented

--- packages/client/src/pages/PortalSettings/categories/developer-tools/JavascriptSDK/presets/Editor.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/JavascriptSDK/presets/Editor.js
@@ -12,10 +12,9 @@ import TabContainer from "@docspace/components/tabs-container";
 import FilesSelectorInput from "SRC_DIR/components/FilesSelectorInput";
 import { objectToGetParams, loadScript } from "@docspace/common/utils";
 import { inject, observer } from "mobx-react";
+import { FilesSelectorFilterTypes } from "@docspace/common/constants";
 
 import RectangleSkeleton from "@docspace/components/skeletons/rectangle";
-import HelpButton from "@docspace/components/help-button";
-import Link from "@docspace/components/link";
 
 import GetCodeDialog from "../sub-components/GetCodeDialog";
 import Button from "@docspace/components/button";
@@ -34,57 +33,42 @@ import {
   Frame,
   Container,
   RowContainer,
-  ColumnContainer,
   Preview,
   GetCodeButtonWrapper,
   FilesSelectorInputWrapper,
 } from "./StyledPresets";
 
 const Editor = (props) => {
-  const { t, setDocumentTitle, currentColorScheme, sdkLink } = props;
+  const { t, setDocumentTitle } = props;
 
   setDocumentTitle(t("JavascriptSdk"));
 
   const scriptUrl = `${window.location.origin}/static/scripts/api.js`;
-
-  const dataSortBy = [
-    { key: "DateAndTime", label: t("Common:LastModifiedDate"), default: true },
-    { key: "AZ", label: t("Common:Title") },
-    { key: "Type", label: t("Common:Type") },
-    { key: "Size", label: t("Common:Size") },
-    { key: "DateAndTimeCreation", label: t("Files:ByCreation") },
-    { key: "Author", label: t("Files:ByAuthor") },
-  ];
-
-  const dataSortOrder = [
-    { key: "descending", label: t("Descending"), default: true },
-    { key: "ascending", label: t("Ascending") },
-  ];
 
   const dataDimensions = [
     { key: "percent", label: "%", default: true },
     { key: "pixel", label: "px" },
   ];
 
-  const [sortBy, setSortBy] = useState(dataSortBy[0]);
-  const [sortOrder, setSortOrder] = useState(dataSortOrder[0]);
   const [widthDimension, setWidthDimension] = useState(dataDimensions[0]);
   const [heightDimension, setHeightDimension] = useState(dataDimensions[1]);
   const [width, setWidth] = useState("100");
   const [height, setHeight] = useState("600");
-  const [withSubfolders, setWithSubfolders] = useState(true);
   const [isGetCodeDialogOpened, setIsGetCodeDialogOpened] = useState(false);
   const [showPreview, setShowPreview] = useState(window.innerWidth > showPreviewThreshold);
 
   const [config, setConfig] = useState({
+    mode: "editor",
     width: `${width}${widthDimension.label}`,
     height: `${height}${heightDimension.label}`,
     frameId: "ds-frame",
-    showHeader: true,
-    showTitle: true,
-    showMenu: true,
-    showFilter: true,
     init: true,
+    editorType: "desktop",
+    // id: {
+    //   id: 4,
+    //   title: "ONLYOFFICE Sample Presentation.pptx",
+    //   fileExst: ".pptx",
+    // },
   });
 
   const params = objectToGetParams(config);
@@ -132,7 +116,7 @@ const Editor = (props) => {
     setHeight(e.target.value);
   };
 
-  const onChangeFolderId = (id) => {
+  const onChangeFileId = (id) => {
     setConfig((config) => {
       return { ...config, id };
     });
@@ -142,30 +126,6 @@ const Editor = (props) => {
     setConfig((config) => {
       return { ...config, frameId: e.target.value };
     });
-  };
-
-  const onChangeWithSubfolders = (e) => {
-    setConfig((config) => {
-      return { ...config, withSubfolders: !withSubfolders };
-    });
-
-    setWithSubfolders(!withSubfolders);
-  };
-
-  const onChangeSortBy = (item) => {
-    setConfig((config) => {
-      return { ...config, sortby: item.key };
-    });
-
-    setSortBy(item);
-  };
-
-  const onChangeSortOrder = (item) => {
-    setConfig((config) => {
-      return { ...config, sortorder: item.key };
-    });
-
-    setSortOrder(item);
   };
 
   const onChangeWidthDimension = (item) => {
@@ -182,48 +142,6 @@ const Editor = (props) => {
     });
 
     setHeightDimension(item);
-  };
-
-  const onChangeShowHeader = (e) => {
-    setConfig((config) => {
-      return { ...config, showHeader: !config.showHeader };
-    });
-  };
-
-  const onChangeShowTitle = () => {
-    setConfig((config) => {
-      return { ...config, showTitle: !config.showTitle };
-    });
-  };
-
-  const onChangeShowMenu = (e) => {
-    setConfig((config) => {
-      return { ...config, showMenu: !config.showMenu };
-    });
-  };
-
-  const onChangeShowFilter = (e) => {
-    setConfig((config) => {
-      return { ...config, showFilter: !config.showFilter };
-    });
-  };
-
-  const onChangeCount = (e) => {
-    setConfig((config) => {
-      return { ...config, count: e.target.value };
-    });
-  };
-
-  const onChangePage = (e) => {
-    setConfig((config) => {
-      return { ...config, page: e.target.value };
-    });
-  };
-
-  const onChangeSearch = (e) => {
-    setConfig((config) => {
-      return { ...config, search: e.target.value };
-    });
   };
 
   const openGetCodeModal = () => setIsGetCodeDialogOpened(true);
@@ -274,14 +192,7 @@ const Editor = (props) => {
   return (
     <SDKContainer>
       <CategoryDescription>
-        <Text className="sdk-description">{t("SDKDescription")}</Text>
-        <Link
-          color={currentColorScheme?.main?.accent}
-          fontSize="12px"
-          fontWeight="400"
-          onClick={() => window.open(sdkLink, "_blank")}>
-          {t("APILink")}.
-        </Link>
+        <Text className="sdk-description">{t("EditorDescription")}</Text>
       </CategoryDescription>
       <CategoryHeader>{t("CreateSampleHeader")}</CategoryHeader>
       <Container>
@@ -291,6 +202,19 @@ const Editor = (props) => {
           </Preview>
         )}
         <Controls>
+          <CategorySubHeader>{t("FileId")}</CategorySubHeader>
+          <ControlsGroup>
+            <LabelGroup>
+              <Label className="label" text={t("Common:SelectFile")} />
+            </LabelGroup>
+            <FilesSelectorInputWrapper>
+              <FilesSelectorInput
+                onSelectFile={onChangeFileId}
+                filterParam={FilesSelectorFilterTypes.ALL}
+                isSelect
+              />
+            </FilesSelectorInputWrapper>
+          </ControlsGroup>
           <CategorySubHeader>{t("CustomizingDisplay")}</CategorySubHeader>
           <ControlsGroup>
             <Label className="label" text={t("EmbeddingPanel:Width")} />
@@ -348,114 +272,25 @@ const Editor = (props) => {
             <Label className="label">{t("InterfaceElements")}</Label>
             <Checkbox
               className="checkbox"
-              label={t("Menu")}
-              onChange={onChangeShowMenu}
-              isChecked={config.showMenu}
+              label={t("RightPanelCollapsed")}
+              onChange={() => {}}
+              isChecked={true}
             />
             <Checkbox
               className="checkbox"
-              label={t("Header")}
-              onChange={onChangeShowHeader}
-              isChecked={config.showHeader}
-            />
-            <Checkbox
-              className="checkbox"
-              label={t("Filter")}
-              onChange={onChangeShowFilter}
-              isChecked={config.showFilter}
+              label={t("TabPlugins")}
+              onChange={() => {}}
+              isChecked={true}
             />
             <RowContainer>
-              <Checkbox
-                label={t("Title")}
-                onChange={onChangeShowTitle}
-                isChecked={config.showTitle}
-              />
-              <Text color="gray">{`(${t("MobileOnly")})`}</Text>
+              <Checkbox label={t("Chat")} onChange={() => {}} isChecked={true} />
+              <Text color="gray">({t("InLeftPanel")})</Text>
+            </RowContainer>
+            <RowContainer>
+              <Checkbox label={t("FeedbackAndSupport")} onChange={() => {}} isChecked={true} />
+              <Text color="gray">({t("InLeftPanel")})</Text>
             </RowContainer>
           </InterfaceElements>
-          <CategorySubHeader>{t("DataDisplay")}</CategorySubHeader>
-          <ControlsGroup>
-            <LabelGroup>
-              <Label className="label" text={t("RoomOrFolder")} />
-              <HelpButton
-                offsetRight={0}
-                size={12}
-                tooltipContent={<Text fontSize="12px">{t("RoomOrFolderDescription")}</Text>}
-              />
-            </LabelGroup>
-            <FilesSelectorInputWrapper>
-              <FilesSelectorInput onSelectFolder={onChangeFolderId} isSelect />
-            </FilesSelectorInputWrapper>
-          </ControlsGroup>
-          <CategorySubHeader>{t("AdvancedDisplay")}</CategorySubHeader>
-          <ControlsGroup>
-            <Label className="label" text={t("SearchTerm")} />
-            <ColumnContainer>
-              <TextInput
-                scale={true}
-                onChange={onChangeSearch}
-                placeholder={t("Common:Search")}
-                value={config.search}
-                tabIndex={5}
-              />
-              <Checkbox
-                className="checkbox"
-                label={t("Files:WithSubfolders")}
-                onChange={onChangeWithSubfolders}
-                isChecked={withSubfolders}
-              />
-            </ColumnContainer>
-          </ControlsGroup>
-          <ControlsGroup>
-            <Label className="label" text={t("Common:SortBy")} />
-            <ComboBox
-              onSelect={onChangeSortBy}
-              options={dataSortBy}
-              scaled={true}
-              selectedOption={sortBy}
-              displaySelectedOption
-              directionY="top"
-            />
-          </ControlsGroup>
-          <ControlsGroup>
-            <Label className="label" text={t("SortOrder")} />
-            <ComboBox
-              onSelect={onChangeSortOrder}
-              options={dataSortOrder}
-              scaled={true}
-              selectedOption={sortOrder}
-              displaySelectedOption
-              directionY="top"
-            />
-          </ControlsGroup>
-          <ControlsGroup>
-            <LabelGroup>
-              <Label className="label" text={t("ItemsCount")} />
-              <HelpButton
-                offsetRight={0}
-                size={12}
-                tooltipContent={<Text fontSize="12px">{t("ItemsCountDescription")}</Text>}
-              />
-            </LabelGroup>
-            <TextInput
-              scale={true}
-              onChange={onChangeCount}
-              placeholder={t("EnterCount")}
-              value={config.count}
-              tabIndex={6}
-            />
-          </ControlsGroup>
-          <ControlsGroup>
-            <Label className="label" text={t("Page")} />
-            <TextInput
-              scale={true}
-              onChange={onChangePage}
-              placeholder={t("EnterPage")}
-              value={config.page}
-              isDisabled={!config.count}
-              tabIndex={7}
-            />
-          </ControlsGroup>
         </Controls>
       </Container>
 
@@ -486,12 +321,10 @@ const Editor = (props) => {
 
 export default inject(({ auth }) => {
   const { settingsStore, setDocumentTitle } = auth;
-  const { theme, currentColorScheme, sdkLink } = settingsStore;
+  const { theme } = settingsStore;
 
   return {
     theme,
     setDocumentTitle,
-    currentColorScheme,
-    sdkLink,
   };
 })(withTranslation(["JavascriptSdk", "Files", "EmbeddingPanel", "Common"])(observer(Editor)));

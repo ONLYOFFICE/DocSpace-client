83afb29ed7438be2a7d4e14b9825ed5d78d44c82
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-01-13 00:27:14 +0300

    Common: TariffBar: add logic

--- packages/common/components/TariffBar/index.js
+++ packages/common/components/TariffBar/index.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { inject, observer } from "mobx-react";
+import { useTranslation } from "react-i18next";
 
 import { combineUrl } from "@docspace/common/utils";
-import Text from "@docspace/components/text";
+import { Text } from "@docspace/shared/components/text";
 
 const StyledWrapper = styled.div`
   cursor: pointer;
@@ -13,8 +15,19 @@ const PROXY_BASE_URL = combineUrl(
   "/portal-settings"
 );
 
-const TariffBar = () => {
+const TariffBar = ({
+  isEnterprise,
+  isNonProfit,
+  isGracePeriod,
+  isFreeTariff,
+  isPaymentPageAvailable,
+  isLicenseExpiring,
+  isLicenseDateExpired,
+  isTrial,
+  standalone,
+}) => {
   const navigate = useNavigate();
+  const { t } = useTranslation("Common");
 
   const onClick = () => {
     const paymentPageUrl = combineUrl(
@@ -24,19 +37,78 @@ const TariffBar = () => {
     navigate(paymentPageUrl);
   };
 
+  const getSaasBar = () => {
+    if (
+      isPaymentPageAvailable &&
+      !isNonProfit &&
+      (isFreeTariff || isGracePeriod)
+    ) {
+      if (isFreeTariff)
+        return { label: t("Common:TryBusiness"), color: "#ED7309" };
+      if (isGracePeriod)
+        return { label: t("Common:LatePayment"), color: "#F24724" };
+    }
+  };
+
+  const getEnterpriseBar = () => {
+    if (
+      isPaymentPageAvailable &&
+      isEnterprise &&
+      (isTrial || isLicenseExpiring || isLicenseDateExpired)
+    ) {
+      if (isTrial) {
+        if (isLicenseDateExpired)
+          return { label: t("Common:TrialExpired"), color: "#ED7309" };
+        return {
+          label: t("Common:TrialDaysLeft", { count: trialDaysLeft }),
+          color: "#ED7309",
+        };
+      }
+      return { label: t("Common:SubscriptionExpired"), color: "#F24724" };
+    }
+  };
+
+  const tariffBar = !standalone ? getSaasBar() : getEnterpriseBar();
+
+  if (!tariffBar) return <></>;
   return (
     <StyledWrapper>
       <Text
         fontSize="12px"
         fontWeight={600}
         lineHeight="16px"
-        color="#ED7309"
+        color={tariffBar.color}
         onClick={onClick}
+        truncate
       >
-        Try Business
+        {tariffBar.label}
       </Text>
     </StyledWrapper>
   );
 };
 
-export default TariffBar;
+export default inject(({ auth }) => {
+  const {
+    settingsStore,
+    currentQuotaStore,
+    isPaymentPageAvailable,
+    currentTariffStatusStore,
+    isEnterprise,
+  } = auth;
+  const { isFreeTariff, isNonProfit, isTrial } = currentQuotaStore;
+  const { isGracePeriod, isLicenseExpiring, isLicenseDateExpired } =
+    currentTariffStatusStore;
+  const { standalone } = settingsStore;
+
+  return {
+    isEnterprise,
+    isNonProfit,
+    isGracePeriod,
+    isFreeTariff,
+    isPaymentPageAvailable,
+    isLicenseExpiring,
+    isLicenseDateExpired,
+    isTrial,
+    standalone,
+  };
+})(observer(TariffBar));

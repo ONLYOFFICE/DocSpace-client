4a3edcf831bac914c36f98e28c4a9dc9631bd05b
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-01-15 18:20:24 +0300

    Client: fix import

--- packages/client/src/pages/Home/Section/Submenu/index.js
+++ packages/client/src/pages/Home/Section/Submenu/index.js
@@ -3,8 +3,8 @@ import { useTranslation } from "react-i18next";
 
 import { Submenu } from "@docspace/shared/components/submenu";
 import Loaders from "@docspace/common/components/Loaders";
-import FilesFilter from "@docspace/common/api/files/filter";
-import { getObjectByLocation } from "@docspace/common/utils";
+import FilesFilter from "@docspace/shared/api/files/filter";
+import { getObjectByLocation } from "@docspace/shared/utils/common";
 
 const SectionSubmenuContent = ({
   isPersonalRoom,

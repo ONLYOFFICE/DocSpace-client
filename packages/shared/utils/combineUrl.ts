429044ea05228d998a76596191dd69b5c68163e3
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2024-01-24 14:09:50 +0500

    Web: Shared: Utils: extended the combine url function to accept numbers

--- packages/shared/utils/combineUrl.ts
+++ packages/shared/utils/combineUrl.ts
@@ -1,9 +1,10 @@
-const combineUrl = (host = "", ...params: string[]) => {
+const combineUrl = (host = "", ...params: string[] | number[]) => {
   let url = host.replace(/\/+$/, "");
 
   params.forEach((part) => {
     if (!part) return;
-    const newPart = part.trim().replace(/^\/+/, "");
+    const convertedPart = String(part);
+    const newPart = convertedPart.trim().replace(/^\/+/, "");
     url += newPart
       ? url.length > 0 && url[url.length - 1] === "/"
         ? newPart

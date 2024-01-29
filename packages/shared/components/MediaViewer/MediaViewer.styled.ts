d1672840bf0507976f8c711b5586edbbe9d4f9db
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2024-01-29 19:20:48 +0500

    Shared:Components:MediaViewer: Added style and utils

---
+++ packages/shared/components/MediaViewer/MediaViewer.styled.ts
@@ -0,0 +1,230 @@
+import styled, { css } from "styled-components";
+
+import { Base } from "@docspace/shared/themes";
+import { DropDown } from "@docspace/shared/components/drop-down";
+import { DropDownItem } from "@docspace/shared/components/drop-down-item";
+
+type StyledButtonScrollProps = {
+  orientation: "left" | "right";
+};
+
+type StyledSwitchToolbarProps = {
+  left?: boolean;
+  isPdfFIle?: boolean;
+};
+
+type StyledViewerContainerProps = {
+  visible: boolean;
+};
+
+export const ControlBtn = styled.div`
+  display: inline-block;
+  height: 30px;
+  line-height: 25px;
+  margin: 5px;
+  width: 40px;
+  border-radius: 2px;
+  cursor: pointer;
+  text-align: center;
+
+  &:hover {
+    background-color: ${(props) =>
+      props.theme.mediaViewer.controlBtn.backgroundColor};
+  }
+`;
+
+ControlBtn.defaultProps = { theme: Base };
+
+export const StyledDropDown = styled(DropDown)`
+  background: #333;
+`;
+
+export const StyledDropDownItem = styled(DropDownItem)`
+  color: #fff;
+
+  .drop-down-item_icon svg {
+    path {
+      fill: #fff !important;
+    }
+  }
+
+  &:hover {
+    background: #444;
+  }
+`;
+
+export const StyledButtonScroll = styled.div<StyledButtonScrollProps>`
+  z-index: 307;
+  position: fixed;
+  top: calc(50% - 20px);
+
+  ${(props) => (props.orientation === "left" ? "left: 20px;" : "right: 20px;")}
+`;
+
+export const StyledMobileDetails = styled.div`
+  z-index: 307;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 53px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.8) 100%
+  );
+
+  svg {
+    path {
+      fill: #fff;
+    }
+  }
+
+  .mobile-close {
+    position: fixed;
+    left: 21px;
+    top: 22px;
+  }
+
+  .mobile-context {
+    position: fixed;
+    right: 22px;
+    top: 22px;
+  }
+
+  .title {
+    font-weight: 600;
+    margin-top: 6px;
+    width: calc(100% - 100px);
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const StyledSwitchToolbar = styled.div<StyledSwitchToolbarProps>`
+  height: 100%;
+  z-index: 306;
+  position: fixed;
+  width: 73px;
+  background: inherit;
+  display: block;
+  opacity: 0;
+  transition: all 0.3s;
+
+  ${(props) =>
+    props.left ? "left: 0" : props.isPdfFIle ? "right: 20px" : "right: 0"};
+
+  &:hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+`;
+
+export const StyledViewerContainer = styled.div<StyledViewerContainerProps>`
+  color: ${(props) => props.theme.mediaViewer.color};
+  display: ${(props) => (props.visible ? "block" : "none")};
+  overflow: hidden;
+  span {
+    position: fixed;
+    ${(props) =>
+      props.theme.interfaceDirection === "rtl"
+        ? css`
+            left: 0;
+            margin-left: 10px;
+          `
+        : css`
+            right: 0;
+            margin-right: 10px;
+          `}
+    bottom: 5px;
+    z-index: 305;
+  }
+  .deleteBtnContainer,
+  .downloadBtnContainer {
+    display: block;
+    width: 16px;
+    height: 16px;
+    margin: 4px 12px;
+    line-height: 19px;
+    svg {
+      path {
+        fill: ${(props) => props.theme.mediaViewer.fill};
+      }
+    }
+  }
+  .details {
+    z-index: 307;
+    padding-top: 21px;
+    height: 64px;
+    width: 100%;
+    background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.8) 100%
+    );
+    position: fixed;
+    top: 0;
+    ${(props) =>
+      props.theme.interfaceDirection === "rtl"
+        ? css`
+            right: 0;
+          `
+        : css`
+            left: 0;
+          `}
+    .title {
+      text-align: center;
+      white-space: nowrap;
+      overflow: hidden;
+      font-size: 20px;
+      font-weight: 600;
+      text-overflow: ellipsis;
+      width: calc(100% - 50px);
+      ${(props) =>
+        props.theme.interfaceDirection === "rtl"
+          ? css`
+              padding-right: 16px;
+            `
+          : css`
+              padding-left: 16px;
+            `}
+      box-sizing: border-box;
+      color: ${(props) => props.theme.mediaViewer.titleColor};
+    }
+  }
+  .mediaPlayerClose {
+    position: fixed;
+    top: 13px;
+    ${(props) =>
+      props.theme.interfaceDirection === "rtl"
+        ? css`
+            left: 12px;
+          `
+        : css`
+            right: 12px;
+          `}
+    height: 17px;
+    &:hover {
+      background-color: transparent;
+    }
+    svg {
+      path {
+        fill: ${(props) => props.theme.mediaViewer.iconColor};
+      }
+    }
+  }
+
+  .containerVideo {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+  }
+`;
+
+StyledViewerContainer.defaultProps = { theme: Base };
---
+++ packages/shared/components/MediaViewer/MediaViewer.utils.ts
@@ -0,0 +1,203 @@
+import type { BoundsType, Point } from "./types";
+
+export const compareTo = (a: number, b: number) => {
+  return Math.trunc(a) > Math.trunc(b);
+};
+
+export const getSizeByAngle = (
+  width: number,
+  height: number,
+  angle: number,
+): [number, number] => {
+  const { abs, cos, sin, PI } = Math;
+
+  const angleByRadians = (PI / 180) * angle;
+
+  const c = cos(angleByRadians);
+  const s = sin(angleByRadians);
+  const halfw = 0.5 * width;
+  const halfh = 0.5 * height;
+  const newWidth = 2 * (abs(c * halfw) + abs(s * halfh));
+  const newHeight = 2 * (abs(s * halfw) + abs(c * halfh));
+
+  return [newWidth, newHeight];
+};
+
+export const getBounds = (
+  image: HTMLImageElement | null,
+  container: HTMLDivElement | null,
+  diffScale: number = 1,
+  angle: number = 0,
+): BoundsType | null => {
+  if (!image || !container) return null;
+
+  let imageBounds = image.getBoundingClientRect();
+  const containerBounds = container.getBoundingClientRect();
+
+  const [width, height] = getSizeByAngle(
+    imageBounds.width,
+    imageBounds.height,
+    angle,
+  );
+
+  if (diffScale !== 1)
+    imageBounds = {
+      ...imageBounds,
+      width: width * diffScale,
+      height: height * diffScale,
+    };
+  else {
+    imageBounds = {
+      ...imageBounds,
+      width,
+      height,
+    };
+  }
+  const originalWidth = image.clientWidth;
+  const widthOverhang = (imageBounds.width - originalWidth) / 2;
+
+  const originalHeight = image.clientHeight;
+  const heightOverhang = (imageBounds.height - originalHeight) / 2;
+
+  const isWidthOutContainer = imageBounds.width >= containerBounds.width;
+
+  const isHeightOutContainer = imageBounds.height >= containerBounds.height;
+
+  const bounds = {
+    right: isWidthOutContainer
+      ? widthOverhang
+      : containerBounds.width - imageBounds.width + widthOverhang,
+    left: isWidthOutContainer
+      ? -(imageBounds.width - containerBounds.width) + widthOverhang
+      : widthOverhang,
+    bottom: isHeightOutContainer
+      ? heightOverhang
+      : containerBounds.height - imageBounds.height + heightOverhang,
+    top: isHeightOutContainer
+      ? -(imageBounds.height - containerBounds.height) + heightOverhang
+      : heightOverhang,
+  };
+
+  return bounds;
+};
+
+export const getImagePositionAndSize = (
+  imageNaturalWidth: number,
+  imageNaturalHeight: number,
+  container: HTMLDivElement | null,
+) => {
+  if (!container) return null;
+
+  const { width: containerWidth, height: containerHeight } =
+    container.getBoundingClientRect();
+
+  let width = Math.min(containerWidth, imageNaturalWidth);
+  let height = (width / imageNaturalWidth) * imageNaturalHeight;
+
+  if (height > containerHeight) {
+    height = containerHeight;
+    width = (height / imageNaturalHeight) * imageNaturalWidth;
+  }
+  const x = (containerWidth - width) / 2;
+  const y = (containerHeight - height) / 2;
+
+  return { width, height, x, y };
+};
+
+export const calculateAdjustImageUtil = (
+  image: HTMLImageElement | null,
+  container: HTMLDivElement | null,
+  point: Point,
+  diffScale: number = 1,
+) => {
+  if (!image || !container) return point;
+
+  // debugger;
+
+  let imageBounds = image.getBoundingClientRect();
+  const containerBounds = container.getBoundingClientRect();
+
+  if (diffScale !== 1) {
+    const { x, y, width, height } = imageBounds;
+
+    const newWidth = imageBounds.width * diffScale;
+    const newHeight = imageBounds.height * diffScale;
+
+    const newX = x + width / 2 - newWidth / 2;
+    const newY = y + height / 2 - newHeight / 2;
+
+    imageBounds = {
+      ...imageBounds,
+      width: newWidth,
+      height: newHeight,
+      left: newX,
+      top: newY,
+      right: newX + newWidth,
+      bottom: newY + newHeight,
+      x: newX,
+      y: newY,
+    };
+  }
+
+  const originalWidth = image.clientWidth;
+  const widthOverhang = (imageBounds.width - originalWidth) / 2;
+
+  const originalHeight = image.clientHeight;
+  const heightOverhang = (imageBounds.height - originalHeight) / 2;
+
+  const isWidthOutContainer = imageBounds.width >= containerBounds.width;
+
+  const isHeightOutContainer = imageBounds.height >= containerBounds.height;
+
+  if (
+    compareTo(imageBounds.left, containerBounds.left) &&
+    isWidthOutContainer
+  ) {
+    point.x = widthOverhang;
+  } else if (
+    compareTo(containerBounds.right, imageBounds.right) &&
+    isWidthOutContainer
+  ) {
+    point.x = containerBounds.width - imageBounds.width + widthOverhang;
+  } else if (!isWidthOutContainer) {
+    point.x = (containerBounds.width - imageBounds.width) / 2 + widthOverhang;
+  }
+
+  if (compareTo(imageBounds.top, containerBounds.top) && isHeightOutContainer) {
+    point.y = heightOverhang;
+  } else if (
+    compareTo(containerBounds.bottom, imageBounds.bottom) &&
+    isHeightOutContainer
+  ) {
+    point.y = containerBounds.height - imageBounds.height + heightOverhang;
+  } else if (!isHeightOutContainer) {
+    point.y =
+      (containerBounds.height - imageBounds.height) / 2 + heightOverhang;
+  }
+
+  return point;
+};
+
+export const calculateAdjustBoundsUtils = (
+  x: number,
+  y: number,
+  bounds: BoundsType | null,
+) => {
+  if (!bounds) return { x, y };
+
+  const { left, right, top, bottom } = bounds;
+
+  if (x > right) {
+    x = right;
+  } else if (x < left) {
+    x = left;
+  }
+
+  if (y > bottom) {
+    y = bottom;
+  } else if (y < top) {
+    y = top;
+  }
+
+  return { x, y };
+};

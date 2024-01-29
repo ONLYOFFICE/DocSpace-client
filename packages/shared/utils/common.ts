dc3729260333280532dd5883dfe76381cadf704c
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2024-01-29 20:03:32 +0500

    Web: Shared: Utils: moved the firstLetterToUppercase function to utilities

--- packages/shared/utils/common.ts
+++ packages/shared/utils/common.ts
@@ -47,6 +47,10 @@ export function changeLanguage(i18n: TI18n, currentLng = getCookie(LANGUAGE)) {
     : i18n.changeLanguage("en");
 }
 
+export const firstLetterToUppercase = (str: string) => {
+  return str[0].toUpperCase() + str.slice(1);
+};
+
 export function createPasswordHash(
   password: string,
   hashSettings: { [key: string]: boolean },

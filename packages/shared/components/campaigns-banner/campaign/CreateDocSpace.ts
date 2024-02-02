e0dd4c0848a74fe4137dce77f5249296348e07c6
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-02-02 15:31:01 +0300

    Shared: CampaignsBanner: fix stories

---
+++ packages/shared/components/campaigns-banner/campaign/CreateDocSpace.ts
@@ -0,0 +1,29 @@
+export const translates = {
+  Title: "ONLYOFFICE for developers",
+  BodyText:
+    "The best online office experience within your app, under your brand.",
+  ActionText: "Start now",
+};
+
+export const config = {
+  borderColor: "#388BDE",
+  title: {
+    color: "#4781D1",
+    fontSize: "12px",
+    fontWeight: "600",
+  },
+  body: {
+    fontSize: "13px",
+    fontWeight: "600",
+    color: "#333",
+  },
+  action: {
+    isButton: true,
+    color: "#fff",
+    backgroundColor: "#388BDE",
+    fontSize: "12px",
+    fontWeight: "700",
+    url: "https://onlyoffice.com",
+    type: "open-url",
+  },
+};

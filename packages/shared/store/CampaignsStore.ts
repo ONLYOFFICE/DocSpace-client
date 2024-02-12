45bb13c8351de689debb59524988cb0dd3004907
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-02-12 15:27:14 +0300

    Shared: CampaignsStore: use campaigns utils

--- packages/shared/store/CampaignsStore.ts
+++ packages/shared/store/CampaignsStore.ts
@@ -3,15 +3,25 @@ import difference from "lodash/difference";
 
 import { LANGUAGE } from "../constants";
 import { getLanguage, getCookie } from "../utils";
+import {
+  getCampaignsLs,
+  getImage,
+  getTranslation,
+  getConfig,
+} from "../utils/campaigns";
 
-const lng: any = getCookie(LANGUAGE) || "en";
-const language = getLanguage(lng instanceof Array ? lng[0] : lng);
+const lng: string[] | string = getCookie(LANGUAGE) || "en";
+const language = getLanguage(typeof lng === "object" ? lng[0] : lng);
 
 class CampaignsStore {
   campaignImage: string | null = null;
+
   campaignTranslate: string | null = null;
+
   campaignConfig: string | null = null;
-  currentCampaign: string | null = null;
+
+  currentCampaign: string | unknown = null;
+
   closedCampaignsLS = localStorage.getItem("closed_campaigns") || "[]";
 
   constructor() {
@@ -25,43 +35,6 @@ class CampaignsStore {
     this.closedCampaignsLS = JSON.stringify(closedCampaigns);
   };
 
-  getImage = async (campaign: string, standalone: boolean): Promise<string> => {
-    if (standalone) {
-      return `/static/campaigns/images/${campaign}.svg`;
-    }
-    const imageUrl = await window.firebaseHelper.getCampaignsImages(campaign);
-    return imageUrl;
-  };
-
-  getTranslation = async (
-    campaign: string,
-    lng: string,
-    standalone: boolean,
-  ) => {
-    let translationUrl;
-    if (standalone) {
-      translationUrl = `/static/campaigns/locales/${lng}/${campaign}.json`;
-    } else {
-      translationUrl = await window.firebaseHelper.getCampaignsTranslations(
-        campaign,
-        lng,
-      );
-    }
-    const res = await fetch(translationUrl);
-    return Promise.resolve(res.json());
-  };
-
-  getConfig = async (campaign: string, standalone: boolean) => {
-    let configUrl;
-    if (standalone) {
-      configUrl = `/static/campaigns/configs/${campaign}.json`;
-    } else {
-      configUrl = await window.firebaseHelper.getCampaignConfig(campaign);
-    }
-    const res = await fetch(configUrl);
-    return Promise.resolve(res.json());
-  };
-
   getBanner = async (standalone: boolean) => {
     let index = Number(localStorage.getItem("bannerIndex") || 0);
 
@@ -73,20 +46,20 @@ class CampaignsStore {
     if (this.campaigns.length < 1 || index + 1 >= this.campaigns.length) {
       index = 0;
     } else {
-      index++;
+      index += 1;
     }
 
     const currentCampaign = this.campaigns[index];
 
     localStorage.setItem("bannerIndex", String(index));
 
-    const image = await this.getImage(currentCampaign, standalone);
-    const translate = await this.getTranslation(
+    const image = await getImage(currentCampaign, standalone);
+    const translate = await getTranslation(
       currentCampaign,
       language,
-      standalone
+      standalone,
     );
-    const config = await this.getConfig(currentCampaign, standalone);
+    const config = await getConfig(currentCampaign, standalone);
 
     runInAction(() => {
       this.currentCampaign = currentCampaign;
@@ -94,16 +67,11 @@ class CampaignsStore {
       this.campaignTranslate = translate;
       this.campaignConfig = config;
     });
-  }
-
-  get campaignsLs() {
-    return (localStorage.getItem("docspace_campaigns") || "")
-      .split(",")
-      .filter((campaign) => campaign.length > 0);
-  }
+  };
 
   get campaigns() {
-    return difference(this.campaignsLs, JSON.parse(this.closedCampaignsLS));
+    const campaignsLs = getCampaignsLs();
+    return difference(campaignsLs, JSON.parse(this.closedCampaignsLS));
   }
 }
 

0b60d17b1e56996ea3a400cffd12029b2c350595
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-02-16 18:08:45 +0300

    Doceditor:Utils: add registry styled components

--- packages/doceditor/src/utils/registry.tsx
+++ packages/doceditor/src/utils/registry.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
-import { useServerInsertedHTML } from "next/navigation";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+import { useServerInsertedHTML } from "next/navigation";
 
 export default function StyledComponentsRegistry({
   children,

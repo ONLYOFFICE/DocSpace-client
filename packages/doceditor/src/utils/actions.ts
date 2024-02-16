1bc5bb0cc40170c27c021147eae816c6ce8abc58
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-02-16 18:06:10 +0300

    Doceditor: update server function

--- packages/doceditor/src/utils/actions.ts
+++ packages/doceditor/src/utils/actions.ts
@@ -1,115 +1,139 @@
 "use server";
+
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { i18n } from "i18next";
 
 import { getLtrLanguageForEditor } from "@docspace/shared/utils/common";
 import { TenantStatus } from "@docspace/shared/enums";
 
-import { IResponse } from "@/types";
+import { TCatchError, TError, TResponse } from "@/types";
 
 const API_PREFIX = "api/2.0";
 
 export async function getData(
-  fileId: string,
+  fileId?: string,
   version?: string,
   doc?: string,
   view?: boolean,
   share?: string,
 ) {
-  const hdrs = headers();
+  try {
+    const hdrs = headers();
 
-  const host = hdrs.get("x-forwarded-host");
-  const proto = hdrs.get("x-forwarded-proto");
+    const host = hdrs.get("x-forwarded-host");
+    const proto = hdrs.get("x-forwarded-proto");
 
-  const baseAPIUrl = `${proto}://${host}/${API_PREFIX}`;
+    const baseURL = `${proto}://${host}`;
+    const baseAPIUrl = `${baseURL}/${API_PREFIX}`;
 
-  const configURL = new URL(`${baseAPIUrl}/files/file/${fileId}/openedit`);
-  const editorURL = new URL(`${baseAPIUrl}/files/docservice`);
-  const userURL = new URL(`${baseAPIUrl}/people/@self`);
-  const settingsURL = new URL(`${baseAPIUrl}/settings?withPassword=false`);
+    if (!fileId) return redirect(`${baseURL}/login`);
 
-  if (view) configURL.searchParams.append("view", view ? "true" : "false");
-  if (version) {
-    configURL.searchParams.append("version", version);
-    editorURL.searchParams.append("version", version);
-  }
-  if (doc) configURL.searchParams.append("doc", doc);
-  if (share) configURL.searchParams.append("share", share);
+    const configURL = new URL(`${baseAPIUrl}/files/file/${fileId}/openedit`);
+    const editorURL = new URL(`${baseAPIUrl}/files/docservice`);
+    const userURL = new URL(`${baseAPIUrl}/people/@self`);
+    const settingsURL = new URL(`${baseAPIUrl}/settings?withPassword=false`);
 
-  const getConfig = new Request(configURL, {
-    headers: hdrs,
-  });
-  const getEditorUrl = new Request(editorURL, {
-    headers: hdrs,
-  });
-  const getUser = new Request(userURL, {
-    headers: hdrs,
-  });
-  const getSettings = new Request(settingsURL, {
-    headers: hdrs,
-  });
+    if (view) configURL.searchParams.append("view", view ? "true" : "false");
+    if (version) {
+      configURL.searchParams.append("version", version);
+      editorURL.searchParams.append("version", version);
+    }
+    if (doc) configURL.searchParams.append("doc", doc);
+    if (share) configURL.searchParams.append("share", share);
 
-  const resActions = [];
+    const getConfig = new Request(configURL, {
+      headers: hdrs,
+    });
+    const getEditorUrl = new Request(editorURL, {
+      headers: hdrs,
+    });
+    const getUser = new Request(userURL, {
+      headers: hdrs,
+    });
+    const getSettings = new Request(settingsURL, {
+      headers: hdrs,
+    });
 
-  resActions.push(fetch(getConfig));
-  resActions.push(fetch(getEditorUrl));
-  resActions.push(fetch(getUser));
-  resActions.push(fetch(getSettings));
+    const resActions = [];
 
-  const [configRes, editorUrlRes, userRes, settingsRes] =
-    await Promise.all(resActions);
+    resActions.push(fetch(getConfig));
+    resActions.push(fetch(getEditorUrl));
+    resActions.push(fetch(getUser));
+    resActions.push(fetch(getSettings));
 
-  if (!configRes.ok) return redirect("/error");
+    const [configRes, editorUrlRes, userRes, settingsRes] =
+      await Promise.all(resActions);
 
-  const actions = [];
+    if (!configRes.ok) return redirect(`${baseURL}`);
 
-  actions.push(configRes.json());
-  actions.push(editorUrlRes.json());
-  actions.push(userRes.json());
-  actions.push(settingsRes.json());
+    const actions = [];
 
-  const [config, editorUrl, user, settings] = await Promise.all(actions);
+    actions.push(configRes.json());
+    actions.push(editorUrlRes.json());
+    actions.push(userRes.json());
+    actions.push(settingsRes.json());
 
-  const response: IResponse = {
-    config: config.response,
-    editorUrl: editorUrl.response,
-    user: user.response,
-    settings: settings.response,
-    successAuth: false,
-    isSharingAccess: false,
-    error: "",
-  };
+    const [config, editorUrl, user, settings] = await Promise.all(actions);
 
-  // needed to reset rtl language in Editor
-  response.config.editorConfig.lang = getLtrLanguageForEditor(
-    response.user?.cultureName,
-    response.settings.culture,
-    true,
-  );
-
-  let error: unknown;
-
-  if (response.settings.tenantStatus === TenantStatus.PortalRestore) {
-    response.error = "restore-backup";
-  }
-
-  const successAuth = !!user;
-
-  if (!successAuth && !doc && !share) {
-    response.error = error = {
-      unAuthorized: true,
+    const response: TResponse = {
+      config: config.response,
+      editorUrl: editorUrl.response,
+      user: user.response,
+      settings: settings.response,
+      successAuth: false,
+      isSharingAccess: false,
+      doc,
     };
+
+    // needed to reset rtl language in Editor
+    response.config.editorConfig.lang = getLtrLanguageForEditor(
+      response.user?.cultureName,
+      response.settings.culture,
+      true,
+    );
+
+    if (response.settings.tenantStatus === TenantStatus.PortalRestore) {
+      response.error = { message: "restore-backup" };
+    }
+
+    const successAuth = !!user;
+
+    if (!successAuth && !doc && !share) {
+      response.error = { message: "unauthorized" };
+    }
+
+    const isSharingAccess = response.config.file.canShare;
+
+    if (view) {
+      response.config.editorConfig.mode = "view";
+    }
+
+    response.successAuth = successAuth;
+    response.isSharingAccess = isSharingAccess;
+
+    return response;
+  } catch (e) {
+    const err = e as TCatchError;
+    console.error("initDocEditor failed", err);
+    let message = "";
+    if (typeof err === "string") message = err;
+    else
+      message =
+        ("response" in err && err.response?.data?.error?.message) ||
+        ("message" in err && err.message) ||
+        "";
+
+    const status =
+      typeof err !== "string"
+        ? ("response" in err && err?.response?.data?.statusCode) ||
+          ("response" in err && err?.response?.data?.status) ||
+          ""
+        : "";
+
+    const error: TError = {
+      message,
+      status,
+    };
+    return { error };
   }
-
-  const isSharingAccess = response.config.file.canShare;
-
-  if (view) {
-    response.config.editorConfig.mode = "view";
-  }
-
-  response.successAuth = successAuth;
-  response.isSharingAccess = isSharingAccess;
-
-  return response;
 }

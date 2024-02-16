2f0efc62df50c808dad7a9efdfad410096692ec0
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-02-16 18:24:50 +0300

    Doceditor:Hooks:UseDeepLink: init

---
+++ packages/doceditor/src/hooks/useDeepLink.ts
@@ -0,0 +1,55 @@
+import React from "react";
+import { isMobile } from "react-device-detect";
+
+import { TFile } from "@docspace/shared/api/files/types";
+import { TSettings } from "@docspace/shared/api/settings/types";
+
+import { getDeepLink } from "@/utils/deepLinkHelper";
+
+export interface UseDeepLinkProps {
+  settings?: TSettings;
+  fileInfo?: TFile;
+  email?: string;
+}
+
+const useDeepLink = ({ settings, fileInfo, email }: UseDeepLinkProps) => {
+  const [isShowDeepLink, setIsShowDeepLink] = React.useState(false);
+
+  React.useEffect(() => {
+    const androidID = settings?.deepLink.androidPackageName;
+    const iOSId = settings?.deepLink.iosPackageId;
+    const deepLinkUrl = settings?.deepLink.url;
+    const isAndroidWebView =
+      window.navigator.userAgent.includes("AscAndroidWebView");
+
+    const defaultOpenDocument = localStorage.getItem("defaultOpenDocument");
+    const params = new URLSearchParams(window.location.search);
+    const withoutRedirect = params.get("without_redirect");
+
+    if (
+      isMobile &&
+      !defaultOpenDocument &&
+      androidID &&
+      iOSId &&
+      deepLinkUrl &&
+      !withoutRedirect &&
+      !isAndroidWebView
+    ) {
+      setIsShowDeepLink(true);
+    }
+
+    if (isMobile && defaultOpenDocument === "app") {
+      getDeepLink(
+        window.location.origin,
+        email || "",
+        fileInfo,
+        settings?.deepLink,
+        window.location.href,
+      );
+    }
+  }, [fileInfo, settings?.deepLink, email]);
+
+  return { isShowDeepLink, setIsShowDeepLink };
+};
+
+export default useDeepLink;

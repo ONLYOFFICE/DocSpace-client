a1009d8679c03773761c7a75e2a2a95d20fd69e9
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-02-19 15:34:15 +0300

    Components:Scrollbar: move custom scrollbar from library to component and optimize for react 18

---
+++ packages/components/scrollbar/custom-scrollbar/index.ts
@@ -0,0 +1,7 @@
+import Scrollbar, { ScrollbarProps, ScrollbarState } from "./Scrollbar";
+
+export { ScrollbarContext } from "./Scrollbar";
+
+export { Scrollbar };
+
+export type { ScrollbarProps, ScrollbarState };

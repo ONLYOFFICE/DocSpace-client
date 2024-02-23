a6ed0d81e4429627b3925177f002038ef62a29d4
Elyor Djalilov <elyor.djalilov@onlyoffice.com>
2024-02-23 18:48:21 +0500

    redesigned 404 error page

--- packages/shared/components/errors/Error404.tsx
+++ packages/shared/components/errors/Error404.tsx
@@ -1,11 +1,28 @@
 import { useTranslation } from "react-i18next";
-
+import { useNavigate } from "react-router";
+import Error404SvgUrl from "PUBLIC_DIR/images/error404.svg?url";
 import ErrorContainer from "../error-container/ErrorContainer";
 
 const Error404 = () => {
   const { t, ready } = useTranslation("Common");
+  const navigate = useNavigate();
 
-  return ready && <ErrorContainer headerText={t("Error404Text")} />;
+  const onClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    ready && (
+      <ErrorContainer
+        headerText={t("Error404HeaderText")}
+        customizedBodyText={t("Error404BodyText")}
+        img={Error404SvgUrl}
+        withImg
+        buttonText={t("GoBack")}
+        onClickButton={onClick}
+      />
+    )
+  );
 };
 
 export default Error404;

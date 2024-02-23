1ef3b97368129545fe9e20d4965a4e3adb0cb498
Elyor Djalilov <elyor.djalilov@onlyoffice.com>
2024-02-23 17:22:21 +0500

    redesigned 403 error page

--- packages/shared/components/errors/Error403.tsx
+++ packages/shared/components/errors/Error403.tsx
@@ -1,12 +1,26 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
-
+import { useNavigate } from "react-router";
+import Error403SvgUrl from "PUBLIC_DIR/images/error403.svg?url";
 import ErrorContainer from "../error-container/ErrorContainer";
 
 const Error403 = () => {
   const { t } = useTranslation("Common");
+  const navigate = useNavigate();
 
-  return <ErrorContainer headerText={t("Error403Text")} />;
+  const onClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <ErrorContainer
+      headerText={t("Error403HeaderText")}
+      customizedBodyText={t("Error403BodyText")}
+      img={Error403SvgUrl}
+      withImg
+      buttonText={t("GoBack")}
+      onClickButton={onClick}
+    />
+  );
 };
 
 export default Error403;

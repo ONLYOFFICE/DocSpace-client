d6051f3d09f457353532ef437221badc04e8d99d
VladaGazizova <vlada.gazizova@onlyoffice.com>
2024-03-18 16:10:34 +0300

    Fixed Bug 66332 – Rooms. Changing the scale/focus of a custom room icon does not count as editing it.

--- packages/shared/components/image-editor/ImageCropper/index.tsx
+++ packages/shared/components/image-editor/ImageCropper/index.tsx
@@ -1,25 +1,25 @@
 // (c) Copyright Ascensio System SIA 2010-2024
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
@@ -47,12 +47,13 @@ const ImageCropper = ({
   setUploadedFile,
   setPreviewImage,
   isDisabled,
+  disableImageRescaling,
 }: ImageCropperProps) => {
   const editorRef = React.useRef<null | AvatarEditor>(null);
   const setEditorRef = (editor: AvatarEditor) => (editorRef.current = editor);
 
   const handlePositionChange = (position: Position) => {
-    if (isDisabled) return;
+    if (isDisabled || disableImageRescaling) return;
 
     onChangeImage({ ...image, x: position.x, y: position.y });
   };
@@ -106,10 +107,14 @@ const ImageCropper = ({
   }, [handleImageChange, image, setPreviewImage]);
 
   return (
-    <StyledImageCropper className="icon_cropper">
+    <StyledImageCropper
+      className="icon_cropper"
+      disableImageRescaling={disableImageRescaling}
+    >
       <div className="icon_cropper-crop_area">
         <ReactSVG className="icon_cropper-grid" src={IconCropperGridSvgUrl} />
         <AvatarEditor
+          className="icon_cropper-avatar-editor"
           ref={setEditorRef}
           image={uploadedFile}
           width={648}
@@ -138,7 +143,7 @@ const ImageCropper = ({
         </div>
       </div>
 
-      {uploadedFile?.name && (
+      {uploadedFile?.name && !disableImageRescaling && (
         <div className="icon_cropper-zoom-container">
           <IconButton
             className="icon_cropper-zoom-container-button"

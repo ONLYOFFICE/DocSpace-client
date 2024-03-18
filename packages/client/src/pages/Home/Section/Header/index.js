ef12b0e1753eb565cf3c61fb22a46555b351f6b2
VladaGazizova <vlada.gazizova@onlyoffice.com>
2024-03-18 11:27:43 +0300

    Shared:Navigation:Fixed hiding the room name and part of the info panel in a public room when zoomed out closer to 1024px.

--- packages/client/src/pages/Home/Section/Header/index.js
+++ packages/client/src/pages/Home/Section/Header/index.js
@@ -1,30 +1,30 @@
-// (c) Copyright Ascensio System SIA 2010-2024
-// 
+﻿// (c) Copyright Ascensio System SIA 2010-2024
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-﻿import FolderLockedReactSvgUrl from "PUBLIC_DIR/images/folder.locked.react.svg?url";
+import FolderLockedReactSvgUrl from "PUBLIC_DIR/images/folder.locked.react.svg?url";
 import ActionsDocumentsReactSvgUrl from "PUBLIC_DIR/images/actions.documents.react.svg?url";
 import SpreadsheetReactSvgUrl from "PUBLIC_DIR/images/spreadsheet.react.svg?url";
 import ActionsPresentationReactSvgUrl from "PUBLIC_DIR/images/actions.presentation.react.svg?url";
@@ -162,6 +162,16 @@ const StyledContainer = styled.div`
       display: block;
       margin: 0 16px;
       overflow: visible;
+      min-width: 50px;
+
+      .button-content {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        display: block;
+        padding-top: 3px;
+      }
 
       @media ${tablet} {
         display: ${({ isInfoPanelVisible }) =>
@@ -1234,6 +1244,7 @@ const SectionHeaderContent = (props) => {
                 navigationButtonLabel={navigationButtonLabel}
                 onNavigationButtonClick={onNavigationButtonClick}
                 tariffBar={<TariffBar />}
+                showNavigationButton={!!showNavigationButton}
               />
             </div>
           )}

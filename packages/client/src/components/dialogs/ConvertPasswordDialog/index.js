eafa72423842ae29a875786a891b921d1eae9bfb
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-03-21 16:33:35 +0300

    Client:ConvertPasswordDialog: delete useless

--- packages/client/src/components/dialogs/ConvertPasswordDialog/index.js
+++ packages/client/src/components/dialogs/ConvertPasswordDialog/index.js
@@ -1,58 +1,51 @@
 // (c) Copyright Ascensio System SIA 2010-2024
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
 import React, { useState, useCallback, useEffect } from "react";
+import { withTranslation } from "react-i18next";
+import { inject, observer } from "mobx-react";
 
 import { ModalDialog } from "@docspace/shared/components/modal-dialog";
 import { Button } from "@docspace/shared/components/button";
 import { Text } from "@docspace/shared/components/text";
-import { toastr } from "@docspace/shared/components/toast";
+import { combineUrl } from "@docspace/shared/utils/combineUrl";
 
-import { withTranslation } from "react-i18next";
-import { inject, observer } from "mobx-react";
 import SimulatePassword from "../../SimulatePassword";
 import StyledComponent from "./StyledConvertPasswordDialog";
 import config from "PACKAGE_FILE";
-import { openDocEditor } from "@docspace/client/src/helpers/filesUtils";
-import { combineUrl } from "@docspace/shared/utils/combineUrl";
 
-let tab, _isMounted;
 const ConvertPasswordDialogComponent = (props) => {
   const {
     t,
     visible,
     setConvertPasswordDialogVisible,
-    isTabletView,
-    copyAsAction,
+
     formCreationInfo,
     setFormCreationInfo,
     setPasswordEntryProcess,
-    isDesktop,
-    completeAction,
-    fileCopyAs,
   } = props;
   const inputRef = React.useRef(null);
 
@@ -89,12 +82,6 @@ const ConvertPasswordDialogComponent = (props) => {
   const onKeyDown = (e) => {
     if (e.key === "Enter") {
       onConvert();
-    }
-  };
-
-  const focusInput = () => {
-    if (inputRef) {
-      inputRef.current.focus();
     }
   };
 

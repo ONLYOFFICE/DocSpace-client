4e739ed80f3bb08cd33006f32142e68cd712f9d2
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2024-03-22 14:38:38 +0300

    Added support for opening settings in a new tab.

--- packages/client/src/pages/PortalSettings/Layout/Article/Body/index.js
+++ packages/client/src/pages/PortalSettings/Layout/Article/Body/index.js
@@ -1,25 +1,25 @@
 // (c) Copyright Ascensio System SIA 2010-2024
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
@@ -33,6 +33,7 @@ import { DeviceType } from "@docspace/shared/enums";
 import { getCatalogIconUrlByType } from "@docspace/shared/utils/catalogIconHelper";
 
 import { isArrayEqual } from "@docspace/shared/utils";
+import { openingNewTab } from "@docspace/shared/utils/openingNewTab";
 
 import withLoading from "SRC_DIR/HOCs/withLoading";
 
@@ -168,21 +169,22 @@ const ArticleBodyContent = (props) => {
     selectedKeys,
   ]);
 
-  const onSelect = (value) => {
+  const onSelect = (value, e) => {
     if (isArrayEqual([value], selectedKeys)) {
       return;
-    }
-
-    // setSelectedKeys([value + "-0"]);
-
-    if (currentDeviceType === DeviceType.mobile) {
-      toggleArticleOpen();
     }
 
     const settingsPath = `/portal-settings${getSelectedLinkByKey(
       value + "-0",
       settingsTree,
     )}`;
+
+    if (openingNewTab(settingsPath, e)) return;
+    // setSelectedKeys([value + "-0"]);
+
+    if (currentDeviceType === DeviceType.mobile) {
+      toggleArticleOpen();
+    }
 
     if (settingsPath === location.pathname) return;
 
@@ -298,7 +300,7 @@ const ArticleBodyContent = (props) => {
           text={mapKeys(item.tKey)}
           value={item.link}
           isActive={item.key === selectedKeys[0][0]}
-          onClick={() => onSelect(item.key)}
+          onClick={(e) => onSelect(item.key, e)}
           folderId={item.id}
           style={{
             marginTop: `${item.key.includes(9) ? "16px" : "0"}`,

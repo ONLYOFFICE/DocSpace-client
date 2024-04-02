76909663677d8ad001477e8d4a96b93318321888
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2024-04-02 08:27:52 +0300

    Client: PortalSettings: Change code block for JS SDK

--- packages/client/src/pages/PortalSettings/categories/developer-tools/JavascriptSDK/sub-components/CodeBlock.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/JavascriptSDK/sub-components/CodeBlock.js
@@ -2,56 +2,53 @@ import React from "react";
 import styled from "styled-components";
 
 import CodeMirror from "@uiw/react-codemirror";
-import { createTheme } from "@uiw/codemirror-themes";
 import { javascript } from "@codemirror/lang-javascript";
-import { tags as t } from "@lezer/highlight";
-
+import { githubLightInit, githubDarkInit } from "@uiw/codemirror-theme-github";
 import { Base } from "@docspace/shared/themes";
 
 const StyledContainer = styled.div`
   border: 1px solid ${(props) => props.theme.plugins.borderColor};
   border-radius: 6px;
   width: 800px;
-  margin-top: 16px;
-
   overflow: hidden;
-
   background-color: ${(props) => props.theme.sdkPresets.previewBackgroundColor};
 `;
 
 StyledContainer.defaultProps = { theme: Base };
 
-const baseTheme = createTheme({
-  theme: "light",
-  settings: {
-    background: "#ffffff",
-    foreground: "#333333",
-    caret: "#333333",
-    selection: "#036dd626",
-    selectionMatch: "#036dd626",
-    lineHighlight: "#8a91991a",
-    gutterBackground: "#ffffff",
-    gutterForeground: "#A3A9AE",
-    gutterBorder: "#ffffff",
-  },
-  styles: [
-    { tag: t.variableName, color: "#EA7F3B" },
-    { tag: [t.string, t.special(t.brace)], color: "#EA7F3B" },
-    { tag: [t.null, t.bool], color: "#456FA0" },
-  ],
-});
-
-const extensions = [javascript({ jsx: true })];
-
 const CodeBlock = ({ config }) => {
   const codeString = `const config = ${JSON.stringify(config, null, "\t")}\n\nconst script = document.createElement("script");\n\nscript.setAttribute("src", "${new URL(window.location).origin}/static/scripts/api.js");\nscript.onload = () => window.DocSpace.SDK.initFrame(config);\n\ndocument.body.appendChild(script);`;
+
+  const extensions = [javascript({ jsx: true })];
+
+  const baseTheme = githubLightInit({
+    settings: {
+      background: "#FFFFFF",
+      caret: "#000000",
+      lineHighlight: "#F3F4F4",
+      gutterBorder: "#F8F9F9",
+      gutterBackground: "#F8F9F9",
+      gutterForeground: "#333333",
+    },
+  });
+
+  const darkTheme = githubDarkInit({
+    settings: {
+      background: "#282828",
+      caret: "#FFFFFF",
+      lineHighlight: "#3D3D3D",
+      gutterBorder: "#242424",
+      gutterBackground: "#242424",
+      gutterForeground: "#ADADAD",
+    },
+  });
 
   return (
     <StyledContainer>
       <CodeMirror
         value={codeString}
-        width="800px"
-        theme={baseTheme}
+        maxWidth="800px"
+        theme={theme.isBase ? baseTheme : darkTheme}
         extensions={extensions}
         editable={true}
         readOnly={true}

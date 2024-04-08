e604c187978994dda45796a69d34ea3ef6773a24
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2024-04-08 17:05:17 +0500

    Shared:Selectors:Room Fixed disabling the button

--- packages/shared/selectors/Room/index.tsx
+++ packages/shared/selectors/Room/index.tsx
@@ -91,7 +91,11 @@ const RoomSelector = ({
     isDoubleClick: boolean,
     doubleClickCallback: () => void,
   ) => {
-    setSelectedItem(item);
+    setSelectedItem((el) => {
+      if (el?.id === item.id) return null;
+
+      return item;
+    });
     if (isDoubleClick && !isMultiSelect) {
       doubleClickCallback();
     }

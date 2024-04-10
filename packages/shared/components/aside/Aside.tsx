8bebb4a47f411f6c2e5d006aad86ea644a5aa317
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-04-10 09:57:48 +0300

    Client:Shared: fix height for ios tablet and mobile devices

--- packages/shared/components/aside/Aside.tsx
+++ packages/shared/components/aside/Aside.tsx
@@ -25,7 +25,6 @@
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
 import React from "react";
-import { isMobileOnly, isIOS } from "react-device-detect";
 
 import { Scrollbar } from "../scrollbar";
 
@@ -47,58 +46,7 @@ const AsidePure = (props: AsideProps) => {
     withoutBodyScroll = false,
     onClose,
   } = props;
-  const [windowHeight] = React.useState(window.innerHeight);
   const contentRef = React.useRef<HTMLElement | null>(null);
-  const diffRef = React.useRef<number | null>(null);
-  const visualPageTop = React.useRef(0);
-
-  const onResize = React.useCallback(
-    (e: Event) => {
-      if (!contentRef.current) return;
-
-      const target = e.target as VisualViewport;
-
-      if (e?.type === "resize") {
-        const diff = windowHeight - target.height - target.pageTop;
-
-        visualPageTop.current = target.pageTop;
-
-        contentRef.current.style.bottom = `${diff}px`;
-
-        contentRef.current.style.height = `${
-          target.height - 64 + target.pageTop
-        }px`;
-
-        contentRef.current.style.position = "fixed";
-
-        diffRef.current = diff;
-      } else if (e?.type === "scroll") {
-        const diff = window.visualViewport?.pageTop ? 0 : visualPageTop.current;
-
-        if (diffRef.current)
-          contentRef.current.style.bottom = `${diffRef.current + diff}px`;
-
-        if (window.visualViewport)
-          contentRef.current.style.height = `${
-            window.visualViewport.height - 64 + diff
-          }px`;
-
-        contentRef.current.style.position = "fixed";
-      }
-    },
-    [windowHeight],
-  );
-
-  React.useEffect(() => {
-    if (isMobileOnly && isIOS) {
-      window.visualViewport?.addEventListener("resize", onResize);
-      window.visualViewport?.addEventListener("scroll", onResize);
-    }
-    return () => {
-      window.visualViewport?.removeEventListener("resize", onResize);
-      window.visualViewport?.removeEventListener("scroll", onResize);
-    };
-  }, [onResize]);
 
   return (
     <StyledAside

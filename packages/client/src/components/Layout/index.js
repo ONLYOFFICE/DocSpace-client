7121198246ca784095a84f3545d68c4106a65d7c
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-04-12 14:43:13 +0300

    Client:Components:Main/Layout: fix dvh for 102 Chrome

--- packages/client/src/components/Layout/index.js
+++ packages/client/src/components/Layout/index.js
@@ -41,6 +41,8 @@ const StyledContainer = styled.div`
   user-select: none;
   width: 100%;
 
+  // need for support 102 Chrome
+  height: 100vh;
   height: 100dvh;
 
   #customScrollBar {
@@ -111,7 +113,16 @@ const Layout = (props) => {
     const htmlEl = document.getElementsByTagName("html")[0];
     const bodyEl = document.getElementsByTagName("body")[0];
 
-    htmlEl.style.height = bodyEl.style.height = "100dvh";
+    const isSupported =
+      "CSS" in window &&
+      "supports" in window.CSS &&
+      window.CSS.supports("height: 100dvh");
+
+    console.log(isSupported);
+
+    htmlEl.style.height = bodyEl.style.height = isSupported
+      ? "100dvh"
+      : "100vh";
     htmlEl.style.overflow = "hidden";
   }, []);
 

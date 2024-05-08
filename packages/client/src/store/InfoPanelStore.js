754617f339760c2627d3c82244b0161e12e9207e
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2024-05-08 16:34:27 +0200

    Client: Members: Fix recalculating titles and infinite loading in members

--- packages/client/src/store/InfoPanelStore.js
+++ packages/client/src/store/InfoPanelStore.js
@@ -651,6 +651,38 @@ class InfoPanelStore {
     };
   };
 
+  fetchMoreMembers = async (t, withoutTitles) => {
+    const roomId = this.infoPanelSelection.id;
+    const oldMembers = this.infoPanelMembers;
+
+    const data = await this.filesStore.getRoomMembers(roomId, false);
+
+    const newMembers = this.convertMembers(t, data, false, true);
+
+    const mergedMembers = {
+      roomId: roomId,
+      administrators: [
+        ...oldMembers.administrators,
+        ...newMembers.administrators,
+      ],
+      users: [...oldMembers.users, ...newMembers.users],
+      expected: [...oldMembers.expected, ...newMembers.expectedMembers],
+      groups: [...oldMembers.groups, ...newMembers.groups],
+    };
+
+    if (!withoutTitles) {
+      this.addMembersTitle(
+        t,
+        mergedMembers.administrators,
+        mergedMembers.users,
+        mergedMembers.expected,
+        mergedMembers.groups,
+      );
+    }
+
+    this.setInfoPanelMembers(mergedMembers);
+  };
+
   addInfoPanelMembers = (t, members) => {
     const convertedMembers = this.convertMembers(t, members);
 

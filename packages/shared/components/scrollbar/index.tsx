5e02b121d4244bb9e9af93fc3140ea6425a24b96
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-05-21 18:22:24 +0300

    Shared:Scrollbar: fix ssr render

--- packages/shared/components/scrollbar/index.tsx
+++ packages/shared/components/scrollbar/index.tsx
@@ -24,6 +24,8 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
+"use client";
+
 import { ScrollbarType } from "./Scrollbar.enums";
 import { ScrollbarComponent as Scrollbar } from "./Scrollbar";
 import { ScrollbarContext } from "./custom-scrollbar";

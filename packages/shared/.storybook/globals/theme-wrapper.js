73fd154d6d674c5c877cdfe5168eac18f59cec96
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-05-22 17:04:16 +0300

    Shared:Storybook: add current color scheme

--- packages/shared/.storybook/globals/theme-wrapper.js
+++ packages/shared/.storybook/globals/theme-wrapper.js
@@ -2,7 +2,16 @@ import PropTypes from "prop-types";
 import { ThemeProvider } from "../../components/theme-provider";
 
 const ThemeWrapper = ({ theme, children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider
+      theme={theme}
+      currentColorScheme={{
+        main: { accent: "#4781D1", buttons: "#5299E0" },
+      }}
+    >
+      {children}
+    </ThemeProvider>
+  );
 };
 
 ThemeWrapper.propTypes = {

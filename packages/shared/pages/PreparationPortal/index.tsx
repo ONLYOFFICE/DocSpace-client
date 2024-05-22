c321aa5bbaf381da235898c110400411afeb5d47
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2024-05-22 14:38:38 +0300

    Shared: Refactoring.

--- packages/shared/pages/PreparationPortal/index.tsx
+++ packages/shared/pages/PreparationPortal/index.tsx
@@ -24,40 +24,33 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 import React, { useEffect, useState } from "react";
-import { withTranslation } from "react-i18next";
-import { observer, inject } from "mobx-react";
+import { useTranslation } from "react-i18next";
 
 import ErrorContainer from "@docspace/shared/components/error-container/ErrorContainer";
 
 import { StyledPreparationPortal } from "./PreparationPortal.styled";
 import { Text } from "../../components/text";
-import { getRestoreProgress } from "../../api/portal";
+
 import { ColorTheme, ThemeId } from "../../components/color-theme";
 import { IPreparationPortal } from "./PreparationPortal.types";
+import {
+  getRecoveryProgress,
+  reachingSecondBoundary,
+  reachingThirdBoundary,
+} from "./PreparationPortal.utils";
 
-const baseSize = 1073741824; // number of bytes in one GB
-const unSizeMultiplicationFactor = 3;
-const baseFirstMultiplicationFactor = 700;
-const baseSecondMultiplicationFactor = 400;
-const baseThirdMultiplicationFactor = 180;
 const firstBound = 10;
 const secondBound = 63;
-const thirdBound = 98;
 
 let timerId: ReturnType<typeof setInterval> | null;
 let progressTimerId: ReturnType<typeof setInterval> | null;
-let prevProgress: number = 0;
-let requestsCount: number = 0;
 
 const PreparationPortal = (props: IPreparationPortal) => {
-  const {
-    multiplicationFactor,
-    t,
-    withoutHeader,
-    style,
-    clearLocalStorage,
-    isDialog,
-  } = props;
+  const { withoutHeader, style, isDialog } = props;
+
+  const { t } = useTranslation(["PreparationPortal", "Common"]);
+
+  const errorInternalServer = t("Common:ErrorInternalServer");
 
   const [percent, setPercent] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
@@ -70,178 +63,34 @@ const PreparationPortal = (props: IPreparationPortal) => {
     timerId = null;
   };
 
-  const returnToPortal = () => {
-    setTimeout(() => {
-      window.location.replace("/");
-    }, 5000);
-  };
-
-  const reachingFirstBoundary = (percentage: number) => {
-    let progress = percentage;
-    const delay = baseFirstMultiplicationFactor * multiplicationFactor;
-
-    if (progressTimerId) return;
-
-    progressTimerId = setInterval(() => {
-      progress += 1;
-
-      if (progress !== firstBound) setPercent(progress);
-      else {
-        if (progressTimerId) clearInterval(progressTimerId);
-        progressTimerId = null;
-      }
-    }, delay);
-  };
-  const reachingSecondBoundary = (percentage: number) => {
-    let progress = percentage;
-
-    const delay = baseSecondMultiplicationFactor * multiplicationFactor;
-
-    if (progressTimerId) return;
-
-    progressTimerId = setInterval(() => {
-      progress += 1;
-
-      if (progress !== secondBound) setPercent(progress);
-      else {
-        if (progressTimerId) clearInterval(progressTimerId);
-        progressTimerId = null;
-      }
-    }, delay);
-  };
-
-  const reachingThirdBoundary = (percentage: number) => {
-    let progress = percentage;
-    const delay = baseThirdMultiplicationFactor * multiplicationFactor;
-    if (progressTimerId) return;
-
-    progressTimerId = setInterval(() => {
-      progress += 1;
-
-      if (progress < thirdBound) setPercent(progress);
-      else {
-        if (progressTimerId) clearInterval(progressTimerId);
-        progressTimerId = null;
-      }
-    }, delay);
-  };
-
   useEffect(() => {
-    if (percent >= firstBound) {
-      if (percent < secondBound) {
-        reachingSecondBoundary(percent);
-        return;
-      }
+    if (percent < firstBound) return;
 
-      reachingThirdBoundary(percent);
+    if (percent < secondBound) {
+      reachingSecondBoundary(percent, secondBound, progressTimerId, setPercent);
+      return;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    reachingThirdBoundary(percent, progressTimerId, setPercent);
   }, [percent]);
 
-  const getIntervalProgress = async () => {
-    try {
-      const response = await getRestoreProgress();
-
-      if (!response) {
-        setErrorMessage(t("Common:ErrorInternalServer"));
-        clearAllIntervals();
-        return;
-      }
-
-      if (response.error) {
-        if (timerId) clearInterval(timerId);
-        if (progressTimerId) clearInterval(progressTimerId);
-
-        progressTimerId = null;
-        timerId = null;
-        setErrorMessage(response.error);
-
-        return;
-      }
-
-      const currProgress = response.progress;
-
-      if (currProgress > 0 && prevProgress !== currProgress) {
-        setPercent(currProgress);
-
-        if (progressTimerId) clearInterval(progressTimerId);
-        progressTimerId = null;
-      }
-
-      prevProgress = currProgress;
-
-      if (currProgress === 100) {
-        clearAllIntervals();
-        clearLocalStorage();
-        returnToPortal();
-      }
-    } catch (error: any) {
-      clearAllIntervals();
-      setErrorMessage(error);
-    }
-  };
-
-  const getRecoveryProgress = async () => {
-    const getMessage = (error: any) => {
-      if (typeof error !== "object") return error;
-
-      return (
-        error?.response?.data?.error?.message ||
-        error?.statusText ||
-        error?.message ||
-        t("Common:ErrorInternalServer")
-      );
-    };
-
-    try {
-      const response = await getRestoreProgress();
-
-      if (!response) {
-        setErrorMessage(t("Common:ErrorInternalServer"));
-        return;
-      }
-
-      const { progress, error } = response;
-
-      if (error) {
-        setErrorMessage(error);
-
-        return;
-      }
-
-      if (progress === 100) {
-        returnToPortal();
-        clearLocalStorage();
-      } else {
-        timerId = setInterval(() => getIntervalProgress(), 1000);
-        if (progress < firstBound) reachingFirstBoundary(progress);
-      }
-
-      setPercent(progress);
-    } catch (err: any) {
-      const status = err?.response?.status;
-      const needCreationTableTime = status === 404;
-
-      if (needCreationTableTime && requestsCount < 3) {
-        // eslint-disable-next-line no-plusplus
-        requestsCount++;
-        getRecoveryProgress();
-        return;
-      }
-
-      setErrorMessage(getMessage(err));
-    }
-  };
   useEffect(() => {
     setTimeout(() => {
-      getRecoveryProgress();
+      getRecoveryProgress(
+        setErrorMessage,
+        errorInternalServer,
+        timerId,
+        progressTimerId,
+        firstBound,
+        setPercent,
+        clearAllIntervals,
+      );
     }, 6000);
 
     return () => {
       clearAllIntervals();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [errorInternalServer]);
 
   const headerText = errorMessage
     ? t("Common:Error")
@@ -276,17 +125,4 @@ const PreparationPortal = (props: IPreparationPortal) => {
   );
 };
 
-export default inject(({ backup }) => {
-  const { backupSize, clearLocalStorage } = backup;
-
-  const multiplicationFactor = backupSize
-    ? backupSize / baseSize
-    : unSizeMultiplicationFactor;
-
-  return {
-    clearLocalStorage,
-    multiplicationFactor,
-  };
-})(
-  withTranslation(["PreparationPortal", "Common"])(observer(PreparationPortal)),
-);
+export default PreparationPortal;
--- packages/shared/utils/pageSessionStorages.ts
+++ packages/shared/utils/pageSessionStorages.ts
@@ -24,8 +24,6 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-export interface IPreparationPortal {
-  isDialog?: boolean;
-  withoutHeader?: boolean;
-  style?: React.CSSProperties;
-}
+export const removeLocalStorageItems = (keysToRemove: string[]) => {
+  return keysToRemove.forEach((k) => localStorage.removeItem(k));
+};

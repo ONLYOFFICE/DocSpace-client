70c3a24e4f7b4c089fb0c43989a3fdf22a8ffcc2
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2024-05-31 21:34:29 +0500

    Doceditor: Added CompletedForm page

---
+++ packages/doceditor/src/components/completed-form/CompletedForm.tsx
@@ -0,0 +1,98 @@
+// (c) Copyright Ascensio System SIA 2009-2024
+//
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+//
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+//
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+//
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+//
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+//
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+"use client";
+
+import React, { useMemo } from "react";
+import { ReactSVG } from "react-svg";
+import { useTheme } from "styled-components";
+import { useTranslation } from "react-i18next";
+
+import { Text } from "@docspace/shared/components/text";
+import { getBgPattern, getLogoUrl } from "@docspace/shared/utils/common";
+
+import CompletedFormDarkIcon from "PUBLIC_DIR/images/completedForm/completed.form.icon.dark.svg?url";
+import CompletedFormLightIcon from "PUBLIC_DIR/images/completedForm/completed.form.icon.light.svg?url";
+
+import {
+  CompletedFormLayout,
+  ButtonWrapper,
+  TextWrapper,
+} from "./CompletedForm.styled";
+import type { CompletedFormProps } from "./CompletedForm.types";
+import { Button } from "@docspace/shared/components/button";
+import Link from "next/link";
+import { WhiteLabelLogoType } from "@docspace/shared/enums";
+import Image from "next/image";
+
+export const CompletedForm = ({}: CompletedFormProps) => {
+  const theme = useTheme();
+  const { t } = useTranslation(["CompletedForm"]);
+
+  const { logoUrl, smallLogoUrl } = useMemo(() => {
+    const logoUrl = getLogoUrl(WhiteLabelLogoType.LoginPage, !theme.isBase);
+    const smallLogoUrl = getLogoUrl(
+      WhiteLabelLogoType.LightSmall,
+      !theme.isBase,
+    );
+
+    return { logoUrl, smallLogoUrl };
+  }, [theme.isBase]);
+
+  const bgPattern = useMemo(
+    () => getBgPattern(theme.currentColorScheme?.id),
+    [theme.currentColorScheme?.id],
+  );
+
+  const iconUrl = theme.isBase ? CompletedFormLightIcon : CompletedFormDarkIcon;
+
+  return (
+    <CompletedFormLayout bgPattern={bgPattern}>
+      <picture className="completed-form__logo">
+        <source media="(max-width: 600px)" srcSet={smallLogoUrl} />
+        <source media="(min-width: 601px)" srcSet={logoUrl} />
+        <img src={logoUrl} alt="logo" />
+      </picture>
+      <Image
+        src={iconUrl}
+        className="completed-form__icon"
+        alt="icon"
+        width={416}
+        height={200}
+      />
+      <TextWrapper>
+        <Text as="h1">{t("CompletedForm:Title")}</Text>
+        <Text as="p">{t("CompletedForm:Description")}</Text>
+      </TextWrapper>
+      <ButtonWrapper>
+        <Button scale label={t("CompletedForm:BackToRoom")} primary />
+        <Button scale label={t("CompletedForm:FillItOutAgain")} />
+      </ButtonWrapper>
+      <Link className="link" href="#">
+        {t("CompletedForm:GoToCompleteFolder")}
+      </Link>
+    </CompletedFormLayout>
+  );
+};

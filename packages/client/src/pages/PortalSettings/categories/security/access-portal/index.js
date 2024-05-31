b0a57ca52c40ded7d81da8089a22b36ace80d310
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2024-05-31 15:35:01 +0300

    Added interpolation for 'AdminsMessageSettingDescription' key.

--- packages/client/src/pages/PortalSettings/categories/security/access-portal/index.js
+++ packages/client/src/pages/PortalSettings/categories/security/access-portal/index.js
@@ -42,6 +42,7 @@ import { size } from "@docspace/shared/utils";
 import { inject, observer } from "mobx-react";
 import { Link } from "@docspace/shared/components/link";
 import { DeviceType } from "@docspace/shared/enums";
+import { PORTAL } from "@docspace/shared/constants";
 
 const AccessPortal = (props) => {
   const {
@@ -173,7 +174,7 @@ const AccessPortal = (props) => {
       </Text>
       <div className="category-item-description">
         <Text fontSize="13px" fontWeight="400">
-          {t("AdminsMessageSettingDescription")}
+          {t("AdminsMessageSettingDescription", { portalName: PORTAL })}
         </Text>
         <Text fontSize="13px" fontWeight="400">
           <Trans t={t} i18nKey="SaveToApply" />

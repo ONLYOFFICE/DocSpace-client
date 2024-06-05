dbede6ca0130bd9a97b794655bb362ddaae5ffc2
gopienkonikita <nikita.gopienko@onlyoffice.com>
2024-06-05 15:52:38 +0300

    Web: Hotkeys: fixed translations

--- packages/client/src/components/panels/HotkeysPanel/ActionsBlock.js
+++ packages/client/src/components/panels/HotkeysPanel/ActionsBlock.js
@@ -34,7 +34,7 @@ const ActionsBlock = ({ t, textStyles, keyTextStyles, CtrlKey }) => {
       <Row className="hotkeys_row">
         <>
           <Text {...textStyles}>{t("HotkeysOpen")}</Text>
-          <Text {...keyTextStyles}>{t("HotkeysEnterKey")}</Text>
+          <Text {...keyTextStyles}>Enter</Text>
         </>
       </Row>
       <Row className="hotkeys_row">

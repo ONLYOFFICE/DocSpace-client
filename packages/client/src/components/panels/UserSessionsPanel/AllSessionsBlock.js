f713c56f508bca0cabe466e53a3fcdd521482e63
Elyor Djalilov <elyor.djalilov@onlyoffice.com>
2024-06-19 20:02:37 +0500

    added empty screen

--- packages/client/src/components/panels/UserSessionsPanel/AllSessionsBlock.js
+++ packages/client/src/components/panels/UserSessionsPanel/AllSessionsBlock.js
@@ -1,11 +1,19 @@
 import { observer, inject } from "mobx-react";
 import { Text } from "@docspace/shared/components/text";
 import { Button } from "@docspace/shared/components/button";
+import { EmptyScreenContainer } from "@docspace/shared/components/empty-screen-container";
 import styled from "styled-components";
 import RowWrapper from "./sub-components";
 
+import EmptyScreenSessionsReactSvgUrl from "PUBLIC_DIR/images/empty_screen_from_sessions.svg?url";
+
 const Wrapper = styled.div`
   padding: 20px 20px 12px;
+
+  .empty-screen-container {
+    width: auto;
+    padding-top: 60px;
+  }
 
   .subtitle {
     font-size: 14px;
@@ -28,7 +36,6 @@ const AllSessionsBlock = (props) => {
     onClickLogoutAllExceptThis,
   } = props;
 
-  const isDisabled = connections.length > 0;
   const exceptId = userLastSession.connections[0]?.id;
 
   return (
@@ -36,14 +43,22 @@ const AllSessionsBlock = (props) => {
       <Wrapper>
         <Text className="subtitle">{t("Profile:AllSessions")}</Text>
         <Text className="desciption">{t("Profile:PanelDescription")}</Text>
-        <Button
-          label={t("Profile:LogoutFromAllSessions")}
-          size="small"
-          onClick={() => onClickLogoutAllExceptThis(t, exceptId)}
-          scale={true}
-          isLoading={isLoading}
-          isDisabled={!isDisabled}
-        />
+        {connections.length > 0 ? (
+          <Button
+            label={t("Profile:LogoutFromAllSessions")}
+            size="small"
+            onClick={() => onClickLogoutAllExceptThis(t, exceptId)}
+            scale={true}
+            isLoading={isLoading}
+          />
+        ) : (
+          <EmptyScreenContainer
+            imageSrc={EmptyScreenSessionsReactSvgUrl}
+            className="empty-screen-container"
+            imageAlt="Empty Screen Sessions image"
+            headerText={t("Settings:NoSessionsHere")}
+          />
+        )}
       </Wrapper>
 
       <RowWrapper t={t} connections={connections} />

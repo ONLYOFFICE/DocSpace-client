3663185b662886dbc04a98caa752a3865a80aa43
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2024-07-11 18:25:32 +0200

    DocEditor: refactor rtl

--- packages/doceditor/src/components/deep-link/DeepLink.styled.ts
+++ packages/doceditor/src/components/deep-link/DeepLink.styled.ts
@@ -97,10 +97,7 @@ export const BgBlock = styled.div<{ bgPattern: string }>`
   background-attachment: fixed;
   background-size: cover;
   position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
+  inset: 0;
   z-index: 0;
 
   @media ${mobile} {

ad867802e24ab165f7f93cf8cb2a2ee484dbddb9
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-07-19 15:11:42 +0400

    Login:Components:WizardForm: add use client

--- packages/login/src/components/WizardForm/WizardForm.styled.ts
+++ packages/login/src/components/WizardForm/WizardForm.styled.ts
@@ -24,6 +24,8 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
+"use client";
+
 import styled from "styled-components";
 import { mobile, tablet } from "@docspace/shared/utils/device";
 

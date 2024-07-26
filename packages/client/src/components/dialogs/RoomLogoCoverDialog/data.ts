480b94a1231afed8af367a152373f5f90e6c6e12
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2024-07-26 20:19:45 +0500

    Client: added room cover icons

---
+++ packages/client/src/components/dialogs/RoomLogoCoverDialog/data.ts
@@ -0,0 +1,65 @@
+// (c) Copyright Ascensio System SIA 2009-2024
+//
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+//
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+//
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+//
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+//
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+//
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+import AppsSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.apps.react.svg?url";
+import DataManagementSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-data-management.svg?url";
+import CatalogFavoritesSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.favorites.react.svg?url";
+import CatalogRecentSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.recent.react.svg?url";
+import CatalogCrmSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.crm.react.svg?url";
+import CatalogSettingsDeveloperSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-developer.svg?url";
+import CatalogFolderSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.folder.react.svg?url";
+import CatalogSettingsGiftSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-gift.svg?url";
+import CatalogArchiveSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.archive.react.svg?url";
+import CatalogSettingsRestoreSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-restore.svg?url";
+import CatalogSettingsPaymentSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-payment.svg?url";
+import CatalogAccountsSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.accounts.react.svg?url";
+import CatalogUserSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.user.react.svg?url";
+import CatalogPortfolioSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-portfolio.react.svg?url";
+import CatalogSearchSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-search.react.svg?url";
+import CatalogSettingsSecuritySvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-security.svg?url";
+import CatalogSpySvgUrl from "PUBLIC_DIR/images/icons/20/catalog.spy.react.svg?url";
+import CatalogShareSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-share.react.svg?url";
+
+export const RoomCoverIcons = [
+  AppsSvgUrl,
+  DataManagementSvgUrl,
+  CatalogFavoritesSvgUrl,
+  CatalogRecentSvgUrl,
+  CatalogCrmSvgUrl,
+  CatalogSettingsDeveloperSvgUrl,
+  CatalogFolderSvgUrl,
+  CatalogSettingsGiftSvgUrl,
+  CatalogArchiveSvgUrl,
+  CatalogSettingsRestoreSvgUrl,
+  CatalogSettingsPaymentSvgUrl,
+  CatalogAccountsSvgUrl,
+  CatalogUserSvgUrl,
+  CatalogPortfolioSvgUrl,
+  CatalogSearchSvgUrl,
+  CatalogSettingsSecuritySvgUrl,
+  CatalogSpySvgUrl,
+  CatalogShareSvgUrl,
+];

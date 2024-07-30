de20aa95002f4cf0ba99e06c0592527cfbc13aad
Vlada Gazizova <94864088+gazizova-vlada@users.noreply.github.com>
2024-07-30 11:17:46 +0300

    Client:Fixed page crash.

--- packages/client/src/store/ContextOptionsStore.js
+++ packages/client/src/store/ContextOptionsStore.js
@@ -1942,7 +1942,7 @@ class ContextOptionsStore {
       selection.findIndex((k) => k.security.Download) !== -1;
 
     const favoriteItems = selection.filter((k) =>
-      k.contextOptions.includes("mark-as-favorite"),
+      k.contextOptions?.includes("mark-as-favorite"),
     );
 
     const moveItems = selection.filter((k) =>

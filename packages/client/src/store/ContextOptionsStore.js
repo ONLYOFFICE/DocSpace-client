91323ffb1f00e74638db8cc43bff870ff98b604a
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2024-08-07 18:32:05 +0500

    Client: Fixed fill form option

--- packages/client/src/store/ContextOptionsStore.js
+++ packages/client/src/store/ContextOptionsStore.js
@@ -188,7 +188,11 @@ class ContextOptionsStore {
   };
 
   onClickLinkFillForm = (item) => {
-    if (!item.startFilling && item.isPDFForm)
+    const isFormRoom =
+      this.selectedFolderStore?.roomType === RoomsType.FormRoom ||
+      this.selectedFolderStore?.parentRoomType === FolderType.FormRoom;
+
+    if (!item.startFilling && item.isPDFForm && !isFormRoom)
       return this.dialogsStore.setFillPDFDialogData(true, item);
 
     return this.gotoDocEditor(false, item);

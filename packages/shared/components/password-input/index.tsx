cc3fc541d60bb6fb2adadc2b5e800f9d72b15498
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-08-12 10:53:00 +0400

    Shared:Components: change import types

--- packages/shared/components/password-input/index.tsx
+++ packages/shared/components/password-input/index.tsx
@@ -24,6 +24,6 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-export { PasswordInputHandle } from "./PasswordInput.types";
+export type { PasswordInputHandle } from "./PasswordInput.types";
 
 export { PasswordInput } from "./PasswordInput";

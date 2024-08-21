64ea57f0eb031c192432609893ede9ee41bb26cd
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2024-08-21 15:50:02 +0400

    Shared: restored Table.constants.ts

--- packages/client/src/store/AccountsHotkeysStore.ts
+++ packages/client/src/store/AccountsHotkeysStore.ts
@@ -28,7 +28,7 @@ import { makeAutoObservable } from "mobx";
 import { isMobile } from "@docspace/shared/utils";
 import { checkDialogsOpen } from "@docspace/shared/utils/checkDialogsOpen";
 import { TUser, TUserGroup } from "@docspace/shared/api/people/types";
-import { TABLE_HEADER_HEIGHT } from "@docspace/shared/utils/device";
+import { TABLE_HEADER_HEIGHT } from "@docspace/shared/components/table/Table.constants";
 
 type AccountsType = TUser | TUserGroup;
 

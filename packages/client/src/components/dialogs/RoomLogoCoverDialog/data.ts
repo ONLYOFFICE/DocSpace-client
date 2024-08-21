d9a7288de3879bb4af11b5a6ef76f0aa57d0b43c
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2024-08-21 15:56:20 +0500

    Client: RoomLogoCoverDialog: import new icons

--- packages/client/src/components/dialogs/RoomLogoCoverDialog/data.ts
+++ packages/client/src/components/dialogs/RoomLogoCoverDialog/data.ts
@@ -24,42 +24,118 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import AppsSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.apps.react.svg?url";
-import DataManagementSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-data-management.svg?url";
-import CatalogFavoritesSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.favorites.react.svg?url";
-import CatalogRecentSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.recent.react.svg?url";
-import CatalogCrmSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.crm.react.svg?url";
-import CatalogSettingsDeveloperSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-developer.svg?url";
 import CatalogFolderSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.folder.react.svg?url";
 import CatalogSettingsGiftSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-gift.svg?url";
 import CatalogArchiveSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.archive.react.svg?url";
-import CatalogSettingsRestoreSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-restore.svg?url";
-import CatalogSettingsPaymentSvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-payment.svg?url";
-import CatalogAccountsSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.accounts.react.svg?url";
-import CatalogUserSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.user.react.svg?url";
-import CatalogPortfolioSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-portfolio.react.svg?url";
-import CatalogSearchSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-search.react.svg?url";
-import CatalogSettingsSecuritySvgUrl from "PUBLIC_DIR/images/icons/20/catalog-settings-security.svg?url";
-import CatalogSpySvgUrl from "PUBLIC_DIR/images/icons/20/catalog.spy.react.svg?url";
 import CatalogShareSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-share.react.svg?url";
+import CatalogLayersSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-layers.react.svg?url";
+import CatalogFlagSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-flag.react.svg?url";
+import CatalogHouseSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-house.react.svg?url";
+import CatalogLifeBuoySvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-life_buoy.react.svg?url";
+import CatalogBellSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-bell.react.svg?url";
+import CatalogBookmarkSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-bookmark.react.svg?url";
+import CatalogMedalSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-medal.react.svg?url";
+import CatalogStarSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-star.react.svg?url";
+import CatalogUmbrellaSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-umbrella.react.svg?url";
+import CatalogCloudSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-cloud.react.svg?url";
+import CatalogZapSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-zap.react.svg?url";
+import CatalogHeartSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-heart.react.svg?url";
+import CatalogAnchorSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-anchor.react.svg?url";
+import CatalogLockSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-lock.react.svg?url";
+import CatalogPlanetSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-planet.react.svg?url";
+import CatalogShieldSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-shield.react.svg?url";
+import CatalogHumanSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-human.react.svg?url";
+import CatalogMailSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-mail.react.svg?url";
+import CatalogImgSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-img.react.svg?url";
+import CatalogFilmSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-film.react.svg?url";
+import CatalogCalendarSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-calendar.react.svg?url";
+import CatalogClipboardSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-clipboard.react.svg?url";
+import CatalogFileEmptySvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-file_empty.react.svg?url";
+import CatalogFile_1_SvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-file_1.react.svg?url";
+import CatalogFile_2_SvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-file_2.react.svg?url";
+import CatalogFile_3_SvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-file_3.react.svg?url";
+import CatalogBookOpenSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-book_open.react.svg?url";
+import CatalogFeatherSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-feather.react.svg?url";
+import CatalogPenSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-pen.react.svg?url";
+import CatalogCameraSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-camera.react.svg?url";
+import CatalogVideoCameraSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-video_camera.react.svg?url";
+import CatalogHeadphonesSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-headphones.react.svg?url";
+import CatalogPhoneSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-phone.react.svg?url";
+import CatalogLoupeSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-loupe.react.svg?url";
+import CatalogTruckSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-truck.react.svg?url";
+import CatalogPackageSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-package.react.svg?url";
+import CatalogLabelSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-label.react.svg?url";
+import CatalogWatchSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-watch.react.svg?url";
+import CatalogScheduleSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-schedule.react.svg?url";
+import CatalogComment_1_SvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-comment_1.react.svg?url";
+import CatalogComment_2_SvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-comment_2.react.svg?url";
+import CatalogMapSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-map.react.svg?url";
+import CatalogDollarSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-dollar.react.svg?url";
+import CatalogCartSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-cart.react.svg?url";
+import CatalogPercentSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-percent.react.svg?url";
+import CatalogHashSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-hash.react.svg?url";
+import CatalogCheckSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-check.react.svg?url";
+import CatalogSlidersSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-sliders.react.svg?url";
+import CatalogSuitcaseSvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-suitcase.react.svg?url";
+import CatalogFace_1_SvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-face_1.react.svg?url";
+import CatalogFace_2_SvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-face_2.react.svg?url";
+import CatalogFace_3_SvgUrl from "PUBLIC_DIR/images/icons/20/catalog.cover-face_3.react.svg?url";
 
 export const RoomCoverIcons = [
-  AppsSvgUrl,
-  DataManagementSvgUrl,
-  CatalogFavoritesSvgUrl,
-  CatalogRecentSvgUrl,
-  CatalogCrmSvgUrl,
-  CatalogSettingsDeveloperSvgUrl,
-  CatalogFolderSvgUrl,
-  CatalogSettingsGiftSvgUrl,
-  CatalogArchiveSvgUrl,
-  CatalogSettingsRestoreSvgUrl,
-  CatalogSettingsPaymentSvgUrl,
-  CatalogAccountsSvgUrl,
-  CatalogUserSvgUrl,
-  CatalogPortfolioSvgUrl,
-  CatalogSearchSvgUrl,
-  CatalogSettingsSecuritySvgUrl,
-  CatalogSpySvgUrl,
+  CatalogLayersSvgUrl,
+  CatalogFlagSvgUrl,
+  CatalogHouseSvgUrl,
+  CatalogLifeBuoySvgUrl,
+  CatalogBellSvgUrl,
+  CatalogBookmarkSvgUrl,
+  CatalogMedalSvgUrl,
+  CatalogStarSvgUrl,
+  CatalogUmbrellaSvgUrl,
+  CatalogCloudSvgUrl,
+  CatalogZapSvgUrl,
+  CatalogHeartSvgUrl,
+  CatalogAnchorSvgUrl,
+  CatalogLockSvgUrl,
+  CatalogPlanetSvgUrl,
   CatalogShareSvgUrl,
+  CatalogShieldSvgUrl,
+  CatalogHumanSvgUrl,
+  CatalogSettingsGiftSvgUrl,
+  CatalogMailSvgUrl,
+  CatalogImgSvgUrl,
+  CatalogFilmSvgUrl,
+  CatalogCalendarSvgUrl,
+  CatalogClipboardSvgUrl,
+  CatalogFolderSvgUrl,
+  CatalogFileEmptySvgUrl,
+  CatalogFile_1_SvgUrl,
+  CatalogFile_2_SvgUrl,
+  CatalogFile_3_SvgUrl,
+  CatalogBookOpenSvgUrl,
+  CatalogFeatherSvgUrl,
+  CatalogPenSvgUrl,
+  CatalogCameraSvgUrl,
+  CatalogVideoCameraSvgUrl,
+  CatalogHeadphonesSvgUrl,
+  CatalogPhoneSvgUrl,
+  CatalogLoupeSvgUrl,
+  CatalogTruckSvgUrl,
+  CatalogPackageSvgUrl,
+  CatalogLabelSvgUrl,
+  CatalogWatchSvgUrl,
+  CatalogScheduleSvgUrl,
+  CatalogComment_1_SvgUrl,
+  CatalogComment_2_SvgUrl,
+  CatalogMapSvgUrl,
+  CatalogDollarSvgUrl,
+  CatalogCartSvgUrl,
+  CatalogPercentSvgUrl,
+  CatalogHashSvgUrl,
+  CatalogCheckSvgUrl,
+  CatalogSlidersSvgUrl,
+  CatalogSuitcaseSvgUrl,
+  CatalogFace_1_SvgUrl,
+  CatalogFace_2_SvgUrl,
+  CatalogFace_3_SvgUrl,
+  CatalogArchiveSvgUrl,
 ];

b7a0551cc3ba26e1f61fb6ee6f950636d9a8c013
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-08-26 10:51:58 +0400

    Client:Packages:Login: Some fixes

--- packages/login/playwright.config.ts
+++ packages/login/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "http://127.0.0.1:5111",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -71,7 +71,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "yarn test:build && yarn test:start",
-    port: 5011,
+    command: "yarn test:start",
+    port: 5111,
+    timeout: 1000 * 60 * 5,
   },
 });

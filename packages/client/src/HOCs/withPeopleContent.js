b93697cf36ce4221dc6ae4afa2d672e1464b476c
Elyor Djalilov <elyor.djalilov@onlyoffice.com>
2024-08-26 21:23:19 +0500

    Web: Client: HOCs. fix onContextClick

--- packages/client/src/HOCs/withPeopleContent.js
+++ packages/client/src/HOCs/withPeopleContent.js
@@ -62,11 +62,9 @@ export default function withContent(WrappedContent) {
 
     const onContextClick = useCallback(
       (item, isSingleMenu) => {
-        if (isSingleMenu) {
-          setTimeout(() => singleContextMenuAction(item), 0);
-        } else {
-          setTimeout(() => multipleContextMenuAction(item), 0);
-        }
+        isSingleMenu
+          ? singleContextMenuAction(item)
+          : multipleContextMenuAction(item);
       },
       [singleContextMenuAction, multipleContextMenuAction],
     );

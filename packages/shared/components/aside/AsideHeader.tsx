c0382bf23490700f190c2f229471b1a7986f8989
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-08-27 17:11:28 +0300

    Shared:Components:Aside: fix react not found

--- packages/shared/components/aside/AsideHeader.tsx
+++ packages/shared/components/aside/AsideHeader.tsx
@@ -24,6 +24,8 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
+import React from "react";
+
 import ArrowPathReactSvgUrl from "PUBLIC_DIR/images/arrow.path.react.svg?url";
 import CrossReactSvgUrl from "PUBLIC_DIR/images/icons/17/cross.react.svg?url";
 

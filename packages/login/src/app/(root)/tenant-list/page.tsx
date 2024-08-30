077766f987c0b121caca8892a084cebcd18f16a8
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-08-30 14:52:01 +0400

    Login:App:TenantList: fix import

--- packages/login/src/app/(root)/tenant-list/page.tsx
+++ packages/login/src/app/(root)/tenant-list/page.tsx
@@ -1,10 +1,11 @@
 import { cookies } from "next/headers";
 
 import { GreetingLoginContainer } from "@/components/GreetingContainer";
-import TenantList from "@/components/TenantList";
 import { getSettings } from "@/utils/actions";
 import { ColorTheme, ThemeId } from "@docspace/shared/components/color-theme";
 import { LANGUAGE } from "@docspace/shared/constants";
+
+import TenantList from "./page.client";
 
 export default async function Page({
   searchParams,

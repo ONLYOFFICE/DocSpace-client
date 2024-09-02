e7efda748d8028e3f2d5457391220d75d3b90292
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-09-02 11:53:12 +0400

    Login: remove space from folder name

--- packages/login/__tests__/example.spec.ts
+++ packages/login/__tests__/example.spec.ts
@@ -24,7 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import { expect, test } from "./fixtures /base";
+import { expect, test } from "./fixtures/base";
 import { endpoints } from "./mocking/endpoints";
 
 test("wizard success", async ({ page, mockRequest }) => {

cd385a2aaaeefae5b4ba71f51799c06ff4205339
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-09-02 11:47:09 +0400

    Login: add snapshotPathTemplate and change browser

--- packages/login/playwright.config.ts
+++ packages/login/playwright.config.ts
@@ -30,15 +30,15 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
+  snapshotPathTemplate: "{testDir}/screenshots{/projectName}/{arg}{ext}",
 
   /* Configure projects for major browsers */
   projects: [
-    /*     {
+    {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    {
+    /*     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
@@ -46,27 +46,7 @@ export default defineConfig({
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
-    }, */
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    {
-      name: "Google Chrome",
-      use: { ...devices["Desktop Chrome"], channel: "chrome" },
-    },
+    },  */
   ],
 
   /* Run your local dev server before starting the tests */

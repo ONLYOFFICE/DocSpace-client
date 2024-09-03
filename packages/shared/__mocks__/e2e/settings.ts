517e77cd72de8750d07e833a550fb51e3e60a707
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-09-04 03:57:49 +0400

    Shared:Mocks:e2e: add new mock data

--- packages/shared/__mocks__/e2e/settings.ts
+++ packages/shared/__mocks__/e2e/settings.ts
@@ -1,51 +1,93 @@
-export const settingsSuccessNoAuth = new Response(
-  JSON.stringify({
-    response: {
-      trustedDomainsType: 0,
-      culture: "en-GB",
-      utcOffset: "00:00:00",
-      utcHoursOffset: 0,
-      greetingSettings: "Web Office",
-      ownerId: "00000000-0000-0000-0000-000000000000",
-      enabledJoin: false,
-      enableAdmMess: false,
-      thirdpartyEnable: false,
-      docSpace: true,
-      standalone: true,
-      baseDomain: "localhost",
-      passwordHash: {
-        size: 256,
-        iterations: 100000,
-        salt: "4d9abe238e2f7b14a30a4565d62214a795a15abb798ed61118a69820d6a6146c",
-      },
-      version: ".",
-      recaptchaType: 0,
-      recaptchaPublicKey: "",
-      debugInfo: false,
-      tenantStatus: 0,
-      tenantAlias: "localhost",
-      forumLink: "https://forum.onlyoffice.com",
-      legalTerms:
-        "https://help.onlyoffice.co/products/files/doceditor.aspx?fileid=5048502&doc=SXhWMEVzSEYxNlVVaXJJeUVtS0kyYk14YWdXTEFUQmRWL250NllHNUFGbz0_IjUwNDg1MDIi0",
-      cookieSettingsEnabled: false,
-      limitedAccessSpace: false,
-      userNameRegex: "^[\\p{L}\\p{M}' \\-]+$",
-      maxImageUploadSize: 0,
+export const settingsSuccessNoAuth = {
+  response: {
+    trustedDomainsType: 0,
+    culture: "en-GB",
+    utcOffset: "00:00:00",
+    utcHoursOffset: 0,
+    greetingSettings: "Web Office",
+    ownerId: "00000000-0000-0000-0000-000000000000",
+    enabledJoin: false,
+    enableAdmMess: false,
+    thirdpartyEnable: false,
+    docSpace: true,
+    standalone: true,
+    baseDomain: "localhost",
+    passwordHash: {
+      size: 256,
+      iterations: 100000,
+      salt: "4d9abe238e2f7b14a30a4565d62214a795a15abb798ed61118a69820d6a6146c",
     },
-    count: 1,
-    links: [
-      {
-        href: "http://192.168.0.16/api/2.0/settings?withPassword=false",
-        action: "GET",
-      },
-    ],
-    status: 0,
-    statusCode: 200,
-  }),
-  { status: 200 },
-);
+    version: ".",
+    recaptchaType: 0,
+    recaptchaPublicKey: "",
+    debugInfo: false,
+    tenantStatus: 0,
+    tenantAlias: "localhost",
+    forumLink: "https://forum.onlyoffice.com",
+    legalTerms:
+      "https://help.onlyoffice.co/products/files/doceditor.aspx?fileid=5048502&doc=SXhWMEVzSEYxNlVVaXJJeUVtS0kyYk14YWdXTEFUQmRWL250NllHNUFGbz0_IjUwNDg1MDIi0",
+    cookieSettingsEnabled: false,
+    limitedAccessSpace: false,
+    userNameRegex: "^[\\p{L}\\p{M}' \\-]+$",
+    maxImageUploadSize: 0,
+  },
+  count: 1,
+  links: [
+    {
+      href: "http://192.168.0.16/api/2.0/settings?withPassword=false",
+      action: "GET",
+    },
+  ],
+  status: 0,
+  statusCode: 200,
+};
 
-export const settingsSuccessNoAuthWizard = {
+export const settingsSuccessWithAuth = {
+  response: {
+    trustedDomainsType: 0,
+    culture: "en-US",
+    utcOffset: "00:00:00",
+    utcHoursOffset: 0,
+    greetingSettings: "Web Office",
+    ownerId: "00000000-0000-0000-0000-000000000000",
+    enabledJoin: false,
+    enableAdmMess: false,
+    thirdpartyEnable: false,
+    docSpace: true,
+    standalone: true,
+    baseDomain: "localhost",
+    passwordHash: {
+      size: 256,
+      iterations: 100000,
+      salt: "4d9abe238e2f7b14a30a4565d62214a795a15abb798ed61118a69820d6a6146c",
+    },
+    version: ".",
+    recaptchaType: 0,
+    recaptchaPublicKey: "",
+    debugInfo: false,
+    tenantStatus: 0,
+    tenantAlias: "localhost",
+    forumLink: "https://forum.onlyoffice.com",
+    legalTerms:
+      "https://help.onlyoffice.co/products/files/doceditor.aspx?fileid=5048502&doc=SXhWMEVzSEYxNlVVaXJJeUVtS0kyYk14YWdXTEFUQmRWL250NllHNUFGbz0_IjUwNDg1MDIi0",
+    licenseUrl: "https://www.gnu.org/licenses/agpl-3.0.en.html",
+    cookieSettingsEnabled: false,
+    limitedAccessSpace: false,
+    userNameRegex: "^[\\p{L}\\p{M}' \\-]+$",
+    maxImageUploadSize: 0,
+  },
+  count: 1,
+  links: [
+    {
+      href: "http://192.168.50.219/api/2.0/settings?withPassword=true",
+      action: "GET",
+    },
+  ],
+  status: 0,
+  statusCode: 200,
+};
+
+export const settingsSuccessWithAuthWizard = {
   response: {
     trustedDomainsType: 0,
     culture: "en-US",
@@ -129,8 +171,22 @@ export const machineNameSuccess = {
   ok: true,
 };
 
-export const isLicenseRequiredSuccess = {
+export const isLicenseRequiredFalseSuccess = {
   response: false,
+  count: 1,
+  links: [
+    {
+      href: "http://192.168.50.219/api/2.0/settings/license/required",
+      action: "GET",
+    },
+  ],
+  status: 0,
+  statusCode: 200,
+  ok: true,
+};
+
+export const isLicenseRequiredTrueSuccess = {
+  response: true,
   count: 1,
   links: [
     {
@@ -341,16 +397,22 @@ export const settingsNotFound = new Response(null, { status: 404 });
 
 export const settingsError = new Response(null, { status: 500 });
 
-const getMockSettingsResponse = (withPassword: boolean): Response => {
-  if (!withPassword) return settingsSuccessNoAuth;
+const getMockResponse = (response: any, headers?: any): Response => {
+  let responseData = response;
+  // TODO: remove this when MSW the server is added
+  if (headers) {
+    if (
+      headers.get("x-test-data-license-required") &&
+      responseData.links[0].href ===
+        "http://192.168.50.219/api/2.0/settings/license/required"
+    ) {
+      responseData = isLicenseRequiredTrueSuccess;
+    }
+  }
 
-  return settingsSuccessNoAuth;
-};
-
-export const getMockResponse = (response: any): Response => {
-  return new Response(JSON.stringify(response), {
+  return new Response(JSON.stringify(responseData), {
     status: 200,
   });
 };
 
-export default getMockSettingsResponse;
+export default getMockResponse;

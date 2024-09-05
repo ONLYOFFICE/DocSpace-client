18f724019108c3ee41e380cef1261e96222b0aff
Timofey Boyko <timofey.boyko@onlyoffice.com>
2024-09-05 12:27:45 +0300

    Login: change playwright config for windows tests

--- packages/login/playwright.config.ts
+++ packages/login/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html", { outputFolder: "../../playwright-report/login" }]],
-  
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -39,7 +39,6 @@ export default defineConfig({
   expect: {
     toHaveScreenshot: {
       maxDiffPixelRatio: 0.01,
-      maxDiffPixels: 10,
     },
   },
 

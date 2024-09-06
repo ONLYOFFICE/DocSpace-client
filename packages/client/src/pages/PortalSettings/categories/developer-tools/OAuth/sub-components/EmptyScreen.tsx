79a10a50650e7663f280781e7ae3c4add6cf1af4
Timofey <timofey.boyko@onlyoffice.com>
2024-09-06 16:44:04 +0800

    Client:OAuth2: change empty screen to new layouts

---
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/OAuth/sub-components/EmptyScreen.tsx
@@ -0,0 +1,49 @@
+import { useTranslation } from "react-i18next";
+import { useTheme } from "styled-components";
+
+import { EmptyView } from "@docspace/shared/components/empty-view";
+import { Text } from "@docspace/shared/components/text";
+import { globalColors } from "@docspace/shared/themes";
+
+import EmptyScreenOauthLightSvg from "PUBLIC_DIR/images/emptyview/empty.oauth2.light.svg";
+import EmptyScreenOauthDarkSvg from "PUBLIC_DIR/images/emptyview/empty.oauth2.dark.svg";
+
+import RegisterNewButton from "./RegisterNewButton";
+
+const OAuthEmptyScreen = () => {
+  const { t } = useTranslation(["OAuth"]);
+  const theme = useTheme();
+
+  const icon = theme.isBase ? (
+    <EmptyScreenOauthLightSvg />
+  ) : (
+    <EmptyScreenOauthDarkSvg />
+  );
+
+  const description = (
+    <div>
+      <Text
+        lineHeight="16px"
+        fontSize="12px"
+        fontWeight={400}
+        color={theme.isBase ? globalColors.grayText : globalColors.darkGrayDark}
+        textAlign="center"
+        style={{ marginBottom: "20px" }}
+      >
+        {t("OAuthAppDescription")}
+      </Text>{" "}
+      <RegisterNewButton />
+    </div>
+  );
+
+  return (
+    <EmptyView
+      icon={icon}
+      title={t("NoOAuthAppHeader")}
+      description={description}
+      options={[]}
+    />
+  );
+};
+
+export default OAuthEmptyScreen;

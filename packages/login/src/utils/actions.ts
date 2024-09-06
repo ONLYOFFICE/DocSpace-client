83ddf485197b0658349aae04afe5bb87b5c7f078
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-09-06 11:47:53 +0400

    Login:Src:Utils: add handler for checkConfirmLink request

--- packages/login/src/utils/actions.ts
+++ packages/login/src/utils/actions.ts
@@ -61,6 +61,7 @@ import {
   portalPasswordSettingHandler,
   machineNameHandler,
   portalTimeZoneHandler,
+  confirmHandler,
 } from "@docspace/shared/__mocks__/e2e";
 
 const IS_TEST = process.env.TEST;
@@ -299,6 +300,7 @@ export async function getPortalPasswordSettings(
   if (!res.ok) return;
 
   const passwordSettings = await res.json();
+  console.log("passwordSettings", passwordSettings);
 
   return passwordSettings.response as TPasswordSettings;
 }
@@ -402,11 +404,15 @@ export async function checkConfirmLink(data: TConfirmLinkParams) {
     JSON.stringify(data),
   );
 
-  const response = await fetch(checkConfirmLink);
+  const response = IS_TEST
+    ? confirmHandler(headers())
+    : await fetch(checkConfirmLink);
+  console.log("response", response);
 
   if (!response.ok) throw new Error(response.statusText);
 
   const result = await response.json();
+  console.log("result", result);
 
   return result.response as TConfirmLinkResult;
 }

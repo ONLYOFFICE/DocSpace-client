f27e1b4eb097faf9b15747710cef3591749e14c3
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-09-10 13:20:05 +0400

    Shared:Mocks:e2e: move the responses into constants

--- packages/shared/__mocks__/e2e/handlers/authentication/confirm.ts
+++ packages/shared/__mocks__/e2e/handlers/authentication/confirm.ts
@@ -13,6 +13,22 @@ const PATH = "/authentication/confirm";
 
 const url = `${BASE_URL}/${API_PREFIX}/${PATH}`;
 
+const getConfirmSuccess = (result: 0 | 1 | 2 | 3 | 4 | 5 | 6 = 0) => {
+  return {
+    response: { result },
+    count: 1,
+    links: [
+      {
+        href: url,
+        action: "POST",
+      },
+    ],
+    status: 0,
+    statusCode: 200,
+    ok: true,
+  };
+};
+
 export const confirm = (headers?: Headers): Response => {
   let isInvalid = false;
   let isExpired = false;
@@ -41,126 +57,28 @@ export const confirm = (headers?: Headers): Response => {
   }
 
   if (isInvalid) {
-    return new Response(
-      JSON.stringify({
-        response: { result: 1 },
-        count: 1,
-        links: [
-          {
-            href: url,
-            action: "POST",
-          },
-        ],
-        status: 0,
-        statusCode: 200,
-        ok: true,
-      }),
-    );
+    return new Response(JSON.stringify(getConfirmSuccess(1)));
   }
 
   if (isExpired) {
-    return new Response(
-      JSON.stringify({
-        response: { result: 2 },
-        count: 1,
-        links: [
-          {
-            href: url,
-            action: "POST",
-          },
-        ],
-        status: 0,
-        statusCode: 200,
-        ok: true,
-      }),
-    );
+    return new Response(JSON.stringify(getConfirmSuccess(2)));
   }
 
   if (isTariffLimit) {
-    return new Response(
-      JSON.stringify({
-        response: { result: 3 },
-        count: 1,
-        links: [
-          {
-            href: url,
-            action: "POST",
-          },
-        ],
-        status: 0,
-        statusCode: 200,
-        ok: true,
-      }),
-    );
+    return new Response(JSON.stringify(getConfirmSuccess(3)));
   }
 
   if (isUserExisted) {
-    return new Response(
-      JSON.stringify({
-        response: { result: 4 },
-        count: 1,
-        links: [
-          {
-            href: url,
-            action: "POST",
-          },
-        ],
-        status: 0,
-        statusCode: 200,
-        ok: true,
-      }),
-    );
+    return new Response(JSON.stringify(getConfirmSuccess(4)));
   }
 
   if (isUserExcluded) {
-    return new Response(
-      JSON.stringify({
-        response: { result: 5 },
-        count: 1,
-        links: [
-          {
-            href: url,
-            action: "POST",
-          },
-        ],
-        status: 0,
-        statusCode: 200,
-        ok: true,
-      }),
-    );
+    return new Response(JSON.stringify(getConfirmSuccess(5)));
   }
 
   if (isQuotaFailed) {
-    return new Response(
-      JSON.stringify({
-        response: { result: 6 },
-        count: 1,
-        links: [
-          {
-            href: url,
-            action: "POST",
-          },
-        ],
-        status: 0,
-        statusCode: 200,
-        ok: true,
-      }),
-    );
+    return new Response(JSON.stringify(getConfirmSuccess(6)));
   }
 
-  return new Response(
-    JSON.stringify({
-      response: { result: 0 },
-      count: 1,
-      links: [
-        {
-          href: url,
-          action: "POST",
-        },
-      ],
-      status: 0,
-      statusCode: 200,
-      ok: true,
-    }),
-  );
+  return new Response(JSON.stringify(getConfirmSuccess()));
 };
--- packages/shared/__mocks__/e2e/handlers/settings/settings.ts
+++ packages/shared/__mocks__/e2e/handlers/settings/settings.ts
@@ -4,6 +4,100 @@ const PATH = "settings";
 
 const url = `${BASE_URL}/${API_PREFIX}/${PATH}`;
 
+export const settingsWizzard = {
+  response: {
+    trustedDomainsType: 0,
+    culture: "en-US",
+    utcOffset: "00:00:00",
+    utcHoursOffset: 0,
+    greetingSettings: "Web Office",
+    ownerId: "00000000-0000-0000-0000-000000000000",
+    enabledJoin: false,
+    enableAdmMess: false,
+    thirdpartyEnable: false,
+    docSpace: true,
+    standalone: true,
+    baseDomain: BASE_URL,
+    wizardToken:
+      "type=Wizard&key=462096722976.4OD9HUTNJZGBFMMPVOTCBNOVCPGLTBKUNPKGCNY&uid=66faa6e4-f133-11ea-b126-00ffeec8b4ef",
+    passwordHash: {
+      size: 256,
+      iterations: 100000,
+      salt: "4d9abe238e2f7b14a30a4565d62214a795a15abb798ed61118a69820d6a6146c",
+    },
+    version: ".",
+    recaptchaType: 0,
+    recaptchaPublicKey: "",
+    debugInfo: false,
+    tenantStatus: 0,
+    tenantAlias: "localhost",
+    forumLink: "https://forum.onlyoffice.com",
+    legalTerms:
+      "https://help.onlyoffice.co/products/files/doceditor.aspx?fileid=5048502&doc=SXhWMEVzSEYxNlVVaXJJeUVtS0kyYk14YWdXTEFUQmRWL250NllHNUFGbz0_IjUwNDg1MDIi0",
+    cookieSettingsEnabled: false,
+    limitedAccessSpace: false,
+    userNameRegex: "^[\\p{L}\\p{M}' \\-]+$",
+    maxImageUploadSize: 0,
+  },
+  count: 1,
+  links: [
+    {
+      href: url,
+      action: "GET",
+    },
+  ],
+  status: 0,
+  statusCode: 200,
+  ok: true,
+};
+
+export const settingsAuth = {};
+
+export const settingsNoAuth = {
+  response: {
+    trustedDomainsType: 0,
+    culture: "en-GB",
+    utcOffset: "00:00:00",
+    utcHoursOffset: 0,
+    greetingSettings: "Web Office",
+    ownerId: "00000000-0000-0000-0000-000000000000",
+    enabledJoin: false,
+    enableAdmMess: false,
+    thirdpartyEnable: false,
+    docSpace: true,
+    standalone: true,
+    baseDomain: "docspace.site",
+    passwordHash: {
+      size: 256,
+      iterations: 100000,
+      salt: "4d9abe238e2f7b14a30a4565d62214a795a15abb798ed61118a69820d6a6146c",
+    },
+    version: ".",
+    recaptchaType: 0,
+    recaptchaPublicKey: "",
+    debugInfo: false,
+    tenantStatus: 0,
+    tenantAlias: "localhost",
+    forumLink: "https://forum.onlyoffice.com",
+    legalTerms:
+      "https://help.onlyoffice.co/products/files/doceditor.aspx?fileid=5048502&doc=SXhWMEVzSEYxNlVVaXJJeUVtS0kyYk14YWdXTEFUQmRWL250NllHNUFGbz0_IjUwNDg1MDIi0",
+    licenseUrl: "https://www.gnu.org/licenses/agpl-3.0.en.html",
+    cookieSettingsEnabled: false,
+    limitedAccessSpace: false,
+    userNameRegex: "^[\\p{L}\\p{M}' \\-]+$",
+    maxImageUploadSize: 0,
+  },
+  count: 1,
+  links: [
+    {
+      href: url,
+      action: "GET",
+    },
+  ],
+  status: 0,
+  statusCode: 200,
+};
+
 export const settings = (headers?: Headers): Response => {
   let isWizard = false;
 
@@ -11,141 +105,7 @@ export const settings = (headers?: Headers): Response => {
     isWizard = true;
   }
 
-  if (isWizard)
-    return new Response(
-      JSON.stringify({
-        response: {
-          trustedDomainsType: 0,
-          culture: "en-US",
-          utcOffset: "00:00:00",
-          utcHoursOffset: 0,
-          greetingSettings: "Web Office",
-          ownerId: "00000000-0000-0000-0000-000000000000",
-          enabledJoin: false,
-          enableAdmMess: false,
-          thirdpartyEnable: false,
-          docSpace: true,
-          standalone: true,
-          baseDomain: BASE_URL,
-          wizardToken:
-            "type=Wizard&key=462096722976.4OD9HUTNJZGBFMMPVOTCBNOVCPGLTBKUNPKGCNY&uid=66faa6e4-f133-11ea-b126-00ffeec8b4ef",
-          passwordHash: {
-            size: 256,
-            iterations: 100000,
-            salt: "4d9abe238e2f7b14a30a4565d62214a795a15abb798ed61118a69820d6a6146c",
-          },
-          version: ".",
-          recaptchaType: 0,
-          recaptchaPublicKey: "",
-          debugInfo: false,
-          tenantStatus: 0,
-          tenantAlias: "localhost",
-          forumLink: "https://forum.onlyoffice.com",
-          legalTerms:
-            "https://help.onlyoffice.co/products/files/doceditor.aspx?fileid=5048502&doc=SXhWMEVzSEYxNlVVaXJJeUVtS0kyYk14YWdXTEFUQmRWL250NllHNUFGbz0_IjUwNDg1MDIi0",
-          cookieSettingsEnabled: false,
-          limitedAccessSpace: false,
-          userNameRegex: "^[\\p{L}\\p{M}' \\-]+$",
-          maxImageUploadSize: 0,
-        },
-        count: 1,
-        links: [
-          {
-            href: url,
-            action: "GET",
-          },
-        ],
-        status: 0,
-        statusCode: 200,
-        ok: true,
-      }),
-    );
+  if (isWizard) return new Response(JSON.stringify(settingsWizzard));
 
-  return new Response(
-    JSON.stringify({
-      response: {
-        timezone: "Asia/Tbilisi",
-        trustedDomains: [],
-        trustedDomainsType: 1,
-        culture: "en-US",
-        utcOffset: "04:00:00",
-        utcHoursOffset: 4,
-        greetingSettings: "Web Office",
-        ownerId: "66faa6e4-f133-11ea-b126-00ffeec8b4ef",
-        nameSchemaId: "Common",
-        enableAdmMess: false,
-        docSpace: true,
-        standalone: true,
-        baseDomain: "localhost",
-        passwordHash: {
-          size: 256,
-          iterations: 100000,
-          salt: "4d9abe238e2f7b14a30a4565d62214a795a15abb798ed61118a69820d6a6146c",
-        },
-        firebase: {
-          apiKey: "",
-          authDomain: "",
-          projectId: "",
-          storageBucket: "",
-          messagingSenderId: "",
-          appId: "",
-          measurementId: "",
-          databaseURL: "",
-        },
-        version: ".",
-        recaptchaType: 0,
-        debugInfo: false,
-        socketUrl: "/socket.io",
-        tenantStatus: 0,
-        tenantAlias: "localhost",
-        helpLink: "https://helpcenter.onlyoffice.com",
-        forumLink: "https://forum.onlyoffice.com",
-        apiDocsLink: "https://api.onlyoffice.com",
-        domainValidator: {
-          regex: "^[a-z0-9]([a-z0-9-]){1,61}[a-z0-9]$",
-          minLength: 3,
-          maxLength: 63,
-        },
-        zendeskKey: "",
-        tagManagerId: "",
-        bookTrainingEmail: "training@onlyoffice.com",
-        documentationEmail: "documentation@onlyoffice.com",
-        legalTerms:
-          "https://help.onlyoffice.co/products/files/doceditor.aspx?fileid=5048502&doc=SXhWMEVzSEYxNlVVaXJJeUVtS0kyYk14YWdXTEFUQmRWL250NllHNUFGbz0_IjUwNDg1MDIi0",
-        cookieSettingsEnabled: false,
-        limitedAccessSpace: false,
-        userNameRegex: "^[\\p{L}\\p{M}' \\-]+$",
-        invitationLimit: 2147483647,
-        plugins: {
-          enabled: true,
-          upload: true,
-          delete: true,
-        },
-        deepLink: {
-          androidPackageName: "com.onlyoffice.documents",
-          url: "oodocuments://openfile",
-          iosPackageId: "944896972",
-        },
-        formGallery: {
-          path: "/api/oforms/",
-          domain: "https://cmsoforms.teamlab.info",
-          ext: ".pdf",
-          uploadPath: "/api/upload",
-          uploadDomain: "https://oforms.teamlab.info",
-          uploadExt: ".pdf",
-          uploadDashboard: "/dashboard/api",
-        },
-        maxImageUploadSize: 5242880,
-      },
-      count: 1,
-      links: [
-        {
-          href: url,
-          action: "GET",
-        },
-      ],
-      status: 0,
-      statusCode: 200,
-    }),
-  );
+  return new Response(JSON.stringify(settingsNoAuth));
 };

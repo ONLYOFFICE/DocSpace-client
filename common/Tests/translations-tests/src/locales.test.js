aeab29a98170fefd14a32d3b4929943837d3000d
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2024-09-11 18:22:56 +0400

    Translations: Tests: added setup of lists

--- common/Tests/translations-tests/src/locales.test.js
+++ common/Tests/translations-tests/src/locales.test.js
@@ -13,6 +13,7 @@
 // limitations under the License.
 const fs = require("fs");
 const path = require("path");
+const crypto = require("crypto");
 
 const BASE_DIR =
   process.env.BASE_DIR || path.resolve(__dirname, "../../../../");
@@ -20,6 +21,11 @@ const BASE_DIR =
 let workspaces = [];
 let translationFiles = [];
 let javascriptFiles = [];
+let parseJsonErrors = [];
+let notTranslatedToasts = [];
+let notTranslatedProps = [];
+let moduleFolders = [];
+let commonTranslations = [];
 
 const getAllFiles = (dir) => {
   const files = fs.readdirSync(dir);
@@ -34,7 +40,15 @@ const getAllFiles = (dir) => {
   });
 };
 
+const convertPathToOS = (filePath) => {
+  return path.sep == "/"
+    ? filePath.replace("\\", "/")
+    : filePath.replace("/", "\\");
+};
+
 beforeAll(() => {
+  console.log(`Base path = ${BASE_DIR}`);
+
   const moduleWorkspaces = [
     "packages/client",
     "packages/doceditor",
@@ -46,7 +60,7 @@ beforeAll(() => {
   workspaces = moduleWorkspaces.map((ws) => path.resolve(BASE_DIR, ws));
   workspaces.push(path.resolve(BASE_DIR, "public/locales"));
 
-  translationFiles = workspaces.flatMap((wsPath) => {
+  const translations = workspaces.flatMap((wsPath) => {
     const clientDir = path.resolve(BASE_DIR, wsPath);
 
     return getAllFiles(clientDir).filter(
@@ -54,13 +68,43 @@ beforeAll(() => {
     );
   });
 
-  console.log(`Base path = ${BASE_DIR}`);
   console.log(
-    `Found translationFiles by *.json filter = ${translationFiles.length}. First path is '${translationFiles[0]}'`
+    `Found translations by *.json filter = ${translations.length}. First path is '${translations[0]}'`
+  );
+
+  for (const tPath of translations) {
+    try {
+      const fileContent = fs.readFileSync(tPath, "utf8");
+
+      const hash = crypto.createHash("md5").update(fileContent).digest("hex");
+
+      const jsonTranslation = JSON.parse(fileContent);
+
+      const translationFile = {
+        path: tPath,
+        translations: Object.entries(jsonTranslation).map(([key, value]) => ({
+          key,
+          value,
+        })),
+        md5hash: hash,
+        language: path.dirname(tPath).split(path.sep).pop(),
+      };
+
+      translationFiles.push(translationFile);
+    } catch (ex) {
+      parseJsonErrors.push({ path: tPath, error: ex });
+      console.log(
+        `File path = ${tPath} failed to parse with error: ${ex.message}`
+      );
+    }
+  }
+
+  console.log(
+    `Found translationFiles = ${translationFiles.length}. First path is '${translationFiles[0]?.path}'`
   );
 
   const searchPattern = /\.(js|jsx|ts|tsx)$/;
-  javascriptFiles = workspaces.flatMap((wsPath) => {
+  const javascripts = workspaces.flatMap((wsPath) => {
     const clientDir = path.resolve(BASE_DIR, wsPath);
 
     return getAllFiles(clientDir).filter(
@@ -74,7 +118,159 @@ beforeAll(() => {
   });
 
   console.log(
-    `Found javascriptFiles by *.js(x) filter = ${javascriptFiles.length}. First path is '${javascriptFiles[0]}'`
+    `Found javascripts by *.js(x) filter = ${javascripts.length}. First path is '${javascripts[0]}'`
+  );
+
+  const pattern1 =
+    "[.{\\s\\(]t\\??\\.?\\(\\s*[\"'`]([a-zA-Z0-9_.:\\s{}/-]+)[\"'`]\\s*[\\),]";
+  const pattern2 = 'i18nKey="([a-zA-Z0-9_.:-]+)"';
+  const pattern3 = 'tKey:\\s"([a-zA-Z0-9_.:-]+)"';
+  const pattern4 = 'getTitle\\("([a-zA-Z0-9_.:-]+)"\\)';
+
+  const regexp = new RegExp(
+    `(${pattern1})|(${pattern2})|(${pattern3})|(${pattern4})`,
+    "gm"
+  );
+
+  const notTranslatedToastsRegex = new RegExp(
+    "(?<=toastr.info\\([\"'`])(.*)(?=[\"'`])" +
+      "|(?<=toastr.error\\([\"'`])(.*)(?=[\"'`])" +
+      "|(?<=toastr.success\\([\"'`])(.*)(?=[\"'`])" +
+      "|(?<=toastr.warn\\([\"'`])(.*)(?=[\"'`])",
+    "gm"
+  );
+
+  const notTranslatedPropsRegex = new RegExp(
+    "<[\\w\\n][^>]* (title|placeholder|label|text)={?[\"'](.*)[\"']}?",
+    "gm"
+  );
+
+  javascripts.forEach((filePath) => {
+    const jsFileText = fs.readFileSync(filePath, "utf8");
+
+    const toastMatches = [...jsFileText.matchAll(notTranslatedToastsRegex)];
+
+    if (toastMatches.length > 0) {
+      toastMatches.forEach((toastMatch) => {
+        const found = toastMatch[0];
+        if (found && !notTranslatedToasts.some((t) => t.value === found)) {
+          notTranslatedToasts.push({ path: filePath, value: found });
+        }
+      });
+    }
+
+    const propsMatches = [...jsFileText.matchAll(notTranslatedPropsRegex)];
+
+    if (propsMatches.length > 0) {
+      propsMatches.forEach((propsMatch) => {
+        const found = propsMatch[0];
+        if (found && !notTranslatedProps.some((t) => t.value === found)) {
+          notTranslatedProps.push({ path: filePath, value: found });
+        }
+      });
+    }
+
+    const matches = [...jsFileText.matchAll(regexp)];
+
+    const translationKeys = matches
+      .map((m) => m[2] || m[4] || m[6] || m[8])
+      .filter((m) => m != null);
+
+    if (translationKeys.length === 0) return;
+
+    const jsFile = {
+      path: filePath,
+      translationKeys: translationKeys,
+    };
+
+    javascriptFiles.push(jsFile);
+  });
+
+  console.log(
+    `Found javascriptFiles = ${javascriptFiles.length}. First path is '${javascriptFiles[0]?.path}'`
+  );
+
+  const list = translationFiles.map((t) => ({
+    modulePath: moduleWorkspaces.find((m) => t.path.includes(m)),
+    language: {
+      path: t.path,
+      language: t.language,
+      translations: t.translations,
+    },
+    lng: t.language,
+  }));
+
+  const moduleTranslations = list.reduce((acc, t) => {
+    const group = acc.find((g) => g.modulePath === t.modulePath);
+    if (group) {
+      group.languages.push(t.language);
+    } else {
+      acc.push({
+        modulePath: t.modulePath,
+        languages: [t.language],
+      });
+    }
+    return acc;
+  }, []);
+
+  console.log(
+    `Found moduleTranslations = ${moduleTranslations.length}. First path is '${moduleTranslations[0]?.modulePath}'`
+  );
+
+  const moduleJsTranslatedFiles = javascriptFiles
+    .map((t) => ({
+      modulePath: moduleWorkspaces.find((m) => t.path.includes(m)),
+      path: t.path,
+      translationKeys: t.translationKeys,
+    }))
+    .reduce((acc, t) => {
+      const group = acc.find((g) => g.modulePath === t.modulePath);
+      if (group) {
+        group.translationKeys.push(...t.translationKeys);
+      } else {
+        acc.push({
+          modulePath: t.modulePath,
+          translationKeys: t.translationKeys,
+        });
+      }
+      return acc;
+    }, []);
+
+  console.log(
+    `Found moduleJsTranslatedFiles = ${moduleJsTranslatedFiles.length}. First path is '${moduleJsTranslatedFiles[0]?.modulePath}'`
+  );
+
+  moduleWorkspaces.forEach((wsPath) => {
+    const t = moduleTranslations.find((t) => t.modulePath === wsPath);
+    const j = moduleJsTranslatedFiles.find((t) => t.modulePath === wsPath);
+
+    if (!j && !t) return;
+
+    moduleFolders.push({
+      path: wsPath,
+      availableLanguages: t?.languages,
+      appliedJsTranslationKeys: j?.translationKeys,
+    });
+  });
+
+  console.log(
+    `Found ModuleFolders = ${moduleFolders.length}. First path is '${moduleFolders[0]?.path}'`
+  );
+
+  commonTranslations = translationFiles
+    .filter((file) =>
+      file.path.startsWith(
+        convertPathToOS(path.join(BASE_DIR, "public/locales"))
+      )
+    )
+    .map((t) => ({
+      path: t.path,
+      language: t.language,
+      translations: t.translations,
+    }));
+
+  console.log(
+    `Found CommonTranslations = ${commonTranslations.length}. First path is '${commonTranslations[0]?.path}'`
   );
 });
 

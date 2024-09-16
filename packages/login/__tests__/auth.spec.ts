1235608f09f87be2b88f98e775a4c7e650dc21ba
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-09-16 18:43:59 +0400

    Login:Tests: update test cases for auth page

--- packages/login/__tests__/auth.spec.ts
+++ packages/login/__tests__/auth.spec.ts
@@ -1,3 +1,4 @@
+import { options } from "./../../shared/components/input-phone/options";
 // (c) Copyright Ascensio System SIA 2009-2024
 //
 // This program is a free software product.
@@ -24,11 +25,12 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import { endpoints } from "@docspace/shared/__mocks__/e2e";
+import { BASE_URL, endpoints } from "@docspace/shared/__mocks__/e2e";
 import { expect, test } from "./fixtures/base";
 import { getUrlWithQueryParams } from "./helpers/getUrlWithQueryParams";
 
 const URL = "/login/confirm/Auth";
+const PORT = 5111;
 
 const QUERY_PARAMS = [
   {
@@ -45,14 +47,78 @@ const QUERY_PARAMS = [
   },
 ];
 
-const URL_WITH_PARAMS = getUrlWithQueryParams(URL, QUERY_PARAMS);
+const QUERY_PARAMS_WITH_REFERENCE_URL = QUERY_PARAMS.concat({
+  name: "referenceUrl",
+  value: `${BASE_URL}:${PORT}/rooms`,
+});
 
-test("auth render", async ({ page, mockRequest }) => {
-  await mockRequest.router(endpoints.login);
+const QUERY_PARAMS_WITH_FILE_HANDLER = QUERY_PARAMS.concat([
+  {
+    name: "referenceUrl",
+    value: `${BASE_URL}:${PORT}/filehandler.ashx?action=download`,
+  },
+  {
+    name: "fileid",
+    value: "23",
+  },
+]);
+
+const URL_WITH_PARAMS = getUrlWithQueryParams(URL, QUERY_PARAMS);
+const URL_WITH_REFERENCE_URL = getUrlWithQueryParams(
+  URL,
+  QUERY_PARAMS_WITH_REFERENCE_URL,
+);
+const URL_WITH_FILE_HANDLER = getUrlWithQueryParams(
+  URL,
+  QUERY_PARAMS_WITH_FILE_HANDLER,
+);
+
+test("auth success", async ({ page, mockRequest }) => {
+  await mockRequest.router([endpoints.login]);
   await page.goto(URL_WITH_PARAMS);
 
   await page.getByTestId("loader").waitFor({ state: "attached" });
   await page.waitForURL("/", { waitUntil: "load" });
 
-  await expect(page).toHaveScreenshot(["desktop", "auth", "auth-render.png"]);
+  await expect(page).toHaveScreenshot(["desktop", "auth", "auth-success.png"]);
+});
+
+test("auth with reference url success", async ({ page, mockRequest }) => {
+  await mockRequest.router([endpoints.login]);
+  await page.goto(URL_WITH_REFERENCE_URL);
+
+  await page.getByTestId("loader").waitFor({ state: "attached" });
+
+  await page.waitForURL(`${BASE_URL}:${PORT}/rooms`, {
+    waitUntil: "load",
+  });
+
+  await expect(page).toHaveScreenshot([
+    "desktop",
+    "auth",
+    "auth-with-reference-url-success.png",
+  ]);
+});
+
+test("auth with file handler success", async ({ page, mockRequest }) => {
+  await mockRequest.router([endpoints.login]);
+  await page.goto(URL_WITH_FILE_HANDLER, { waitUntil: "domcontentloaded" });
+
+  await expect(page).toHaveScreenshot(
+    ["desktop", "auth", "auth-with-file-handler-success.png"],
+    { fullPage: true },
+  );
+
+  await page.waitForURL(
+    `${BASE_URL}:${PORT}/filehandler.ashx?action=download`,
+    {
+      waitUntil: "load",
+    },
+  );
+
+  await expect(page).toHaveScreenshot([
+    "desktop",
+    "auth",
+    "auth-with-file-handler-success-redirect.png",
+  ]);
 });

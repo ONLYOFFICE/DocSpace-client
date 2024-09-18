36785755a95242834f30b066dc5672b4e7f23515
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2024-09-18 15:04:49 +0400

    Tests: Translations: fix Assignment to constant variable.

--- common/tests/test/locales.test.js
+++ common/tests/test/locales.test.js
@@ -427,7 +427,7 @@ describe("Locales Tests", () => {
   });
 
   test("WrongTranslationVariablesTest: Verify that translation keys across different languages have consistent variables.", () => {
-    const message = `Next keys have wrong or empty variables:\r\n\r\n`;
+    let message = `Next keys have wrong or empty variables:\r\n\r\n`;
     const regVariables = new RegExp("\\{\\{([^\\{].?[^\\}]+)\\}\\}", "gm");
 
     const groupedByLng = translationFiles.reduce((acc, t) => {
@@ -501,7 +501,7 @@ describe("Locales Tests", () => {
   });
 
   test("WrongTranslationTagsTest: Verify that HTML tags within translation strings are consistent across different languages.", () => {
-    const message = `Next keys have wrong or empty translation's html tags:\r\n\r\n`;
+    let message = `Next keys have wrong or empty translation's html tags:\r\n\r\n`;
     const regString = "<(?:\"[^\"]*\"['\"]*|'[^']*'['\"]*|[^'\">])+>";
     const regTags = new RegExp(regString, "gm");
 

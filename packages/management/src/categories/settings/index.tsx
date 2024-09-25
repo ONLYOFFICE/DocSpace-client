e93a0b51bcb7a8b5facbd9db61c4772b89d25833
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-09-25 09:09:28 +0300

    Management: hide branding for community

--- packages/management/src/categories/settings/index.tsx
+++ packages/management/src/categories/settings/index.tsx
@@ -41,17 +41,13 @@ import ConnectDialog from "client/ConnectDialog";
 const Settings = () => {
   const { t } = useTranslation(["Common", "Settings"]);
   const navigate = useNavigate();
-  const { dialogsStore } = useStore();
+  const { dialogsStore, currentTariffStatusStore } = useStore();
   const { connectDialogVisible } = dialogsStore;
+  const { isCommunity } = currentTariffStatusStore;
 
   const [currentTabId, setCurrentTabId] = useState("branding");
 
   const data = [
-    {
-      id: "branding",
-      name: t("Settings:Branding"),
-      content: <Branding />,
-    },
     {
       id: "data-backup",
       name: t("Settings:DataBackup"),
@@ -68,6 +64,14 @@ const Settings = () => {
       content: <Restore />,
     },
   ];
+
+  if (!isCommunity) {
+    data.unshift({
+      id: "branding",
+      name: t("Settings:Branding"),
+      content: <Branding />,
+    });
+  }
 
   const onSelect = (element: TTabItem) => {
     navigate(

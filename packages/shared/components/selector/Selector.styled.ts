5baa307b292a690dfc400f16123a75f51b90b86c
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2024-09-27 15:17:05 +0200

    Shared: Selector: Add padding after last element in selector

--- packages/shared/components/selector/Selector.styled.ts
+++ packages/shared/components/selector/Selector.styled.ts
@@ -143,6 +143,10 @@ const StyledBody = styled.div<{
   .selector_info-bar {
     margin: 0px 20px 16px;
   }
+
+  .selector-body-scroll .scroll-body {
+    padding-bottom: 12px !important;
+  }
 `;
 
 const StyledSelectAll = styled.div`

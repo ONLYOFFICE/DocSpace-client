bb62520703a46d4b35c8d4c3ae7e43428b8c454a
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-09-30 14:11:13 +0300

    Client: fix icon

--- packages/client/src/pages/PortalSettings/components/NotAvailable.tsx
+++ packages/client/src/pages/PortalSettings/components/NotAvailable.tsx
@@ -24,7 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import LockedReactSvgUrl from "PUBLIC_DIR/images/locked.react.svg?url";
+import LockedReactSvgUrl from "PUBLIC_DIR/images/icons/16/locked.react.svg?url";
 
 import React from "react";
 import styled from "styled-components";

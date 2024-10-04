2e1e76378157afc83a560781f7c480fdc5842381
TatianaLopaeva <tatiana.lopaeva@onlyoffice.com>
2024-10-04 18:47:35 +0300

    Hiding unnecessary functions.

--- packages/client/src/store/SelectedFolderStore.ts
+++ packages/client/src/store/SelectedFolderStore.ts
@@ -306,36 +306,36 @@ class SelectedFolderStore {
     this.inRoom = inRoom;
   };
 
-  addDefaultLogoPaths = () => {
-    const cachebreaker = new Date().getTime();
-    this.logo = {
-      small: `/storage/room_logos/root/${this.id}_small.png?${cachebreaker}`,
-      medium: `/storage/room_logos/root/${this.id}_medium.png?${cachebreaker}`,
-      large: `/storage/room_logos/root/${this.id}_large.png?${cachebreaker}`,
-      original: `/storage/room_logos/root/${this.id}_original.png?${cachebreaker}`,
-    };
-  };
+  // addDefaultLogoPaths = () => {
+  //   const cachebreaker = new Date().getTime();
+  //   this.logo = {
+  //     small: `/storage/room_logos/root/${this.id}_small.png?${cachebreaker}`,
+  //     medium: `/storage/room_logos/root/${this.id}_medium.png?${cachebreaker}`,
+  //     large: `/storage/room_logos/root/${this.id}_large.png?${cachebreaker}`,
+  //     original: `/storage/room_logos/root/${this.id}_original.png?${cachebreaker}`,
+  //   };
+  // };
 
-  removeLogoPaths = () => {
-    this.logo = {
-      small: "",
-      medium: "",
-      large: "",
-      original: "",
-    };
-  };
+  // removeLogoPaths = () => {
+  //   this.logo = {
+  //     small: "",
+  //     medium: "",
+  //     large: "",
+  //     original: "",
+  //   };
+  // };
 
-  updateLogoPathsCacheBreaker = () => {
-    if (!this.logo?.original) return;
+  // updateLogoPathsCacheBreaker = () => {
+  //   if (!this.logo?.original) return;
 
-    const cachebreaker = new Date().getTime();
-    this.logo = {
-      small: `${this.logo.small.split("?")[0]}?${cachebreaker}`,
-      medium: `${this.logo.medium.split("?")[0]}?${cachebreaker}`,
-      large: `${this.logo.large.split("?")[0]}?${cachebreaker}`,
-      original: `${this.logo.original.split("?")[0]}?${cachebreaker}`,
-    };
-  };
+  //   const cachebreaker = new Date().getTime();
+  //   this.logo = {
+  //     small: `${this.logo.small.split("?")[0]}?${cachebreaker}`,
+  //     medium: `${this.logo.medium.split("?")[0]}?${cachebreaker}`,
+  //     large: `${this.logo.large.split("?")[0]}?${cachebreaker}`,
+  //     original: `${this.logo.original.split("?")[0]}?${cachebreaker}`,
+  //   };
+  // };
 
   setDefaultValuesIfUndefined: (selectedFolder: TSetSelectedFolder) => void = (
     selectedFolder,

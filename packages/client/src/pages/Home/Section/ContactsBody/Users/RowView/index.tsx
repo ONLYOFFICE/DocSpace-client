ad4d75c32ba0b212be3331cf8b25806dc9f99e9b
Timofey <timofey.boyko@onlyoffice.com>
2024-10-09 17:50:08 +0800

    Fix after merge

--- packages/client/src/pages/Home/Section/ContactsBody/Users/RowView/index.tsx
+++ packages/client/src/pages/Home/Section/ContactsBody/Users/RowView/index.tsx
@@ -45,19 +45,11 @@ const PeopleRowContainer = ({
   filterTotal,
   withPaging,
   currentDeviceType,
-<<<<<<< HEAD:packages/client/src/pages/Home/Section/ContactsBody/Users/RowView/index.tsx
   isUsersEmptyView,
   contactsTab,
   showStorageInfo,
   isDefaultUsersQuotaSet,
 }: RowViewProps) => {
-=======
-  insideGroupIsFiltered,
-  insideGroupFilterTotal,
-  hasMoreInsideGroupUsers,
-  fetchMoreInsideGroupUsers,
-}) => {
->>>>>>> develop:packages/client/src/pages/Home/Section/AccountsBody/InsideGroup/RowView/PeopleRowContainer.js
   useViewEffect({
     view: viewAs!,
     setView: (view: string) => {
@@ -94,7 +86,6 @@ const PeopleRowContainer = ({
   );
 };
 
-<<<<<<< HEAD:packages/client/src/pages/Home/Section/ContactsBody/Users/RowView/index.tsx
 export default inject(
   ({ peopleStore, settingsStore, currentQuotaStore }: RowViewStores) => {
     const { usersStore, viewAs, setViewAs } = peopleStore;
@@ -134,40 +125,3 @@ export default inject(
     };
   },
 )(observer(PeopleRowContainer));
-=======
-export default inject(({ peopleStore, filesStore, settingsStore }) => {
-  const {
-    viewAs: accountsViewAs,
-    setViewAs,
-    usersStore,
-    infoPanelStore,
-  } = peopleStore;
-  const { theme, withPaging, currentDeviceType } = settingsStore;
-  const { isVisible: infoPanelVisible } = infoPanelStore;
-
-  const {
-    currentGroup,
-    insideGroupIsFiltered,
-    insideGroupFilterTotal,
-    hasMoreInsideGroupUsers,
-    fetchMoreInsideGroupUsers,
-  } = peopleStore.groupsStore;
-
-  const { peopleList } = usersStore;
-
-  return {
-    peopleList,
-    currentGroup,
-    accountsViewAs,
-    setViewAs,
-    theme,
-    infoPanelVisible,
-    withPaging,
-    currentDeviceType,
-    insideGroupIsFiltered,
-    insideGroupFilterTotal,
-    hasMoreInsideGroupUsers,
-    fetchMoreInsideGroupUsers,
-  };
-})(observer(PeopleRowContainer));
->>>>>>> develop:packages/client/src/pages/Home/Section/AccountsBody/InsideGroup/RowView/PeopleRowContainer.js

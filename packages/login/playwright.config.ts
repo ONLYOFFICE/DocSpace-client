42df632ffc0c20ad8d40e824fbb6756e33cc8be3
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-10-09 12:44:16 +0400

    Login: Сhange the value for the screenshot viewport in the config

--- packages/login/playwright.config.ts
+++ packages/login/playwright.config.ts
@@ -46,7 +46,10 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1440, height: 1024 },
+      },
     },
     /*     {
       name: "firefox",

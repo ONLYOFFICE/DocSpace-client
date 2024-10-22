50f8809a1fbfb67129355973af919e0a10634ca5
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-10-22 11:16:27 +0400

    Login: update reporter in  playwright config

--- packages/login/playwright.config.ts
+++ packages/login/playwright.config.ts
@@ -25,7 +25,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { outputFolder: "../../playwright-report/login" }]],
+  reporter: [
+    [
+      process.env.CI ? "dot" : "html",
+      {
+        outputFolder: "../../playwright-report/login",
+        open: "never",
+      },
+    ],
+  ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {

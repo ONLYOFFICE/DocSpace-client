d9151db6f146d0e3bc28f821edf54737eef7a28e
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-10-28 04:43:10 +0300

    Shared: delete useless store

--- packages/shared/store/SettingsStore.ts
+++ packages/shared/store/SettingsStore.ts
@@ -327,6 +327,8 @@ class SettingsStore {
 
   isDefaultPasswordProtection: boolean = false;
 
+  isBannerVisible = false;
+
   constructor() {
     makeAutoObservable(this);
   }
@@ -1173,6 +1175,10 @@ class SettingsStore {
 
   checkEnablePortalSettings = (isPaid: boolean) => {
     return isManagement() && this.portals?.length === 1 ? false : isPaid;
+  };
+
+  setIsBannerVisible = (visible: boolean) => {
+    this.isBannerVisible = visible;
   };
 }
 

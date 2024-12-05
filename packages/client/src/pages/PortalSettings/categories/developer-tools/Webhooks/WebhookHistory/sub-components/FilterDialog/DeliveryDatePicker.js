959a3c644ffcee6980e50a9523d6ebc9482a4557
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2024-12-05 15:49:11 +0400

    Fix error  'css' is not defined

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/FilterDialog/DeliveryDatePicker.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/FilterDialog/DeliveryDatePicker.js
@@ -25,7 +25,7 @@
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
 import React, { useState, useEffect, useRef } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import moment from "moment-timezone";
 
 import { Text } from "@docspace/shared/components/text";

92dfbb552976c9d686bc72d73bf6abec91adb0c3
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-12-06 14:37:35 +0300

    Client: init branding store

--- packages/client/src/store/index.js
+++ packages/client/src/store/index.js
@@ -86,6 +86,8 @@ import AvatarEditorDialogStore from "./AvatarEditorDialogStore";
 
 import OAuthStore from "./OAuthStore";
 
+import BrandingStore from "./portal-settings/BrandingStore";
+
 const oauthStore = new OAuthStore(userStore);
 
 const selectedFolderStore = new SelectedFolderStore(settingsStore);
@@ -326,6 +328,8 @@ const campaignsStore = new CampaignsStore(settingsStore, userStore);
 
 const editGroupStore = new EditGroupStore(peopleStore);
 
+const brandingStore = new BrandingStore(settingsStore);
+
 const store = {
   authStore,
   userStore,
@@ -383,6 +387,8 @@ const store = {
   indexingStore,
   editGroupStore,
   avatarEditorDialogStore,
+
+  brandingStore,
 };
 
 export default store;

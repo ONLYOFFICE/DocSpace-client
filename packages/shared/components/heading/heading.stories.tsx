90dd9629b616e8ed098648b7e49dd515b6e58b50
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-12-09 14:13:50 +0400

    Shared:Components:Heading: refactoring and rewrite to scss modules

--- packages/shared/components/heading/heading.stories.tsx
+++ packages/shared/components/heading/heading.stories.tsx
@@ -27,44 +27,141 @@
 import React from "react";
 import { Meta, StoryObj } from "@storybook/react";
 
-import { Heading } from "./Heading";
-import { HeadingProps } from "./Heading.types";
-import { HeadingLevel, HeadingSize } from "./Heading.enums";
+import { Heading } from ".";
 
 const meta = {
   title: "Components/Heading",
   component: Heading,
   argTypes: {
     color: { control: "color" },
+    level: {
+      control: { type: "select" },
+      options: ["h1", "h2", "h3", "h4", "h5", "h6"],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["xsmall", "small", "medium", "large", "xlarge"],
+    },
+    type: {
+      control: { type: "select" },
+      options: ["default", "header", "menu", "content"],
+    },
   },
   parameters: {
     docs: {
       description: {
-        component: "Heading text structured in levels",
+        component:
+          "Flexible Heading component with multiple configuration options",
       },
     },
   },
 } satisfies Meta<typeof Heading>;
+
 type Story = StoryObj<typeof Heading>;
 
 export default meta;
 
-const Template = ({ ...args }: HeadingProps) => {
-  return (
-    <div style={{ margin: "7px" }}>
-      <Heading {...args}>Sample text Heading</Heading>
-    </div>
-  );
-};
-
 export const Default: Story = {
-  render: (args) => <Template {...args} />,
+  args: {
+    level: "h1",
+    size: "large",
+    children: "Default Heading",
+  },
 };
 
-Default.args = {
-  level: HeadingLevel.h1,
-  title: "",
-  truncate: false,
-  isInline: false,
-  size: HeadingSize.large,
+export const HeadingLevels: Story = {
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <Heading level="h1">H1 Heading</Heading>
+      <Heading level="h2">H2 Heading</Heading>
+      <Heading level="h3">H3 Heading</Heading>
+      <Heading level="h4">H4 Heading</Heading>
+      <Heading level="h5">H5 Heading</Heading>
+      <Heading level="h6">H6 Heading</Heading>
+    </div>
+  ),
+};
+
+export const HeadingSizes: Story = {
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <Heading level="h1" size="xsmall">
+        XSmall Heading
+      </Heading>
+      <Heading level="h1" size="small">
+        Small Heading
+      </Heading>
+      <Heading level="h1" size="medium">
+        Medium Heading
+      </Heading>
+      <Heading level="h1" size="large">
+        Large Heading
+      </Heading>
+      <Heading level="h1" size="xlarge">
+        XLarge Heading
+      </Heading>
+    </div>
+  ),
+};
+
+export const HeadingTypes: Story = {
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <Heading level="h1" type="header">
+        Header Type
+      </Heading>
+      <Heading level="h1" type="menu">
+        Menu Type
+      </Heading>
+      <Heading level="h1" type="content">
+        Content Type
+      </Heading>
+    </div>
+  ),
+};
+
+export const StyledHeadings: Story = {
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <Heading level="h1" color="red" fontSize="24px" fontWeight={700}>
+        Custom Styled Heading
+      </Heading>
+      <Heading level="h2" truncate style={{ maxWidth: "200px" }}>
+        Truncated Long Heading That Will Be Shortened with Ellipsis
+      </Heading>
+      <Heading level="h3" isInline>
+        Inline Heading
+      </Heading>
+    </div>
+  ),
+};
+
+export const AccessibilityHeadings: Story = {
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <Heading level="h1" aria-label="Descriptive Accessibility Label">
+        Heading with Aria Label
+      </Heading>
+      <Heading level="h2" aria-describedby="additional-description">
+        Heading with Aria Describedby
+      </Heading>
+      <div id="additional-description" style={{ display: "none" }}>
+        This is an additional description for the heading
+      </div>
+    </div>
+  ),
+};
+
+export const CustomDataAttributes: Story = {
+  render: () => (
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <Heading
+        level="h1"
+        data-custom="custom-value"
+        data-testid="custom-heading"
+      >
+        Heading with Custom Data Attributes
+      </Heading>
+    </div>
+  ),
 };

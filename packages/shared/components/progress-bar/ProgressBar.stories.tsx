5bf92d0ac17b48b6ab471af7063536afc4e891a8
gopienkonikita <nikita.gopienko@onlyoffice.com>
2024-12-10 13:14:00 +0300

    Shared: Components: ProgressBar: rewrite with SCSS

--- packages/shared/components/progress-bar/ProgressBar.stories.tsx
+++ packages/shared/components/progress-bar/ProgressBar.stories.tsx
@@ -26,7 +26,7 @@
 
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { ProgressBar } from "./ProgressBar";
+import { ProgressBar } from ".";
 
 const meta: Meta<typeof ProgressBar> = {
   title: "Components/ProgressBar",

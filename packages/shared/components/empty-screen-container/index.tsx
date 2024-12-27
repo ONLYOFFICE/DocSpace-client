82ad1d4bc7405851a7bd78a34350031cecb7782a
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2024-12-27 12:28:25 +0400

    Shared:Components:EmptyScreenContainer: rewrite to scss modules

--- packages/shared/components/empty-screen-container/index.tsx
+++ packages/shared/components/empty-screen-container/index.tsx
@@ -24,4 +24,81 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-export { EmptyScreenContainer } from "./EmptyScreenContainer";
+import React from "react";
+import classNames from "classnames";
+
+import { isTablet } from "../../utils";
+import { Text } from "../text";
+
+import styles from "./EmptyScreenContainer.module.scss";
+
+import type { EmptyScreenContainerProps } from "./EmptyScreenContainer.types";
+
+const EmptyScreenContainer = (props: EmptyScreenContainerProps) => {
+  const {
+    imageSrc,
+    imageAlt,
+    headerText,
+    subheadingText,
+    descriptionText,
+    buttons,
+    imageStyle,
+    buttonStyle,
+    withoutFilter,
+    className,
+  } = props;
+
+  return (
+    <div
+      className={classNames(
+        styles.body,
+        {
+          [styles.withoutFilter]: withoutFilter,
+          [styles.withSubheading]: !!subheadingText,
+          [styles.withDescription]: !!descriptionText,
+        },
+        className,
+      )}
+      data-testid="empty-screen-container"
+    >
+      <img
+        src={imageSrc}
+        alt={imageAlt}
+        style={!isTablet() ? imageStyle : {}}
+        className={styles.image}
+      />
+
+      {headerText ? (
+        <Text
+          as="span"
+          fontSize="19px"
+          fontWeight="700"
+          className={styles.header}
+          noSelect
+        >
+          {headerText}
+        </Text>
+      ) : null}
+
+      {subheadingText ? (
+        <Text as="span" fontWeight="600" className={styles.subheading} noSelect>
+          {subheadingText}
+        </Text>
+      ) : null}
+
+      {descriptionText ? (
+        <Text as="span" fontSize="12px" className={styles.description} noSelect>
+          {descriptionText}
+        </Text>
+      ) : null}
+
+      {buttons ? (
+        <div className={styles.buttons} style={buttonStyle}>
+          {buttons}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export { EmptyScreenContainer };

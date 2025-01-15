ae2ba4d102dae6375e3a35e1d2179ff83b26aa48
Vlada Gazizova <vlada.gazizova@onlyoffice.com>
2025-01-15 09:45:14 +0300

    Fixed Bug 69929 - SDK. The drop-down list for creating PDF forms disappears when the frame width is 1000px

--- packages/client/src/store/ContextOptionsStore.js
+++ packages/client/src/store/ContextOptionsStore.js
@@ -87,11 +87,7 @@ import { isMobile, isMobileOnly, isTablet } from "react-device-detect";
 import config from "PACKAGE_FILE";
 import { toastr } from "@docspace/shared/components/toast";
 import { combineUrl } from "@docspace/shared/utils/combineUrl";
-import {
-  isDesktop,
-  isLockedSharedRoom,
-  trimSeparator,
-} from "@docspace/shared/utils";
+import { isLockedSharedRoom, trimSeparator } from "@docspace/shared/utils";
 import { getDefaultAccessUser } from "@docspace/shared/utils/getDefaultAccessUser";
 import { copyShareLink } from "@docspace/shared/utils/copy";
 import {
@@ -2573,21 +2569,19 @@ class ContextOptionsStore {
       });
     }
 
-    const formActions = isDesktop()
-      ? [
-          {
-            id: "personal_form-template",
-            icon: FormReactSvgUrl,
-            label: t("Translations:NewForm"),
-            key: "new-form-base",
-            items: [
-              createTemplateForm,
-              createTemplateNewFormFile,
-              templateOformsGallery,
-            ],
-          },
-        ]
-      : [createTemplateForm, createTemplateNewFormFile, templateOformsGallery];
+    const formActions = [
+      {
+        id: "personal_form-template",
+        icon: FormReactSvgUrl,
+        label: t("Translations:NewForm"),
+        key: "new-form-base",
+        items: [
+          createTemplateForm,
+          createTemplateNewFormFile,
+          templateOformsGallery,
+        ],
+      },
+    ];
 
     const showUploadFolder = !(isMobile || isTablet);
     const options = isRoomsFolder

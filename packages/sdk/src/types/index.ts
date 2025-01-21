dd88fe7bec335cfe3244f88054964a3ebb9b3008
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2025-01-21 18:00:18 +0300

    Sdk: Types: Added missing TError type

--- packages/sdk/src/types/index.ts
+++ packages/sdk/src/types/index.ts
@@ -23,3 +23,9 @@
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+export type TError = {
+  message?: "unauthorized" | "restore-backup" | string;
+  status?: "not-found" | "access-denied" | number | string;
+  type?: string;
+};

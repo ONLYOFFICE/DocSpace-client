533976870f443b2427c318c6c1238ddd5a1630a0
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2025-01-28 02:27:58 +0400

    Replace deprecated @babel/plugin-proposal-private-property-in-object"

--- packages/shared/.storybook/main.js
+++ packages/shared/.storybook/main.js
@@ -25,7 +25,7 @@ module.exports = {
         babelOptions: {
           plugins: [
             [
-              "@babel/plugin-proposal-private-property-in-object",
+              "@babel/plugin-transform-private-property-in-object",
               {
                 loose: true,
               },

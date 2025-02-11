7bad20993e14f3a4b76823c267224b7fe633d78d
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2025-02-11 17:41:19 +0300

    Sdk: Added build options

--- packages/sdk/next.config.js
+++ packages/sdk/next.config.js
@@ -32,6 +32,9 @@ const pkg = require("./package.json");
 const nextConfig = {
   basePath: "/sdk",
   output: "standalone",
+  typescript: {
+    ignoreBuildErrors: process.env.TS_ERRORS_IGNORE === "true",
+  },
   compiler: {
     styledComponents: true,
   },
@@ -51,6 +54,7 @@ const nextConfig = {
     NEXT_PUBLIC_E2E_TEST: process.env.E2E_TEST,
   },
   webpack: (config) => {
+    console.log("ENV", { env: process.env });
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.(".svg"),

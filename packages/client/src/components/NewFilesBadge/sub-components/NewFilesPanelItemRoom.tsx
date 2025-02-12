82ddeb05ba8d3f3aab2301fd0da9a249aee55d21
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2025-02-12 17:05:06 +0400

    Client:Components:NewFilesBadges: add public key when opening room

--- packages/client/src/components/NewFilesBadge/sub-components/NewFilesPanelItemRoom.tsx
+++ packages/client/src/components/NewFilesBadge/sub-components/NewFilesPanelItemRoom.tsx
@@ -30,21 +30,20 @@ import { inject, observer } from "mobx-react";
 import { RoomIcon } from "@docspace/shared/components/room-icon";
 import { Text } from "@docspace/shared/components/text";
 
-import FilesActionStore from "SRC_DIR/store/FilesActionsStore";
-
 import { StyledRoomItem } from "../NewFilesBadge.styled";
 import { NewFilesPanelItemRoomProps } from "../NewFilesBadge.types";
 
 const NewFilesPanelItemRoomComponent = ({
   room,
-
+  getFolderInfo,
   openItemAction,
   onClose,
 }: NewFilesPanelItemRoomProps) => {
   const theme = useTheme();
 
-  const onClick = () => {
-    openItemAction!({ ...room, isFolder: true });
+  const onClick = async () => {
+    const roomInfo = await getFolderInfo!(room.id);
+    openItemAction!({ ...roomInfo, isFolder: true });
     onClose();
   };
 
@@ -75,10 +74,11 @@ const NewFilesPanelItemRoomComponent = ({
   );
 };
 
-export const NewFilesPanelItemRoom = inject(
-  ({ filesActionsStore }: { filesActionsStore: FilesActionStore }) => {
+export const NewFilesPanelItemRoom = inject<TStore>(
+  ({ filesActionsStore, filesStore }) => {
     const { openItemAction } = filesActionsStore;
+    const { getFolderInfo } = filesStore;
 
-    return { openItemAction };
+    return { openItemAction, getFolderInfo };
   },
 )(observer(NewFilesPanelItemRoomComponent));

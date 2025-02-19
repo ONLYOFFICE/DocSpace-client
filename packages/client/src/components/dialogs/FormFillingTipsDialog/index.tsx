4f93a6586cb8ca2e36982728f7fb3c80900c97d9
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2025-02-19 17:24:48 +0500

    Client: fixed eslint

--- packages/client/src/components/dialogs/FormFillingTipsDialog/index.tsx
+++ packages/client/src/components/dialogs/FormFillingTipsDialog/index.tsx
@@ -25,7 +25,7 @@
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 import React from "react";
 import { observer, inject } from "mobx-react";
-import styled, { useTheme } from "styled-components";
+import { useTheme } from "styled-components";
 import { useTranslation } from "react-i18next";
 import { TUser } from "@docspace/shared/api/people/types";
 import TutorialPreview from "PUBLIC_DIR/images/form_filling_tutorial.gif";

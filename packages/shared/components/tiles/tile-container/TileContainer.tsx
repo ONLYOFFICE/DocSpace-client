a5a411cffccc79f686449b3c6bad2c00fe90f253
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2025-02-20 13:39:27 +0400

    Shared:Components:Tiles: add tile Container component

---
+++ packages/shared/components/tiles/tile-container/TileContainer.tsx
@@ -0,0 +1,153 @@
+// (c) Copyright Ascensio System SIA 2009-2025
+//
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+//
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+//
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+//
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+//
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+//
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+import React from "react";
+import styles from "./TileContainer.module.scss";
+import { TileContainerProps, TileItemProps } from "./TileContainer.types";
+import classNames from "classnames";
+import { Heading, HeadingSize } from "../../heading";
+
+export const TileContainer = ({
+  children,
+  useReactWindow,
+  id = "tileContainer",
+  className,
+  infiniteGrid: InfiniteGrid,
+  headingFolders,
+  headingFiles,
+  isDesc,
+  style,
+  ...rest
+}: TileContainerProps) => {
+  const Rooms: React.ReactElement[] = [];
+  const Folders: React.ReactElement[] = [];
+  const Files: React.ReactElement[] = [];
+
+  React.Children.map(children, (item) => {
+    if (
+      !item ||
+      !React.isValidElement<TileItemProps>(item) ||
+      !item.props?.item
+    )
+      return null;
+
+    const { isFolder, isRoom, fileExst, id: itemId } = item.props.item;
+    if ((isFolder || itemId === -1) && !fileExst && !isRoom) {
+      Folders.push(
+        <div
+          className={classNames(styles.tileItemWrapper, styles.folder)}
+          key={itemId}
+        >
+          {item}
+        </div>,
+      );
+    } else if (isRoom) {
+      Rooms.push(
+        <div
+          className={classNames(styles.tileItemWrapper, styles.room)}
+          key={itemId}
+        >
+          {item}
+        </div>,
+      );
+    } else {
+      Files.push(
+        <div
+          className={classNames(styles.tileItemWrapper, styles.file)}
+          key={itemId}
+        >
+          {item}
+        </div>,
+      );
+    }
+  });
+
+  const headingClassNames = classNames(styles.header, {
+    [styles.isDesc]: isDesc,
+  });
+
+  const renderTile = (
+    <>
+      {Rooms.length > 0 ? (
+        useReactWindow ? (
+          Rooms
+        ) : (
+          <div className={classNames(styles.gridWrapper, styles.rooms)}>
+            {Rooms}
+          </div>
+        )
+      ) : null}
+
+      {Folders.length > 0 ? (
+        <Heading
+          size={HeadingSize.xsmall}
+          id="folder-tile-heading"
+          className={headingClassNames}
+        >
+          {headingFolders}
+        </Heading>
+      ) : null}
+      {Folders.length > 0 ? (
+        useReactWindow ? (
+          Folders
+        ) : (
+          <div className={classNames(styles.gridWrapper, styles.folders)}>
+            {Folders}
+          </div>
+        )
+      ) : null}
+
+      {Files.length > 0 ? (
+        <Heading size={HeadingSize.xsmall} className={headingClassNames}>
+          {headingFiles}
+        </Heading>
+      ) : null}
+      {Files.length > 0 ? (
+        useReactWindow ? (
+          Files
+        ) : (
+          <div className={classNames(styles.gridWrapper, styles.files)}>
+            {Files}
+          </div>
+        )
+      ) : null}
+    </>
+  );
+
+  return (
+    <div
+      className={classNames(styles.tileContainer, className)}
+      id={id}
+      style={style}
+    >
+      {useReactWindow && InfiniteGrid ? (
+        <InfiniteGrid>{renderTile}</InfiniteGrid>
+      ) : (
+        renderTile
+      )}
+    </div>
+  );
+};

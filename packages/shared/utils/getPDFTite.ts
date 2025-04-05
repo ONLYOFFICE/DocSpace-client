73428c84935abbddf7573d5656607e56b025a927
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2025-04-05 12:26:11 +0400

    Fix some imports

--- packages/shared/utils/getPDFTite.ts
+++ packages/shared/utils/getPDFTite.ts
@@ -24,7 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import { TTranslation } from "types";
+import { TTranslation } from "../types";
 
 export const getSinglePDFTitle = (t: TTranslation, isForm: boolean) => {
   return isForm ? t("Common:Document") : t("Common:Form");

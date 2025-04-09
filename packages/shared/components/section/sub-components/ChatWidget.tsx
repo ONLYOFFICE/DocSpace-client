e9ec5471148df9537b350a9e37baa2d5352b0141
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2025-04-09 17:36:44 +0400

    Fix management running

--- packages/shared/components/section/sub-components/ChatWidget.tsx
+++ packages/shared/components/section/sub-components/ChatWidget.tsx
@@ -24,7 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import { memo, useEffect, useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import classNames from "classnames";
 
 import CrossReactSvgUrl from "PUBLIC_DIR/images/icons/17/cross.react.svg?url";

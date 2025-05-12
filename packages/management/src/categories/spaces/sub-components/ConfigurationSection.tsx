d263f4891fe042dbf3aebf9615dcb6b43dd75aee
gopienkonikita <nikita.gopienko@onlyoffice.com>
2025-05-12 14:40:46 +0300

    Client: Management: Upgrade React to 19.1.0

--- packages/management/src/categories/spaces/sub-components/ConfigurationSection.tsx
+++ packages/management/src/categories/spaces/sub-components/ConfigurationSection.tsx
@@ -24,7 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import React from "react";
+import React, { type JSX } from "react";
 import { observer } from "mobx-react";
 import toLower from "lodash/toLower";
 import { isMobile } from "react-device-detect";

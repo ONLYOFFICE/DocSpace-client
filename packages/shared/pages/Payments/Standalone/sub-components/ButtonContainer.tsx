10022cd7c8c783c0622bb581be60282c1cf590b1
gopienkonikita <nikita.gopienko@onlyoffice.com>
2025-05-16 19:49:32 +0300

    Client: Shared: Payments: ButtonContainer: fixed lint error

--- packages/shared/pages/Payments/Standalone/sub-components/ButtonContainer.tsx
+++ packages/shared/pages/Payments/Standalone/sub-components/ButtonContainer.tsx
@@ -24,7 +24,6 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import React, { type JSX } from "react";
 import { useTranslation } from "react-i18next";
 import { Button, ButtonSize } from "@docspace/shared/components/button";
 

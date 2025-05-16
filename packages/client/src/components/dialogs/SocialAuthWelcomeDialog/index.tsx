77ac3acfbc23898dbec4c7bcc69a9b54471029cf
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2025-05-16 16:05:53 +0500

    Client: remove useless import

--- packages/client/src/components/dialogs/SocialAuthWelcomeDialog/index.tsx
+++ packages/client/src/components/dialogs/SocialAuthWelcomeDialog/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { inject, observer } from "mobx-react";
 import { useNavigate } from "react-router";
 import { useTheme } from "styled-components";

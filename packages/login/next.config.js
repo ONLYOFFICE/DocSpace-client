74fe25bd400c89603fcb532239abe8116cae76a6
gopienkonikita <nikita.gopienko@onlyoffice.com>
2025-05-23 16:18:32 +0300

    Client: Login: init logger

--- packages/login/next.config.js
+++ packages/login/next.config.js
@@ -51,6 +51,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: process.env.TS_ERRORS_IGNORE === "true",
   },
+  serverExternalPackages: ["nconf"],
   compiler: {
     styledComponents: true,
   },

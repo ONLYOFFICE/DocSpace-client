f5525807a66701449185a348c15bf00ca9358e75
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2025-05-30 16:51:32 +0200

    Shared: AmazonSettings: Add stories

--- packages/shared/components/amazon-settings/mockData.ts
+++ packages/shared/components/amazon-settings/mockData.ts
@@ -24,29 +24,78 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import styled from "styled-components";
+import {
+  bucket,
+  REGION,
+  SERVICE_URL,
+  FORCEPATH_STYLE,
+  USE_HTTP,
+  SSE,
+  SSE_S3,
+  filePath,
+} from "./AmazonSettings.constants";
 
-export const StyledBody = styled.div`
-  margin-bottom: 16px;
-  .backup_storage-tooltip {
-    display: flex;
-    align-items: center;
-    p {
-      margin-inline-end: 8px;
-    }
-  }
-  .backup_text-input {
-    margin: 4px 0 10px 0;
-  }
+export const mockStorageRegions = [
+  {
+    displayName: "US East (N. Virginia)",
+    systemName: "us-east-1",
+  },
+  {
+    displayName: "US East (Ohio)",
+    systemName: "us-east-2",
+  },
+  {
+    displayName: "US West (Oregon)",
+    systemName: "us-west-2",
+  },
+  {
+    displayName: "Europe (Ireland)",
+    systemName: "eu-west-1",
+  },
+  {
+    displayName: "Europe (Frankfurt)",
+    systemName: "eu-central-1",
+  },
+];
 
-  .backup_radio-button-settings {
-    margin-top: 8px;
-    margin-bottom: 16px;
-  }
+export const mockSelectedStorage = {
+  id: "1",
+  title: "Amazon S3",
+  tenantId: "1",
+  isSet: true,
+  isThirdParty: true,
+  properties: [
+    {
+      name: "bucket",
+      title: "Bucket",
+      value: "my-test-bucket",
+    },
+    {
+      name: "accessKey",
+      title: "Access Key",
+      value: "AKIAIOSFODNN7EXAMPLE",
+    },
+    {
+      name: "region",
+      title: "Region",
+      value: "eu-central-1",
+    },
+  ],
+};
 
-  svg {
-    path {
-      fill: ${(props) => props.theme.iconButton.color} !important;
-    }
-  }
-`;
+export const mockFormSettings = {
+  [bucket]: "my-test-bucket",
+  [REGION]: "eu-central-1",
+  [SERVICE_URL]: "s3.amazonaws.com",
+  [FORCEPATH_STYLE]: "true",
+  [USE_HTTP]: "false",
+  [SSE]: SSE_S3,
+  [filePath]: "backup",
+};
+
+export const mockErrorsFields = {
+  [bucket]: false,
+  [REGION]: false,
+  [SERVICE_URL]: false,
+  [filePath]: false,
+};

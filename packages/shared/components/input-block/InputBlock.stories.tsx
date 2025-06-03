07da3f082c84f5498deecfcc75f31b4690c7872f
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2025-06-03 15:37:56 +0500

    Client: InputBlock: remove useless stories

--- packages/shared/components/input-block/InputBlock.stories.tsx
+++ packages/shared/components/input-block/InputBlock.stories.tsx
@@ -143,17 +143,6 @@ export const Password: Story = {
   },
 };
 
-export const CustomColors: Story = {
-  render: (args) => <Template {...args} />,
-  args: {
-    ...defaultArgs,
-    id: "custom-colors-input",
-    iconColor: "#6c2bd9",
-    hoverColor: "#4c1d95",
-    placeholder: "Custom icon colors",
-  },
-};
-
 export const Sizes: Story = {
   args: defaultArgs,
   render: () => (
@@ -184,14 +173,4 @@ export const Sizes: Story = {
       />
     </div>
   ),
-};
-
-export const WithCustomStyle: Story = {
-  render: (args) => <Template {...args} />,
-  args: {
-    ...defaultArgs,
-    id: "custom-style-input",
-    style: { width: "400px", backgroundColor: "#f3f4f6" },
-    placeholder: "Custom styled input",
-  },
 };

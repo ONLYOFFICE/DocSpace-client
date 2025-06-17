23cb36c6ec7f03965bc2e7c6ca37050abdaf24c5
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2025-06-17 14:31:03 +0300

    Shared: Types: Fixed lint errors

--- packages/shared/types/index.ts
+++ packages/shared/types/index.ts
@@ -24,11 +24,6 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import {
-  TGetColorTheme,
-  TSettings,
-  TVersionBuild,
-} from "../api/settings/types";
 import { RoomsType } from "../enums";
 import { TTheme, TColorScheme } from "../themes";
 import FirebaseHelper from "../utils/firebase";

6846de2f8c03b4095ef2f146d5445e135f42a117
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2025-06-19 19:01:26 +0400

    Login:Tests: fix typeScript errors

--- packages/login/__tests__/fixtures/base.ts
+++ packages/login/__tests__/fixtures/base.ts
@@ -38,14 +38,18 @@ import {
 import path from "path";
 import { allHandlers } from "@docspace/shared/__mocks__/handlers";
 
-export const test = base.extend<{
-  page: Page;
-  clientRequestInterceptor: WorkerFixture;
-  serverRequestInterceptor: SetupServerApi;
-  port: string;
-  baseUrl: string;
-  resetHandlers: void;
-}>({
+export const test = base.extend<
+  {
+    page: Page;
+    clientRequestInterceptor: WorkerFixture;
+    resetHandlersServer: void;
+  },
+  {
+    serverRequestInterceptor: SetupServerApi;
+    port: string;
+    baseUrl: string;
+  }
+>({
   port: [
     async ({}, use) => {
       const { port, server } = await createNextTestServer(

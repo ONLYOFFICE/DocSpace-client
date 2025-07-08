8cef108aecde8cb880b01cffd98e77bedb715721
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2025-07-08 14:12:57 +0400

    Merge branch 'develop' into feature/mock-service-worker

---
+++ packages/shared/components/table/sub-components/group-menu-item/GroupMenuItem.stories.tsx
@@ -0,0 +1,100 @@
+// (c) Copyright Ascensio System SIA 2009-2025
+//
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+//
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+//
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+//
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+//
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+//
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { GroupMenuItem } from "./GroupMenuItem";
+import { TGroupMenuItem } from "../../Table.types";
+
+const meta = {
+  title: "Components/Table/GroupMenuItem",
+  component: GroupMenuItem,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "GroupMenuItem component for table group menu items with dropdown support",
+      },
+    },
+  },
+  argTypes: {
+    isBlocked: { control: "boolean" },
+  },
+} satisfies Meta<typeof GroupMenuItem>;
+
+export default meta;
+type Story = StoryObj<typeof GroupMenuItem>;
+
+const createMenuItem = (
+  overrides: Partial<TGroupMenuItem> = {},
+): TGroupMenuItem => {
+  return {
+    label: "Menu Item",
+    disabled: false,
+    onClick: () => {},
+    iconUrl: "",
+    title: "Menu Item Title",
+    withDropDown: false,
+    options: [],
+    id: "group-menu-item",
+    ...overrides,
+  };
+};
+
+export const Default: Story = {
+  args: {
+    item: createMenuItem(),
+    isBlocked: false,
+  },
+};
+
+export const WithDropdown: Story = {
+  args: {
+    item: createMenuItem({
+      withDropDown: true,
+      options: [
+        {
+          key: "option-1",
+          label: "Option 1",
+          onClick: () => {},
+        },
+        {
+          key: "option-2",
+          label: "Option 2",
+          onClick: () => {},
+        },
+      ],
+    }),
+    isBlocked: false,
+  },
+};
+
+export const Blocked: Story = {
+  args: {
+    item: createMenuItem(),
+    isBlocked: true,
+  },
+};

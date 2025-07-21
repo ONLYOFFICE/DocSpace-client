7e6112d0f2c206a638249e4e3a2b82b895585a6e
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2025-07-21 11:24:15 +0200

    Client: Add recent folder to getIsFiles

--- packages/client/src/store/InfoPanelStore.js
+++ packages/client/src/store/InfoPanelStore.js
@@ -506,6 +506,7 @@ class InfoPanelStore {
     return (
       pathname.indexOf("files") !== -1 ||
       pathname.indexOf("personal") !== -1 ||
+      pathname.indexOf("recent") !== -1 ||
       pathname.indexOf("media") !== -1
     );
   };

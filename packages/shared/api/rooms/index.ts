7e76ad40fe47b071e1f3cb0ed8a4ecf8199e3790
Ik-Akmal <isomadinovakmal1997@gmail.com>
2025-07-21 14:02:37 +0500

    fix: lint error

--- packages/shared/api/rooms/index.ts
+++ packages/shared/api/rooms/index.ts
@@ -454,7 +454,7 @@ export function createExternalLink(
   denyDownload: boolean = false,
   title?: string,
   disabled?: boolean,
-  password?: string | null = null,
+  password: string | null = null,
   expirationDate?: moment.Moment,
 ) {
   const skipRedirect = true;

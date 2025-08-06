44b33cfd09ab85b78199defc6170c99f4bf3a9f7
Viktor Fomin <viktor.fomin@onlyoffice.com>
2025-08-06 13:24:31 +0300

    Client: add subscribe on encryption

--- packages/client/src/Shell.jsx
+++ packages/client/src/Shell.jsx
@@ -141,16 +141,16 @@ const Shell = ({ page = "home", ...rest }) => {
 
   useEffect(() => {
     SocketHelper.emit(SocketCommands.Subscribe, {
-      roomParts: "storage-encryption",
-    });
-
-    SocketHelper.emit(SocketCommands.Subscribe, {
       roomParts: "restore",
     });
 
     if (standalone) {
       SocketHelper.emit(SocketCommands.SubscribeInSpaces, {
         roomParts: "restore",
+      });
+
+      SocketHelper.emit(SocketCommands.SubscribeInSpaces, {
+        roomParts: "encryption",
       });
     }
 
@@ -198,6 +198,21 @@ const Shell = ({ page = "home", ...rest }) => {
       });
     };
   }, [setPreparationPortalDialogVisible]);
+
+  useEffect(() => {
+    const storageEncryptionHandler = () => {
+      window.location.href = "/encryption-portal";
+    };
+
+    SocketHelper.on(SocketEvents.StorageEncryption, storageEncryptionHandler);
+
+    return () => {
+      SocketHelper.off(
+        SocketEvents.StorageEncryption,
+        storageEncryptionHandler,
+      );
+    };
+  }, []);
 
   useEffect(() => {
     const callback = (loginEventId) => {

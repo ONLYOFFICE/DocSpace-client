dff470bb5f62914d94b76240f97d4b3f1089530c
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2025-08-24 12:36:45 +0400

    Fix error  'useCallback' is defined but never used  @typescript-eslint/no-unused-vars

--- packages/client/src/pages/PortalSettings/categories/developer-tools/index.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/index.js
@@ -24,7 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import { inject, observer } from "mobx-react";
 
 import config from "PACKAGE_FILE";

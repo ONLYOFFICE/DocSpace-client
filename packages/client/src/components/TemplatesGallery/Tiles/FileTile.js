08048f56e8e3700e4908fcb6ebbda6387e8d045c
Vlada Gazizova <vlada.gazizova@onlyoffice.com>
2025-09-02 11:30:23 +0300

    Client:TemplatesGallery:Add for tile dark theme, rewrite to scss.

--- packages/client/src/components/TemplatesGallery/Tiles/FileTile.js
+++ packages/client/src/components/TemplatesGallery/Tiles/FileTile.js
@@ -25,126 +25,9 @@
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
 import { ReactSVG } from "react-svg";
-import styled, { css } from "styled-components";
-import { tablet } from "@docspace/shared/utils";
-import { globalColors } from "@docspace/shared/themes";
 import InfoReactSvgUrl from "PUBLIC_DIR/images/info.icon.react.svg?url";
-
-const Styled = styled.div`
-  width: 100%;
-  height: 100%;
-  min-height: 0;
-  box-sizing: border-box;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  border: 2px solid transparent;
-  border-radius: 4px;
-
-  .thumbnail-container {
-    flex: 1;
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    border: ${`1px solid ${globalColors.lightGraySelected}`};
-    margin: 10px 10px 0 10px;
-
-    ${(props) =>
-      props.smallPreview &&
-      css`
-        align-items: flex-start;
-      `}
-  }
-
-  .thumbnail-image {
-    max-width: 100%;
-    max-height: 100%;
-    width: auto;
-    height: auto;
-    object-fit: contain;
-
-    ${(props) =>
-      props.smallPreview &&
-      css`
-        width: 100%;
-        height: auto;
-        object-fit: cover;
-        object-position: top;
-      `}
-  }
-
-  .name {
-    height: 38px;
-    width: 100%;
-    box-sizing: border-box;
-    padding: 8px 10px 10px 10px;
-    display: flex;
-    align-items: center;
-    overflow: hidden;
-    flex-shrink: 0;
-  }
-
-  .name-text {
-    font-weight: 600;
-    font-size: 13px;
-    line-height: 20px;
-    color: ${globalColors.grayText};
-    width: 100%;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    display: block;
-    min-width: 0;
-    max-width: 100%;
-  }
-
-  .info-icon {
-    position: absolute;
-    top: 0px;
-    right: 0px;
-    height: 32px;
-    width: 32px;
-    border-radius: 3px;
-    background: ${globalColors.white};
-    box-shadow: 0px 2px 4px 0px ${globalColors.boxShadowDarkColor};
-    display: none;
-    cursor: pointer;
-
-    .icon {
-      height: 16px;
-      width: 16px;
-      padding: 8px 0px 0px 8px;
-    }
-  }
-
-  &:hover {
-    border-color: ${globalColors.lightBlueMain};
-
-    .info-icon {
-      display: block;
-    }
-
-    .name-text {
-      color: ${globalColors.lightBlueMain};
-    }
-  }
-
-  &:active {
-    background: ${globalColors.lightGrayHover};
-
-    .name-text {
-      color: ${globalColors.grayText};
-    }
-  }
-
-  @media ${tablet} {
-    .info-icon {
-      display: block;
-    }
-  }
-`;
+import classNames from "classnames";
+import styles from "./FileTile.module.scss";
 
 const FileTile = ({ item, smallPreview, onClickInfo, onClick }) => {
   const previewSrc = item?.attributes.card_prewiew.data?.attributes.url;
@@ -154,26 +37,34 @@ const FileTile = ({ item, smallPreview, onClickInfo, onClick }) => {
     onClickInfo?.(e);
   };
 
+  const thumbnailContainerClass = classNames(styles.thumbnailContainer, {
+    [styles.smallPreview]: smallPreview,
+  });
+
+  const thumbnailImageClass = classNames(styles.thumbnailImage, {
+    [styles.smallPreview]: smallPreview,
+  });
+
   return (
-    <Styled smallPreview={smallPreview} onClick={onClick}>
-      <div className="thumbnail-container">
+    <div className={styles.fileTile} onClick={onClick}>
+      <div className={thumbnailContainerClass}>
         <img
           src={previewSrc}
-          className="thumbnail-image"
+          className={thumbnailImageClass}
           alt="Thumbnail-img"
           data-testid="template-thumbnail"
         />
       </div>
-      <div className="name">
-        <div className="name-text" title={item.attributes.name_form}>
+      <div className={styles.name}>
+        <div className={styles.nameText} title={item.attributes.name_form}>
           {item.attributes.name_form}
         </div>
       </div>
 
-      <div className="info-icon" onClick={handleInfoClick}>
-        <ReactSVG src={InfoReactSvgUrl} className="icon" />
+      <div className={styles.infoIcon} onClick={handleInfoClick}>
+        <ReactSVG src={InfoReactSvgUrl} className={styles.icon} />
       </div>
-    </Styled>
+    </div>
   );
 };
 

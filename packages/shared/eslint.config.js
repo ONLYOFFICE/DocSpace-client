5d574d6b72785be6210f3b2b176bd8d33fe8f403
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2025-09-03 15:20:05 +0400

    Eslint: extend ignores

--- packages/shared/eslint.config.js
+++ packages/shared/eslint.config.js
@@ -1,24 +1,24 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import importPlugin from 'eslint-plugin-import';
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
-import globals from 'globals';
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import importPlugin from "eslint-plugin-import";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettierConfig,
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: './tsconfig.eslint.json',
-        sourceType: 'module',
+        project: "./tsconfig.eslint.json",
+        sourceType: "module",
         ecmaFeatures: {
           jsx: true,
         },
@@ -30,111 +30,116 @@ export default [
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
+      "@typescript-eslint": tseslint.plugin,
       react,
-      'react-hooks': reactHooks,
-      'jsx-a11y': jsxA11y,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
       import: importPlugin,
       prettier,
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
     rules: {
-      
       // Custom rules
-      'import/prefer-default-export': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      '@typescript-eslint/ban-types': 'off',
-      'jsx-a11y/label-has-associated-control': 'off',
-      'jsx-a11y/no-static-element-interactions': 'off',
-      'jsx-a11y/anchor-is-valid': 'off',
-      'jsx-a11y/tabindex-no-positive': 'off',
-      'jsx-a11y/no-autofocus': 'off',
-      'jsx-a11y/click-events-have-key-events': 'off',
-      'jsx-a11y/no-noninteractive-element-interactions': 'off',
-      'jsx-a11y/mouse-events-have-key-events': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-props-no-spreading': 'off',
-      'consistent-return': 'off',
-      'no-nested-ternary': 'off',
-      'no-return-assign': 'off',
-      'react/require-default-props': 'off',
-      'react/function-component-definition': 'off',
-      'react/forbid-prop-types': 'off',
-      'react/prop-types': 'off',
-      'class-methods-use-this': 'off',
-      'no-console': 'off',
-      'no-useless-escape': 'off',
-      'no-param-reassign': 'off',
-      'prefer-destructuring': 'off',
-      '@typescript-eslint/no-duplicate-enum-values': 'off',
-      '@typescript-eslint/only-throw-error': ['error'],
-      '@typescript-eslint/no-loop-func': 'error',
-      '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
-      'prettier/prettier': [
-        'error',
+      "import/prefer-default-export": "off",
+      "import/no-extraneous-dependencies": "off",
+      "@typescript-eslint/ban-types": "off",
+      "jsx-a11y/label-has-associated-control": "off",
+      "jsx-a11y/no-static-element-interactions": "off",
+      "jsx-a11y/anchor-is-valid": "off",
+      "jsx-a11y/tabindex-no-positive": "off",
+      "jsx-a11y/no-autofocus": "off",
+      "jsx-a11y/click-events-have-key-events": "off",
+      "jsx-a11y/no-noninteractive-element-interactions": "off",
+      "jsx-a11y/mouse-events-have-key-events": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-props-no-spreading": "off",
+      "consistent-return": "off",
+      "no-nested-ternary": "off",
+      "no-return-assign": "off",
+      "react/require-default-props": "off",
+      "react/function-component-definition": "off",
+      "react/forbid-prop-types": "off",
+      "react/prop-types": "off",
+      "class-methods-use-this": "off",
+      "no-console": "off",
+      "no-useless-escape": "off",
+      "no-param-reassign": "off",
+      "prefer-destructuring": "off",
+      "@typescript-eslint/no-duplicate-enum-values": "off",
+      "@typescript-eslint/only-throw-error": ["error"],
+      "@typescript-eslint/no-loop-func": "error",
+      "@typescript-eslint/no-use-before-define": [
+        "error",
+        { functions: false },
+      ],
+      "prettier/prettier": [
+        "error",
         {
-          endOfLine: 'auto',
+          endOfLine: "auto",
         },
       ],
-      'react/jsx-filename-extension': [
+      "react/jsx-filename-extension": [
         1,
-        { extensions: ['.js', '.ts', '.jsx', '.tsx'] },
+        { extensions: [".js", ".ts", ".jsx", ".tsx"] },
       ],
-      'lines-between-class-members': [
-        'error',
-        'always',
+      "lines-between-class-members": [
+        "error",
+        "always",
         { exceptAfterSingleLine: true },
       ],
-      'react/jsx-no-leaked-render': ['error', { validStrategies: ['ternary'] }],
-      'react/jsx-max-depth': ['error', { max: 10 }],
-      'react/jsx-key': [
-        'error',
+      "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
+      "react/jsx-max-depth": ["error", { max: 10 }],
+      "react/jsx-key": [
+        "error",
         {
           checkFragmentShorthand: true,
           checkKeyMustBeforeSpread: true,
           warnOnDuplicates: true,
         },
       ],
-      'react/jsx-no-useless-fragment': 'warn',
-      'react/jsx-curly-brace-presence': 'warn',
-      'react/prefer-stateless-function': 'error',
-      'react/no-unused-prop-types': 'error',
-      'react/jsx-pascal-case': 'error',
-      'react/no-danger': 'error',
-      'react/no-danger-with-children': 'error',
-      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
-      'react/no-this-in-sfc': 'error',
+      "react/jsx-no-useless-fragment": "warn",
+      "react/jsx-curly-brace-presence": "warn",
+      "react/prefer-stateless-function": "error",
+      "react/no-unused-prop-types": "error",
+      "react/jsx-pascal-case": "error",
+      "react/no-danger": "error",
+      "react/no-danger-with-children": "error",
+      "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
+      "react/no-this-in-sfc": "error",
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
   },
   {
-    files: ['**/*.{js,cjs}'],
+    files: ["**/*.{js,cjs}"],
     languageOptions: {
       globals: {
         ...globals.node,
       },
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
     rules: {
-      '@typescript-eslint/no-require-imports': 'off',
+      "@typescript-eslint/no-require-imports": "off",
     },
   },
   {
     ignores: [
-      './tsconfig.json',
-      'coverage/**',
-      'storybook-static/**',
-      'components/scrollbar/custom-scrollbar/**',
+      "./tsconfig.json",
+      "./eslint.config.js",
+      "node_modules/**",
+      "storybook-static/**",
+      "coverage/**",
+      "storybook-static/**",
+      "components/scrollbar/custom-scrollbar/**",
     ],
   },
 ];

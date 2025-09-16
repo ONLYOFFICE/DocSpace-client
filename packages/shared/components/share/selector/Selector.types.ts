03addd6b6865c73efa3c896416b9f22fee5b4787
Ik-Akmal <isomadinovakmal1997@gmail.com>
2025-09-16 18:40:46 +0500

    fix: update import path for RoomMember type to use relative path

--- packages/shared/components/share/selector/Selector.types.ts
+++ packages/shared/components/share/selector/Selector.types.ts
@@ -24,7 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import { RoomMember } from "api/rooms/types";
+import type { RoomMember } from "../../../api/rooms/types";
 import type { TFile, TFolder } from "../../../api/files/types";
 
 export interface ShareSelectorProps {

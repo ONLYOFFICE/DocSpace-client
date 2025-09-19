56d7274d74fd5183b52ce66c45ca2ebf80a8a5f8
Viktor Fomin <viktor.fomin@onlyoffice.com>
2025-09-19 13:43:13 +0300

    Client: PortalSettings: Webhooks: use empty view component

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/EmptyFilter.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/EmptyFilter.js
@@ -24,48 +24,17 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import EmptyFilterImg from "PUBLIC_DIR/images/emptyview/empty.history.light.svg?url";
-import EmptyFilterDarkImg from "PUBLIC_DIR/images/emptyview/empty.history.dark.svg?url";
-import ClearEmptyFilterIcon from "PUBLIC_DIR/images/clear.empty.filter.svg?url";
+import EmptyFilterImg from "PUBLIC_DIR/images/emptyview/empty.history.light.svg";
+import EmptyFilterDarkImg from "PUBLIC_DIR/images/emptyview/empty.history.dark.svg";
+import ClearEmptyFilterSvg from "PUBLIC_DIR/images/clear.empty.filter.svg";
 
 import React from "react";
-import styled from "styled-components";
 import { inject, observer } from "mobx-react";
 import { useTranslation } from "react-i18next";
 
-import { Text } from "@docspace/shared/components/text";
-import { Link } from "@docspace/shared/components/link";
-import { globalColors } from "@docspace/shared/themes";
+import { EmptyView } from "@docspace/shared/components/empty-view";
+
 import { formatFilters } from "SRC_DIR/helpers/webhooks";
-
-const EmptyFilterWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  margin-top: 149px;
-`;
-
-const EmptyFilterContent = styled.div`
-  display: flex;
-
-  .emptyFilterText {
-    margin-inline-start: 40px;
-  }
-
-  .clearFilter {
-    display: block;
-    margin-top: 26px;
-    cursor: pointer;
-  }
-
-  .clearFilterIcon {
-    margin-inline-end: 8px;
-  }
-
-  .emptyFilterHeading {
-    margin-bottom: 8px;
-  }
-`;
 
 const EmptyFilter = (props) => {
   const { applyFilters, clearHistoryFilters, theme } = props;
@@ -81,49 +50,23 @@ const EmptyFilter = (props) => {
     );
   };
 
+  const options = [
+    {
+      key: "empty-view-filter",
+      to: "",
+      description: t("Common:ClearFilter"),
+      icon: <ClearEmptyFilterSvg />,
+      onClick: clearFilters,
+    },
+  ];
+
   return (
-    <EmptyFilterWrapper>
-      <EmptyFilterContent>
-        <img
-          src={theme.isBase ? EmptyFilterImg : EmptyFilterDarkImg}
-          alt="Empty filter"
-        />
-        <div className="emptyFilterText">
-          <Text
-            fontSize="16px"
-            fontWeight={700}
-            as="p"
-            className="emptyFilterHeading"
-          >
-            {t("Common:NotFoundTitle")}
-          </Text>
-          <Text
-            fontSize="12px"
-            color={
-              theme.isBase ? globalColors.grayText : globalColors.grayDarkText
-            }
-          >
-            {t("NoResultsMatched")}
-          </Text>
-          <span className="clearFilter" onClick={clearFilters}>
-            <img
-              src={ClearEmptyFilterIcon}
-              alt={t("ClearFilter")}
-              className="clearFilterIcon"
-            />
-            <Link
-              color={theme.isBase ? globalColors.lightGrayDark : "inherit"}
-              isHovered
-              fontWeight={600}
-              type="action"
-              dataTestId="clear_empty_filter_link"
-            >
-              {t("Common:ClearFilter")}
-            </Link>
-          </span>
-        </div>
-      </EmptyFilterContent>
-    </EmptyFilterWrapper>
+    <EmptyView
+      icon={theme.isBase ? <EmptyFilterImg /> : <EmptyFilterDarkImg />}
+      title={t("Common:NotFoundTitle")}
+      options={options}
+      description={t("NoResultsMatched")}
+    />
   );
 };
 

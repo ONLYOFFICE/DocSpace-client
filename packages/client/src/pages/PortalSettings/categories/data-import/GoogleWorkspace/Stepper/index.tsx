2b12e9297b42ac938515c33ff3fcc63e37a0946e
Alexander Evdokimov <evdoki15@yandex.ru>
2025-10-02 19:42:44 +0300

    Client: DataImport: GoogleWorkspace: consolidate shared files and folders export details in Google Workspace stepper

--- packages/client/src/pages/PortalSettings/categories/data-import/GoogleWorkspace/Stepper/index.tsx
+++ packages/client/src/pages/PortalSettings/categories/data-import/GoogleWorkspace/Stepper/index.tsx
@@ -29,7 +29,6 @@ import { Trans } from "react-i18next";
 import { Text } from "@docspace/shared/components/text";
 import { HelpButton } from "@docspace/shared/components/help-button";
 
-import PeopleIcon from "PUBLIC_DIR/images/icons/16/catalog.accounts.react.svg?url";
 import type { TFunction } from "i18next";
 import SelectFileStep from "../../components/SelectFileStep";
 import SelectUsersStep from "../../components/SelectUsersStep";
@@ -128,17 +127,13 @@ export const getStepsData = (
           t={t}
           serviceName="Google Workspace"
           usersExportDetails={{
-            name: t("Common:Contacts"),
-            icon: PeopleIcon,
+            name: t("Settings:Users"),
           }}
           personalExportDetails={{
             name: t("Settings:GoogleDriveFiles"),
           }}
-          sharedFilesExportDetails={{
-            name: t("Settings:SharedFiles"),
-          }}
-          sharedFoldersExportDetails={{
-            name: t("Settings:SharedFolders"),
+          sharedFilesAndFoldersExportDetails={{
+            name: t("Settings:SharedFilesAndFoldersDetails"),
           }}
         />
       ),

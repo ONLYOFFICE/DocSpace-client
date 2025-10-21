583aedbfa5477e6bea3ba92143223be2f1e384af
Vlada Gazizova <vlada.gazizova@onlyoffice.com>
2025-10-22 00:18:22 +0300

    Client:TemplateGallery:Delete unused code.

--- packages/client/src/components/TemplateGallery/TilesContainer/index.tsx
+++ packages/client/src/components/TemplateGallery/TilesContainer/index.tsx
@@ -24,7 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 import { observer, inject } from "mobx-react";
 import { withTranslation } from "react-i18next";
 import type { FC } from "react";

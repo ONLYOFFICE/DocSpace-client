6c50ed541d2a0ee86784816933f8413f38276b83
TatianaLopaeva <tatiana.lopaeva@onlyoffice.com>
2025-10-23 11:54:30 +0300

    Client: PortalSettings: Services: Added AI service dialog and refactored service components.

--- packages/client/src/pages/PortalSettings/categories/services/sub-components/AITools/AIServiceDialog.tsx
+++ packages/client/src/pages/PortalSettings/categories/services/sub-components/AITools/AIServiceDialog.tsx
@@ -23,6 +23,31 @@
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+// (c) Copyright Ascensio System SIA 2009-2025
+//
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+//
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+//
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+//
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+//
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+//
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
 import React from "react";
 import { useTranslation } from "react-i18next";
@@ -38,12 +63,6 @@ import { BACKUP_SERVICE } from "@docspace/shared/constants";
 import ServiceToggleSection from "../ServiceToggleSection";
 import ServiceContent from "../ServiceContent";
 
-import ClockIcon from "PUBLIC_DIR/images/icons/32/clock.svg";
-import ServiceIcon from "PUBLIC_DIR/images/icons/32/service.icon.svg";
-import MoveIcon from "PUBLIC_DIR/images/icons/32/move.icon.svg";
-import CloudsIcon from "PUBLIC_DIR/images/icons/32/clouds.icon.svg";
-import HistoryIcon from "PUBLIC_DIR/images/icons/32/history.icon.svg";
-
 import styles from "../../styles/BackupServiceDialog.module.scss";
 
 interface BackupServiceDialogProps {
@@ -52,11 +71,11 @@ interface BackupServiceDialogProps {
   onToggle: (id: string, enabled: boolean) => void;
   isEnabled?: boolean;
   description?: string;
-  backupServicePrice?: number;
-  formatWalletCurrency?: (
-    item: number | null,
-    fractionDigits: number,
-  ) => string;
+  // backupServicePrice?: number;
+  // formatWalletCurrency?: (
+  //   item: number | null,
+  //   fractionDigits: number,
+  // ) => string;
 }
 
 interface ServiceOption {
@@ -66,56 +85,23 @@ interface ServiceOption {
   icon: React.ReactNode;
 }
 
-const BackupServiceDialog: React.FC<BackupServiceDialogProps> = ({
+const AIServiceDialog: React.FC<BackupServiceDialogProps> = ({
   visible,
   onClose,
   onToggle,
   isEnabled = false,
   description,
-  backupServicePrice,
-  formatWalletCurrency,
+  // backupServicePrice,
+  //formatWalletCurrency,
 }) => {
   const { t } = useTranslation(["Services", "Common", "Payments"]);
 
   const handleToggleChange = () => {
-    onToggle(BACKUP_SERVICE, isEnabled);
+    onToggle("ai", isEnabled);
     onClose();
   };
 
-  const serviceOptions: ServiceOption[] = [
-    {
-      id: "temporary",
-      title: t("Common:TemporaryStorage"),
-      description: t("TemporaryDescription"),
-      icon: <ClockIcon />,
-    },
-    {
-      id: "backup-room",
-      title: t("Common:RoomsModule"),
-      description: t("BackupRoomDescription", {
-        sectionName: t("Common:MyDocuments"),
-      }),
-      icon: <ServiceIcon />,
-    },
-    {
-      id: "third-party-resource",
-      title: t("Common:ThirdPartyResource"),
-      description: t("ThirdPartyResourceDescription"),
-      icon: <MoveIcon />,
-    },
-    {
-      id: "third-party-storage",
-      title: t("Common:ThirdPartyStorage"),
-      description: t("ThirdPartyStorageDescription"),
-      icon: <CloudsIcon />,
-    },
-    {
-      id: "automatic-backup",
-      title: t("Common:AutoBackup"),
-      description: t("AutomaticBackupDescription"),
-      icon: <HistoryIcon />,
-    },
-  ];
+  const serviceOptions: ServiceOption[] = [];
 
   return (
     <ModalDialog
@@ -124,17 +110,14 @@ const BackupServiceDialog: React.FC<BackupServiceDialogProps> = ({
       displayType={ModalDialogType.aside}
       withBodyScroll
     >
-      <ModalDialog.Header>{t("Common:Backup")}</ModalDialog.Header>
+      <ModalDialog.Header>{"AI"}</ModalDialog.Header>
       <ModalDialog.Body>
         <ServiceToggleSection
           isEnabled={isEnabled}
           onToggle={handleToggleChange}
-          title={t("Common:Backup")}
-          priceText={t("Payments:PerBackupWithBracket", {
-            currency: formatWalletCurrency!(backupServicePrice!, 2),
-          })}
+          title={"AI"}
           description={description}
-          testId="service-backup-toggle-button"
+          testId="service-ai-toggle-button"
         />
         <div className={styles.servicesList}>
           {serviceOptions.map((service) => (
@@ -171,4 +154,4 @@ export default inject(({ paymentStore }: TStore) => {
     backupServicePrice,
     formatWalletCurrency,
   };
-})(observer(BackupServiceDialog));
+})(observer(AIServiceDialog));

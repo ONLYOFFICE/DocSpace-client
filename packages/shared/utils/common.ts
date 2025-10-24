2b94ef30ceb946f8afb6a17ea43dcc17299897e2
Viktor Fomin <viktor.fomin@onlyoffice.com>
2025-10-24 17:52:35 +0300

    Shared: Utils: Common: delete unused

--- packages/shared/utils/common.ts
+++ packages/shared/utils/common.ts
@@ -83,7 +83,7 @@ import {
   TIMEZONE,
 } from "../constants";
 
-import { TI18n, TTranslation, ValueOf } from "../types";
+import { TTranslation, ValueOf } from "../types";
 import { TUser } from "../api/people/types";
 import { TFolder, TFile, TGetFolder } from "../api/files/types";
 import { TRoom } from "../api/rooms/types";
@@ -96,7 +96,7 @@ import TopLoaderService from "../components/top-loading-indicator";
 
 import { Encoder } from "./encoder";
 import { combineUrl } from "./combineUrl";
-import { getCookie, setCookie } from "./cookie";
+import { setCookie } from "./cookie";
 import { checkIsSSR } from "./device";
 
 import { hasOwnProperty } from "./object";
@@ -111,14 +111,6 @@ export const desktopConstants = Object.freeze({
 
 let timer: null | ReturnType<typeof setTimeout> = null;
 type I18n = I18nextProviderProps["i18n"];
-
-export function changeLanguage(i18n: TI18n, currentLng = getCookie(LANGUAGE)) {
-  return currentLng
-    ? i18n.language !== currentLng
-      ? i18n.changeLanguage(currentLng)
-      : Promise.resolve((...args: string[]) => i18n.t(...args))
-    : i18n.changeLanguage("en");
-}
 
 export function createPasswordHash(
   password: string,
@@ -281,18 +273,6 @@ export function showLoader() {
   // }, 500);
 }
 
-export function showProgress() {
-  if (isMobile) return;
-  TopLoaderService.cancel();
-  TopLoaderService.start();
-}
-
-export function isMe(user: TUser, userName: string) {
-  return (
-    user && user.id && (userName === "@self" || user.userName === userName)
-  );
-}
-
 export function isAdmin(currentUser: TUser) {
   return (
     currentUser.isAdmin ||
@@ -350,16 +330,6 @@ export const getUserTypeTranslation = (type: EmployeeType, t: TTranslation) => {
       return t("Common:Guest");
   }
 };
-
-export function clickBackdrop() {
-  const elms = document.getElementsByClassName(
-    "backdrop-active",
-  ) as HTMLCollectionOf<HTMLDivElement>;
-
-  if (elms && elms.length > 0) {
-    elms[0].click();
-  }
-}
 
 export function objectToGetParams(obj: object) {
   const params = Object.entries(obj)
@@ -490,27 +460,6 @@ export const getDirectionByLanguage = (lng: string) => {
   return isLanguageRtl(lng) ? "rtl" : "ltr";
 };
 
-// temporary function needed to replace rtl language in Editor to ltr
-export const getLtrLanguageForEditor = (
-  userLng: string | undefined,
-  portalLng: string,
-  isEditor: boolean = false,
-): string => {
-  let isEditorPath;
-  if (typeof window !== "undefined") {
-    isEditorPath = window?.location.pathname.indexOf("doceditor") !== -1;
-  }
-  const isUserLngRtl = isLanguageRtl(userLng || "en");
-  // const isPortalLngRtl = isLanguageRtl(portalLng);
-
-  if (userLng === undefined && portalLng) return portalLng;
-
-  if ((!isEditor && !isEditorPath) || (userLng && !isUserLngRtl))
-    return userLng || "en";
-
-  return "en";
-};
-
 export function loadScript(
   url: string,
   id: string,
@@ -573,12 +522,6 @@ export function convertToLanguage(key: string) {
   if (splittedKey.length > 1) return splittedKey[0];
 
   return key;
-}
-
-export function sleep(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
 }
 
 export function isElementInViewport(el: HTMLElement) {

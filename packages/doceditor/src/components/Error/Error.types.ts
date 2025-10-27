4069490adf82adbd96eb447a1513fae930821b6b
Viktor Fomin <viktor.fomin@onlyoffice.com>
2025-10-27 16:37:53 +0300

    Editor: delete useless

--- packages/doceditor/src/components/Error/Error.types.ts
+++
@@ -1,8 +0,0 @@
-import type { TUser } from "@docspace/shared/api/people/types";
-import type { TSettings } from "@docspace/shared/api/settings/types";
-
-export type ErrorProps = {
-  user: TUser;
-  settings: TSettings;
-  error: Error;
-};

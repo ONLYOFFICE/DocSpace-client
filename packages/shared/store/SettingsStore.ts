b9bc4f36940c5439300c9b556b73319f6ebafd95
Ik-Akmal <isomadinovakmal1997@gmail.com>
2025-10-30 15:19:25 +0500

    fix: enable error logging in settings store

--- packages/shared/store/SettingsStore.ts
+++ packages/shared/store/SettingsStore.ts
@@ -1074,7 +1074,7 @@ class SettingsStore {
     } catch (error) {
       if (isRequestAborted(error)) return;
 
-      // console.error(error);
+      console.error(error);
     } finally {
       this.setIsLoading(false);
       this.setIsLoaded(true);

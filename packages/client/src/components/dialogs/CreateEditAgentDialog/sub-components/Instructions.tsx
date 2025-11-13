ccbd340332e6d2ffbd86e331bb46a90c0c27a7c6
TatianaLopaeva <tatiana.lopaeva@onlyoffice.com>
2025-11-13 17:16:08 +0300

    Fix Bug 78318 - AI. Change the description of the Instruction section when creating an AI agent.

--- packages/client/src/components/dialogs/CreateEditAgentDialog/sub-components/Instructions.tsx
+++ packages/client/src/components/dialogs/CreateEditAgentDialog/sub-components/Instructions.tsx
@@ -71,14 +71,14 @@ const InstructionsSettings = ({
             className="set_room_params-info-description"
             noSelect
           >
-            {t("InstructionsDescription")}
+            {t("InstructionsDescriptionAgent")}
           </Text>
         </div>
         <Textarea
           value={value}
           onChange={handleChange}
           heightTextArea={144}
-          placeholder={t("InstructionsPlaceholder")}
+          placeholder={t("InstructionsDescriptionAgentExample")}
         />
       </div>
     </StyledParam>

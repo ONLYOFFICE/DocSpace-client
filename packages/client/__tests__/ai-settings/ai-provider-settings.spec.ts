d6d287931e3779fe0658c46feccb9363cabb6fa3
gopienkonikita <nikita.gopienko@onlyoffice.com>
2026-04-03 15:09:46 +0300

    Client: E2E: AI Settings: fixed test

--- packages/client/__tests__/ai-settings/ai-provider-settings.spec.ts
+++ packages/client/__tests__/ai-settings/ai-provider-settings.spec.ts
@@ -435,12 +435,12 @@ test.describe("AI Provider", () => {
     const tilesWithErrors = page.getByTestId("ai-tile-error-icon");
     await expect(tilesWithErrors).toHaveCount(4);
 
-    const toastMessage = page
-      .getByTestId("toast")
+    const providerError = page
+      .getByTestId("default-provider-field-container")
       .getByText(
         "The specified API key is invalid or does not have access rights. Verify that the key is correct and try again",
       );
-    await expect(toastMessage).toBeVisible();
+    await expect(providerError).toBeVisible();
 
     await expectScreenshot(page,[
       "desktop",

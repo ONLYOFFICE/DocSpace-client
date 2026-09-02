beb30a2767548268687663cd7f99d528aaa4349f
gopienkonikita <nikita.gopienko@onlyoffice.com>
2026-09-02 17:44:21 +0300

    Client: JavascriptSDK. Place the connectors catalog under the content, within its width

--- packages/client/src/pages/PortalSettings/categories/developer-tools/JavascriptSDK/presets/RoomSelector.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/JavascriptSDK/presets/RoomSelector.js
@@ -61,7 +61,6 @@ import {
 import { CancelTextInput } from "../sub-components/CancelTextInput";
 import { FrameIdSetter } from "../sub-components/FrameIdSetter";
 import { HeightSetter } from "../sub-components/HeightSetter";
-import Integration from "../sub-components/Integration";
 import { MainElementParameter } from "../sub-components/MainElementParameter";
 import { PresetWrapper } from "../sub-components/PresetWrapper";
 import { PreviewBlock } from "../sub-components/PreviewBlock";
@@ -330,12 +329,8 @@ const RoomSelector = (props) => {
               dropDownTestId="room_type_dropdown"
             />
           </ControlsSection>
-
-          <Integration compact className="integration-examples" />
         </Controls>
       </Container>
-
-      <Integration className="integration-examples integration-examples-bottom" />
     </PresetWrapper>
   );
 };
